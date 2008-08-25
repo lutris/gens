f6e2a6a9b1e061ccd874860145b7d941762c6690
David Korth <david@gs_laptop.gerbilsoft.gsft>
2008-08-24 20:14:19 -0400

    Finished adding controls to the AddControllerVBox() function.

--- src/gens/ui/gtk/controller_config_window.c
+++ src/gens/ui/gtk/controller_config_window.c
@@ -117,7 +117,22 @@ void AddControllerVBox(GtkWidget *frame, int port)
 	GtkWidget *hbox;
 	GtkWidget *label_port;
 	GtkWidget *check_teamplayer;
+	GtkWidget *table_players;
+	GtkWidget *label_player;
+	GtkWidget *optionmenu_padtype;
+	GtkWidget *optionmenu_padtype_menu;
+	GtkWidget *optionmenu_padtype_menuitem;
+	GtkWidget *button_configure;
 	char tmp[128];
+	char player[4];
+	int i, j;
+	
+	// TODO: Move this somewhere else?
+	const char* PadTypes[2] =
+	{
+		"3 buttons",
+		"6 buttons",
+	};
 	
 	if (!frame || (port != 1 && port != 2))
 		return;
@@ -142,7 +157,7 @@ void AddControllerVBox(GtkWidget *frame, int port)
 	label_port = gtk_label_new(tmp);
 	sprintf(tmp, "label_port_%d", port);
 	gtk_widget_set_name(label_port, tmp);
-	gtk_label_set_use_markup(label_port, TRUE);
+	gtk_label_set_use_markup(GTK_LABEL(label_port), TRUE);
 	gtk_widget_show(label_port);
 	gtk_frame_set_label_widget(GTK_FRAME(frame), label_port);
 	
@@ -153,4 +168,75 @@ void AddControllerVBox(GtkWidget *frame, int port)
 	gtk_widget_show(check_teamplayer);
 	gtk_box_pack_start(GTK_BOX(hbox), check_teamplayer, FALSE, FALSE, 0);
 	GLADE_HOOKUP_OBJECT(controller_config_window, check_teamplayer, tmp);
+	
+	// Table for the player controls.
+	table_players = gtk_table_new(4, 3, FALSE);
+	sprintf(tmp, "table_players_%d", port);
+	gtk_widget_set_name(table_players, tmp);
+	gtk_table_set_col_spacings(GTK_TABLE(table_players), 10);
+	gtk_widget_show(table_players);
+	gtk_box_pack_start(GTK_BOX(vbox), table_players, TRUE, TRUE, 0);
+	GLADE_HOOKUP_OBJECT(controller_config_window, table_players, tmp);
+	
+	// Player inputs
+	for (i = 0; i < 4; i++)
+	{
+		if (i == 0)
+			sprintf(player, "%d", port);
+		else
+			sprintf(player, "%d%c", port, 'A' + (char)i);
+		
+		// Player label
+		sprintf(tmp, "Player %s", player);
+		label_player = gtk_label_new(tmp);
+		sprintf(tmp, "label_player_%s", player);
+		gtk_widget_set_name(label_player, tmp);
+		gtk_misc_set_alignment(GTK_MISC(label_player), 0, 0.5);
+		gtk_widget_show(label_player);
+		GLADE_HOOKUP_OBJECT(controller_config_window, label_player, tmp);
+		gtk_table_attach(GTK_TABLE(table_players), label_player, 0, 1, i, i + 1,
+				 (GtkAttachOptions)(GTK_FILL),
+				 (GtkAttachOptions)(0), 0, 0);
+		
+		// Pad type
+		optionmenu_padtype = gtk_option_menu_new();
+		sprintf(tmp, "optionmenu_padtype_%s", player);
+		gtk_widget_set_name(optionmenu_padtype, tmp);
+		gtk_widget_show(optionmenu_padtype);
+		GLADE_HOOKUP_OBJECT(controller_config_window, optionmenu_padtype, tmp);
+		gtk_table_attach(GTK_TABLE(table_players), optionmenu_padtype, 1, 2, i, i + 1,
+				 (GtkAttachOptions)(GTK_FILL),
+				 (GtkAttachOptions)(0), 0, 0);
+		
+		// Pad type menu
+		optionmenu_padtype_menu = gtk_menu_new();
+		sprintf(tmp, "optionmenu_padtype_menu_%s", player);
+		gtk_widget_set_name(optionmenu_padtype_menu, tmp);
+		gtk_widget_show(optionmenu_padtype_menu);
+		GLADE_HOOKUP_OBJECT(controller_config_window, optionmenu_padtype_menu, tmp);
+		
+		// Pad type submenu
+		for (j = 0; j < 2; j++)
+		{
+			optionmenu_padtype_menuitem = gtk_menu_item_new_with_label(PadTypes[j]);
+			sprintf(tmp, "optionmenu_padtype_menuitem_%s_%d", player, j);
+			gtk_widget_set_name(optionmenu_padtype_menuitem, tmp);
+			gtk_widget_show(optionmenu_padtype_menuitem);
+			GLADE_HOOKUP_OBJECT(controller_config_window, optionmenu_padtype_menuitem, tmp);
+			gtk_container_add(GTK_CONTAINER(optionmenu_padtype_menu), optionmenu_padtype_menuitem);
+		}
+		
+		// Add the pad type submenu to the pad type dropdown.
+		gtk_option_menu_set_menu(GTK_OPTION_MENU(optionmenu_padtype), optionmenu_padtype_menu);
+		
+		// "Reconfigure" button
+		button_configure = gtk_button_new_with_label("Reconfigure");
+		sprintf(tmp, "button_configure_%s", player);
+		gtk_widget_set_name(button_configure, tmp);
+		gtk_widget_show(button_configure);
+		GLADE_HOOKUP_OBJECT(controller_config_window, button_configure, tmp);
+		gtk_table_attach(GTK_TABLE(table_players), button_configure, 2, 3, i, i + 1,
+				 (GtkAttachOptions)(GTK_FILL),
+				 (GtkAttachOptions)(0), 0, 0);
+	}
 }
