450dc67d8aba67739f81b32918d495309c63fad6
David Korth <david@gs_laptop.gerbilsoft.gsft>
2008-08-25 04:17:11 -0400

    dos2unix'd.

--- src/gens/gens_core/gfx/hqfilters.c
+++ src/gens/gens_core/gfx/hqfilters.c
@@ -5,39 +5,59 @@
 extern "C"
 {
   
-#endif	/*  */
+#endif	/* 
+ */
   
 //void hq2x_16(unsigned char*, unsigned char*, int, int, int, int);
   unsigned int _LUT16to32[65536];
-   unsigned int _RGBtoYUV[65536];
-   
+   
+unsigned int _RGBtoYUV[65536];
+   
 //extern unsigned char MD_Screen;
 #include "vdp_rend.h"
 #include "misc.h"
 #ifdef __cplusplus
 } 
-#endif	/*  */
- int
+#endif	/* 
+ */
+ 
+int
 Blit_HQ2x_InitLUTs (void) 
 {
-  int i, j, k, r, g, b, Y, u, v;
-  for (i = 0; i < 65536; i++)
-    _LUT16to32[i] =
+  
+int i, j, k, r, g, b, Y, u, v;
+  
+
+for (i = 0; i < 65536; i++)
+    
+_LUT16to32[i] =
       ((i & 0xF800) << 8) + ((i & 0x07E0) << 5) + ((i & 0x001F) << 3);
-  for (i = 0; i < 32; i++)
-    for (j = 0; j < 64; j++)
-      for (k = 0; k < 32; k++)
+  
+
+for (i = 0; i < 32; i++)
+    
+for (j = 0; j < 64; j++)
+      
+for (k = 0; k < 32; k++)
 	
 	{
-	  r = i << 3;
-	  g = j << 2;
-	  b = k << 3;
-	  Y = (r + g + b) >> 2;
-	  u = 128 + ((r - b) >> 2);
-	  v = 128 + ((-r + 2 * g - b) >> 3);
-	  _RGBtoYUV[(i << 11) + (j << 5) + k] = (Y << 16) + (u << 8) + v;
-	}
-  
+	  
+r = i << 3;
+	  
+g = j << 2;
+	  
+b = k << 3;
+	  
+Y = (r + g + b) >> 2;
+	  
+u = 128 + ((r - b) >> 2);
+	  
+v = 128 + ((-r + 2 * g - b) >> 3);
+	  
+_RGBtoYUV[(i << 11) + (j << 5) + k] = (Y << 16) + (u << 8) + v;
+	
+}
+  
 /*  __asm
   {
     mov  eax, 1
@@ -49,9 +69,10 @@ Blit_HQ2x_InitLUTs (void)
   return nMMXsupport;
   */ 
     return Have_MMX;
-}
 
-
+}
+
+
 //void Blit_HQ2x(unsigned char *Dest, int pitch, int x, int y, int offset) {
   // pitch = bytes between dest rows (1280)
   // x = source width /pixels (320) (256)
