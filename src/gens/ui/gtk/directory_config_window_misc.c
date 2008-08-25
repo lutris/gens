1be38c43d4cd74db8bd2f127107ee1f9ee489c5f
David Korth <david@gs_laptop.gerbilsoft.gsft>
2008-08-25 18:12:12 -0400

    Implemented Directory_Config_Change().

--- src/gens/ui/gtk/directory_config_window_misc.c
+++ src/gens/ui/gtk/directory_config_window_misc.c
@@ -42,29 +42,25 @@ void Open_Directory_Config(void)
  */
 void Directory_Config_Change(int dir)
 {
-	// TODO
-	/*
-	GtkWidget *entry_file;
+	GtkWidget *entry_direntry;
 	char tmp[64];
-	char newFile[GENS_PATH_MAX];
+	char newDir[GENS_PATH_MAX];
 	int ret;
 	
-	// Get the entry widget for this file.
-	sprintf(tmp, "entry_%s", BIOSMiscFiles[file].tag);
-	entry_file = lookup_widget(directory_config_window, tmp);
+	// Get the entry widget for this directory.
+	sprintf(tmp, "entry_%s", DirEntries[dir].tag);
+	entry_direntry = lookup_widget(directory_config_window, tmp);
 	
-	// Request a new file.
-	sprintf(tmp, "Select %s File", BIOSMiscFiles[file].title);
-	ret = UI_OpenFile(tmp, gtk_entry_get_text(GTK_ENTRY(entry_file)),
-			  BIOSMiscFiles[file].filter, newFile);
+	// Request a new directory.
+	sprintf(tmp, "Select %s Directory", DirEntries[dir].title);
+	ret = UI_SelectDir(tmp, gtk_entry_get_text(GTK_ENTRY(entry_direntry)), newDir);
 	
 	// If Cancel was selected, don't do anything.
 	if (ret)
 		return;
 	
-	// Set the new file.
-	gtk_entry_set_text(GTK_ENTRY(entry_file), newFile);
-	*/
+	// Set the new directory.
+	gtk_entry_set_text(GTK_ENTRY(entry_direntry), newDir);
 }
 
 
