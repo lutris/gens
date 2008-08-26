817ee9c9a03d7d91ffa2cde8617f3f58bcf5e0e2
David Korth <david@gs_laptop.gerbilsoft.gsft>
2008-08-26 17:43:49 -0400

    select_cdrom_window: Populate the combo box.
    The combo box's entry field now contains the current CD-ROM drive setting,
    and the dropdown is populated with all CD-ROM devices found in /dev.
    
    TODO: Use a better way to find the CD-ROM devices.

--- src/gens/ui/gtk/select_cdrom/select_cdrom_window_misc.c
+++ src/gens/ui/gtk/select_cdrom/select_cdrom_window_misc.c
@@ -4,6 +4,7 @@
 
 
 #include <string.h>
+#include <sys/stat.h>
 
 #include "select_cdrom_window.h"
 #include "select_cdrom_window_callbacks.h"
@@ -13,6 +14,11 @@
 #include <gtk/gtk.h>
 #include "gtk-misc.h"
 
+#include "rom.h"
+
+
+// CD-ROM device prefixes.
+const char* CDROM_Prefix[4] = {"/dev/cdrom", "/dev/scd", "/dev/sr", NULL};
 
 /**
  * Open_Select_CDROM(): Opens the Select CD-ROM window.
@@ -20,6 +26,9 @@
 void Open_Select_CDROM(void)
 {
 	GtkWidget *SelCD;
+	GtkWidget *combo_drive;
+	int curPrefix, i; char tmp[64];
+	struct stat fileStat;
 	
 	SelCD = create_select_cdrom_window();
 	if (!SelCD)
@@ -31,7 +40,47 @@ void Open_Select_CDROM(void)
 	gtk_window_set_transient_for(GTK_WINDOW(SelCD), GTK_WINDOW(gens_window));
 	
 	// Load settings.
-	// TODO
+	
+	// CD-ROM drive
+	combo_drive = lookup_widget(SelCD, "combo_drive");
+	gtk_entry_set_text(GTK_ENTRY(GTK_BIN(combo_drive)->child), CDROM_DEV);
+	
+	// Add CD-ROM drives to the dropdown.
+	// TODO: Improve this using udev or something.
+	
+	curPrefix = 0;
+	while (CDROM_Prefix[curPrefix])
+	{
+		for (i = -1; i <= 9; i++)
+		{
+			if (i == -1)
+				strcpy(tmp, CDROM_Prefix[curPrefix]);
+			else
+				sprintf(tmp, "%s%d", CDROM_Prefix[curPrefix], i);
+				
+			if (lstat(tmp, &fileStat))
+			{
+				// Error occurred while stat'ing this file. Skip it.
+				continue;
+			}
+			
+			// File exists.
+			
+			// If this isn't "/dev/cdrom*", don't add it if it's a symlink.
+			if (curPrefix != 0 && S_ISLNK(fileStat.st_mode))
+				continue;
+			
+			// If this is neither a symlink nor a block device, don't add it.
+			if (!S_ISLNK(fileStat.st_mode) && !S_ISBLK(fileStat.st_mode))
+				continue;
+			
+			// Add the device file.
+			gtk_combo_box_append_text(GTK_COMBO_BOX(combo_drive), tmp);
+		}
+		
+		// Next prefix.
+		curPrefix++;
+	}
 	
 	// Show the Select CD-ROM window.
 	gtk_widget_show_all(SelCD);
