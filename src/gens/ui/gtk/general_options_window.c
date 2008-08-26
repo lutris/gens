8a1fe4e38a512279ec3f531317fcea4037e2cb9d
David Korth <david@gs_laptop.gerbilsoft.gsft>
2008-08-25 20:24:36 -0400

    More controls in the "General Options" window.
    I'm using GtkRadioButtons for the color selection. I wrote a static
    function to automate generating these radio buttons, since they
    will be used at leat 3 times.

--- src/gens/ui/gtk/general_options_window.c
+++ src/gens/ui/gtk/general_options_window.c
@@ -35,16 +35,16 @@
 #include "rom.h"
 
 
-// Macro to create a frame (with label) and add it toa a container.
-#define CREATE_CONTAINER_FRAME(FrameWidget, FrameName, Container,		\
-			       LabelWidget, LabelName, Caption)			\
+// Macro to create a frame (with label) and add it toa a box.
+#define CREATE_BOX_FRAME(FrameWidget, FrameName, Box,				\
+			 LabelWidget, LabelName, Caption)			\
 {										\
 	FrameWidget = gtk_frame_new(NULL);					\
 	gtk_widget_set_name(FrameWidget, FrameName);				\
 	gtk_container_set_border_width(GTK_CONTAINER(FrameWidget), 5);		\
 	gtk_frame_set_shadow_type(GTK_FRAME(FrameWidget), GTK_SHADOW_NONE);	\
 	gtk_widget_show(FrameWidget);						\
-	gtk_container_add(GTK_CONTAINER(Container), FrameWidget);		\
+	gtk_box_pack_start(GTK_BOX(Box), FrameWidget, TRUE, TRUE, 0);	\
 	GLADE_HOOKUP_OBJECT(general_options_window, FrameWidget, FrameName);	\
 										\
 	LabelWidget = gtk_label_new(Caption);					\
@@ -90,6 +90,21 @@ GtkWidget *general_options_window = NULL;
 
 GtkAccelGroup *accel_group;
 
+// Message colors.
+const char* MsgColors[9] =
+{
+	"White", "#FFFFFF",
+	"Blue",  "#FF0000",
+	"Green", "#00FF00",
+	"Red",   "#0000FF",
+	NULL,
+};
+
+static void create_color_radio_buttons(const char* groupName, 
+				       const char** colors,
+				       const int num,
+				       GtkWidget* container);
+
 
 /**
  * create_general_options_window(): Create the General Options Window.
@@ -102,6 +117,10 @@ GtkWidget* create_general_options_window(void)
 	GtkWidget *frame_system, *label_system, *table_system;
 	GtkWidget *check_system_autofixchecksum, *check_system_autopause;
 	GtkWidget *check_system_fastblur, *check_system_segacd_leds;
+	GtkWidget *frame_fps, *label_fps, *table_fps;
+	GtkWidget *check_fps_enable, *check_fps_doublesized;
+	GtkWidget *check_fps_transparency;
+	GtkWidget *hbox_fps_color;
 	
 	if (general_options_window)
 	{
@@ -132,8 +151,8 @@ GtkWidget* create_general_options_window(void)
 	GLADE_HOOKUP_OBJECT(general_options_window, vbox_go, "vbox_go");
 	
 	// System frame
-	CREATE_CONTAINER_FRAME(frame_system, "frame_system", vbox_go,
-			       label_system, "label_system", "<b><i>System</i></b>");
+	CREATE_BOX_FRAME(frame_system, "frame_system", vbox_go,
+			 label_system, "label_system", "<b><i>System</i></b>");
 	
 	// System table
 	CREATE_CONTAINER_TABLE(table_system, "table_system", frame_system, 2, 2, 47);
@@ -154,6 +173,90 @@ GtkWidget* create_general_options_window(void)
 	CREATE_TABLE_CHECKBOX(check_system_segacd_leds, "check_system_segacd_leds",
 			      "Show SegaCD LEDs", table_system, 1, 2, 1, 2);
 	
+	// FPS frame
+	CREATE_BOX_FRAME(frame_fps, "frame_fps", vbox_go,
+			 label_fps, "label_fps", "<b><i>FPS counter</i></b>");
+	
+	// FPS table
+	CREATE_CONTAINER_TABLE(table_fps, "table_fps", frame_fps, 2, 2, 91);
+	
+	// FPS enable
+	CREATE_TABLE_CHECKBOX(check_fps_enable, "check_fps_enable",
+			      "Enable", table_fps, 0, 1, 0, 1);
+	
+	// FPS double sized
+	CREATE_TABLE_CHECKBOX(check_fps_doublesized, "check_fps_doublesized",
+			      "Double Sized", table_fps, 1, 2, 0, 1);
+	
+	// FPS transparency
+	CREATE_TABLE_CHECKBOX(check_fps_transparency, "check_fps_transparency",
+			      "Transparency", table_fps, 0, 1, 1, 2);
+	
+	// FPS color
+	hbox_fps_color = gtk_hbox_new(FALSE, 5);
+	gtk_widget_set_name(hbox_fps_color, "hbox_fps_color");
+	gtk_widget_show(hbox_fps_color);
+	gtk_table_attach(GTK_TABLE(table_fps), hbox_fps_color, 1, 2, 1, 2,
+			 (GtkAttachOptions)(GTK_FILL),
+			 (GtkAttachOptions)(GTK_FILL), 0, 0);
+	GLADE_HOOKUP_OBJECT(general_options_window, hbox_fps_color, "hbox_fps_color");
+	
+	// FPS Colors
+	create_color_radio_buttons("fps_color", MsgColors, 4, hbox_fps_color);
+	
 	gtk_widget_show_all(general_options_window);
 	return general_options_window;
+}
+
+
+/**
+ * create_color_radio_buttons(): Create color radiobuttons.
+ * @param groupName Prefix for each button's name.
+ * @param colors Array of colors.
+ * @param num Number of colors to use.
+ * @param container Container for the radio buttons.
+ */
+static void create_color_radio_buttons(const char* groupName, 
+				       const char** colors,
+				       const int num,
+				       GtkWidget* container)
+{
+	GtkWidget *label_color, *hbutton_box_color, *button_color;
+	GSList *color_group = NULL;
+	GdkColor tmpColor; int i;
+	char tmp[64];
+	
+	// Color label
+	sprintf(tmp, "label_%s", groupName);
+	label_color = gtk_label_new("Color:");
+	gtk_widget_set_name(label_color, tmp);
+	gtk_widget_show(label_color);
+	gtk_box_pack_start(GTK_BOX(container), label_color, TRUE, TRUE, 0);
+	GLADE_HOOKUP_OBJECT(general_options_window, label_color, tmp);
+	
+	// HButtonBox
+	sprintf(tmp, "hbutton_box_%s", groupName);
+	hbutton_box_color = gtk_hbutton_box_new();
+	gtk_widget_set_name(hbutton_box_color, tmp);
+	gtk_widget_show(hbutton_box_color);
+	gtk_box_pack_start(GTK_BOX(container), hbutton_box_color, TRUE, TRUE, 0);
+	GLADE_HOOKUP_OBJECT(general_options_window, hbutton_box_color, tmp);
+	
+	// Color Buttons
+	for (i = 0; i < num; i++)
+	{
+		if (!MsgColors[i * 2])
+			return;
+		
+		sprintf(tmp, "%s_%s", groupName, colors[i * 2]);
+		button_color = gtk_radio_button_new(color_group);
+		color_group = gtk_radio_button_get_group(GTK_RADIO_BUTTON(button_color));
+		gtk_widget_set_name(button_color, tmp);
+		gdk_color_parse(colors[(i * 2) + 1], &tmpColor);
+		gtk_widget_modify_bg(button_color, GTK_STATE_NORMAL, &tmpColor);
+		gtk_toggle_button_set_mode(GTK_TOGGLE_BUTTON(button_color), FALSE);
+		gtk_widget_show(button_color);
+		gtk_box_pack_start(GTK_BOX(hbutton_box_color), button_color, TRUE, TRUE, 0);
+		GLADE_HOOKUP_OBJECT(general_options_window, button_color, tmp);
+	}	
 }
