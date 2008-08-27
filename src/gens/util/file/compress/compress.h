e76185727e4d10064807527de676053b8d276517
David Korth <gerbilsoft@verizon.net>
2008-08-27 00:55:04 -0400

    Initial implementation of the new compression file handler.
    The new compression file handler currently implements three functions
    for each type of compressed file. By using the new compression file
    handler, support for more compression formats can be added without
    overcomplicating the rest of the Gens code.

---
+++ src/gens/util/file/compress/compress.h
@@ -0,0 +1,81 @@
+/***************************************************************************
+ * Gens: File Compression Function Definitions.                            *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville                       *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun                              *
+ * Copyright (c) 2008 by David Korth                                       *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+/***************************************************************************
+ * This file defines various function prototypes that are required
+ * to be implemented by file compression handlers. These function
+ * pointers are then stored in a struct that is then used by
+ * the ROM loading code to identify and decompress ROM images.
+ ***************************************************************************/
+
+#ifndef GENS_COMPRESSION_H
+#define GENS_COMPRESSION_H
+
+#include <stdio.h>
+
+// Function pointer calling conventions from SDL's begin_code.h
+#ifndef GENS_FNCALL
+#if defined(__WIN32__) && !defined(__GNUC__)
+#define GENS_FNCALL __cdecl
+#else
+#ifdef __OS2__
+#define GENS_FNCALL _System
+#else
+#define GENS_FNCALL
+#endif
+#endif
+#endif
+
+/**
+ * COMPRESS_Detect_Format(): Detects if a file is in this compression handler's format.
+ * @param f File pointer of the file to check.
+ * @return 0 if this is not in this handler's format; non-zero if it is.
+ */
+typedef int (GENS_FNCALL *COMPRESS_Detect_Format)(FILE *f);
+
+/**
+ * COMPRESS_Get_First_File_Size(): Gets the filesize of the first file in the specified archive.
+ * @param f File pointer of the archive.
+ * @return Filesize, or 0 on error.
+ */
+typedef int (GENS_FNCALL *COMPRESS_Get_First_File_Size)(FILE *f);
+
+/**
+ * COMPRESS_Get_First_File(): Gets the first file from the specified archive.
+ * @param f File pointer of the archive.
+ * @param buf Buffer to write the file to.
+ * @param size Size of the buffer, in bytes.
+ * @return Number of bytes read, or -1 on error.
+ */
+typedef int (GENS_FNCALL *COMPRESS_Get_First_File)(FILE *f, void *buf, int size);
+
+/**
+ * COMPRESS_Handler(): Struct containing function pointers for compression handlers.
+ */
+struct COMPRESS_Handler
+{
+	COMPRESS_Detect_Format		detect_format;
+	COMPRESS_Get_First_File_Size	get_first_file_size;
+	COMPRESS_Get_First_File		get_first_file;
+};
+
+#endif
---
+++ src/gens/util/file/compress/gzip.c
@@ -0,0 +1,97 @@
+/***************************************************************************
+ * Gens: GZip File Compression Handler.                                    *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville                       *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun                              *
+ * Copyright (c) 2008 by David Korth                                       *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#include <zlib.h>
+#include "compress.h"
+
+/**
+ * GZip_Detect_Format(): Detects if a file is in GZip format.
+ * @param f File pointer of the file to check.
+ * @return 0 if this is not in GZip format; non-zero if it is.
+ */
+int GZip_Detect_Format(FILE *f)
+{
+	// Magic Number for GZip:
+	// First two bytes: "\x1F\x8B"
+	
+	unsigned char buf[2];
+	printf("1\n");
+	fseek(f, 0, SEEK_SET);
+	printf("2\n");
+	fread(buf, 2, sizeof(unsigned char), f);
+	printf("3\n");
+	return (buf[0] == 0x1F && buf[1] == 0x8B);
+}
+
+/**
+ * GZIP_Get_First_File_Size(): Gets the filesize of the first file in the specified archive.
+ * @param f File pointer of the archive.
+ * @return Filesize, or 0 on error.
+ */
+int GZip_Get_First_File_Size(FILE *f)
+{
+	gzFile gzfd;
+	char buf[1024];
+	int filesize = 0;
+	
+	gzfd = gzdopen(dup(fileno(f)), "rb");
+	if (!gzfd)
+	{
+		// Error obtaining a GZip file descriptor.
+		return 0;
+	}
+	
+	// Read through the GZip file until we hit an EOF.
+	while (!gzeof(gzfd))
+	{
+		filesize += gzread(gzfd, buf, 1024);
+	}
+	
+	// Close the GZip fd and return the filesize.
+	gzclose(gzfd);
+	return filesize;
+}
+
+/**
+ * GZip_Get_First_File(): Gets the first file from the specified archive.
+ * @param f File pointer of the archive.
+ * @param buf Buffer to write the file to.
+ * @param size Size of the buffer, in bytes.
+ * @return Number of bytes read, or -1 on error.
+ */
+int GZip_Get_First_File(FILE *f, void *buf, int size)
+{
+	gzFile gzfd;
+	int retval;
+	
+	gzfd = gzdopen(dup(fileno(f)), "rb");
+	if (!gzfd)
+	{
+		// Error obtaining a GZip file descriptor.
+		return -1;
+	}
+	
+	// Decompress the GZip file into memory.
+	retval = gzread(gzfd, buf, size);
+	gzclose(gzfd);
+	return retval;
+}
