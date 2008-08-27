be509224774984353f42f9961eb743b51b7e3dbd
David Korth <gerbilsoft@verizon.net>
2008-08-27 02:00:08 -0400

    gzip.c: #include reordering.

--- src/gens/util/file/compress/gzip.c
+++ src/gens/util/file/compress/gzip.c
@@ -20,8 +20,8 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-#include <zlib.h>
 #include "compress.h"
+#include <zlib.h>
 
 /**
  * GZip_Detect_Format(): Detects if a file is in GZip format.
