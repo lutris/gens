554371602f173f1aaba82f18e6997968db4af174
David Korth <gerbilsoft@verizon.net>
2008-08-27 21:07:36 -0400

    scrshot.c: Added PNG screenshot support.
    TODO: dlopen() libpng so it doesn't have to be statically linked.
    Also, add a window to select PNG vs. BMP and other image options.

--- src/gens/util/gfx/scrshot.c
+++ src/gens/util/gfx/scrshot.c
@@ -9,14 +9,30 @@
 #include "vdp_rend.h"
 #include "g_sdldraw.h"
 #include "byteswap.h"
+#include <png.h>
 
 char ScrShot_Dir[GENS_PATH_MAX] = "." G_DIR_SEPARATOR_S;
 
+static int Save_Shot_BMP(void);
+static int Save_Shot_PNG(void);
+
 /**
- * Save_Shot(): Save a screenshot.
+ * Save_Shot(): Saves a screenshot.
  * @return 1 on success; 0 on error.
  */
-int Save_Shot(void)
+int Save_Shot()
+{
+	// TODO: dlopen() libpng.
+	// If it's found, save as PNG; otherwise, BMP.
+	Save_Shot_PNG();
+}
+
+
+/**
+ * Save_Shot_BMP(): Save a screenshot in BMP format.
+ * @return 1 on success; 0 on error.
+ */
+static int Save_Shot_BMP(void)
 {
 	FILE *ScrShot_File = 0;
 	unsigned char *Dest = NULL;
@@ -134,6 +150,163 @@ int Save_Shot(void)
 	{
 		free(Dest);
 		Dest = NULL;
+	}
+	
+	MESSAGE_NUM_L("Screen shot %d saved", "Screen shot %d saved", num, 1500);
+	
+	return 1;
+}
+
+
+/**
+ * Save_Shot_PNG(): Save a screenshot in PNG format.
+ * @return 1 on success; 0 on error.
+ */
+static int Save_Shot_PNG(void)
+{
+	FILE *ScrShot_File = 0;
+	int num;
+	char filename[GENS_PATH_MAX];
+	struct stat sbuf;
+	png_structp png_ptr;
+	png_infop info_ptr;
+	unsigned char *rowBuffer;
+	unsigned short MD_Color;
+	
+	// Bitmap dimensions.
+	int w, h, x, y;
+	int bmpSize;
+	
+	// If no game is running, don't do anything.
+	if (!Game)
+		return 0;
+	
+	// Variables used:
+	// VDP_Num_Vis_Lines: Number of lines visible on the screen. (bitmap height)
+	// MD_Screen: MD screen buffer.
+	// VDP_Reg.Set4: If 0x01 is set, 320 pixels width; otherwise, 256 pixels width.
+	w = (VDP_Reg.Set4 & 0x01 ? 320 : 256);
+	h = VDP_Num_Vis_Lines;
+	
+	// Allocate the row buffer.
+	if ((rowBuffer = (unsigned char*)malloc(w * 3)) == NULL)
+	{
+		// Could not allocate enough memory.
+		return 0;
+	}
+	
+	// Build the filename.
+	num = -1;
+	do
+	{
+		num++;
+		sprintf(filename, "%s%s_%03d.png", ScrShot_Dir, Rom_Name, num);
+	}
+	while (!stat(filename, &sbuf));
+	
+	// Initialize libpng.
+	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (!png_ptr)
+	{
+		printf("Error initializing the PNG pointer.\n");
+		free(rowBuffer);
+		return 0;
+	}
+	info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr)
+	{
+		printf("Error initializing the PNG info pointer.\n");
+		png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+		free(rowBuffer);
+		return 0;
+	}
+	if (setjmp(png_jmpbuf(png_ptr)))
+	{
+		printf("Error initializing the PNG setjmp pointer.\n");
+		png_destroy_write_struct(&png_ptr, &info_ptr);
+		free(rowBuffer);
+		return 0;
+	}
+	
+	// Attempt to open the file.
+	if ((ScrShot_File = fopen(filename, "wb")) == 0)
+	{
+		// Error opening the file.
+		printf("Error opening the PNG file.\n");
+		png_destroy_write_struct(&png_ptr, &info_ptr);
+		free(rowBuffer);
+		return 0;
+	}
+	
+	// Initialize libpng I/O.
+	png_init_io(png_ptr, ScrShot_File);
+	
+	// Disable PNG filters.
+	png_set_filter(png_ptr, 0, PNG_FILTER_NONE);
+	
+	// Set the compression level to 5. (Levels range from 1 through 9.)
+	// TODO: Add a UI option to set compression level.
+	png_set_compression_level(png_ptr, 5);
+	
+	// Set up the PNG header.
+	png_set_IHDR(png_ptr, info_ptr, w, h, 8, PNG_COLOR_TYPE_RGB,
+		     PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT,
+		     PNG_FILTER_TYPE_DEFAULT);
+	
+	// Write the PNG information to the file.
+	png_write_info(png_ptr, info_ptr);
+	
+	// 16-bit (565) functions.
+	
+#ifdef GENS_LIL_ENDIAN
+	// PNG stores data in big-endian format.
+	// On little-endian systems, byteswapping needs to be enabled.
+	png_set_swap(png_ptr);
+#endif
+	
+	// Write the image.
+	if (!Mode_555)
+	{
+		// 16-bit color, 565 pixel format.
+		for (y = 0; y < h; y++)
+		{
+			for (x = 0; x < w; x++)
+			{
+				MD_Color = MD_Screen[(y * 336) + x + 8];
+				rowBuffer[(x * 3) + 0] = (unsigned char)((MD_Color & 0xF800) >> 8);
+				rowBuffer[(x * 3) + 1] = (unsigned char)((MD_Color & 0x07E0) >> 3);
+				rowBuffer[(x * 3) + 2] = (unsigned char)((MD_Color & 0x001F) << 3);
+			}
+			// Write the row.
+			png_write_row(png_ptr, rowBuffer);
+		}
+	}
+	else
+	{
+		// 16-bit color, 555 pixel format.
+		for (y = 0; y < h; y++)
+		{
+			for (x = 0; x < w; x++)
+			{
+				MD_Color = MD_Screen[(y * 336) + x + 8];
+				rowBuffer[(x * 3) + 0] = (unsigned char)((MD_Color & 0x7C00) >> 7);
+				rowBuffer[(x * 3) + 1] = (unsigned char)((MD_Color & 0x03E0) >> 2);
+				rowBuffer[(x * 3) + 2] = (unsigned char)((MD_Color & 0x001F) << 3);
+			}
+			// Write the row.
+			png_write_row(png_ptr, rowBuffer);
+		}
+	}
+	
+	// Finished writing.
+	png_write_end(png_ptr, info_ptr);
+	png_destroy_write_struct(&png_ptr, &info_ptr);
+	fclose(ScrShot_File);
+	
+	if (rowBuffer)
+	{
+		free(rowBuffer);
+		rowBuffer = NULL;
 	}
 	
 	MESSAGE_NUM_L("Screen shot %d saved", "Screen shot %d saved", num, 1500);
