f0a92840c8f488da72ad7a510477df0a846661ba
David Korth <gerbilsoft@verizon.net>
2008-08-31 06:38:18 -0400

    io.c, io.h: Cleaned up controller handling a bit.
    Added macros to define all the variables. This will be used until
    the bitfield conversion is finished.

--- src/gens/gens_core/io/io.c
+++ src/gens/gens_core/io/io.c
@@ -6,151 +6,36 @@
 
 #include "io.h"
 
-unsigned int Controller_1_Type;
-unsigned int Controller_1_State;
-unsigned int Controller_1_COM;
-unsigned int Controller_1_Counter;
-unsigned int Controller_1_Delay;
-unsigned int Controller_1_Up;
-unsigned int Controller_1_Down;
-unsigned int Controller_1_Left;
-unsigned int Controller_1_Right;
-unsigned int Controller_1_Start;
-unsigned int Controller_1_Mode;
-unsigned int Controller_1_A;
-unsigned int Controller_1_B;
-unsigned int Controller_1_C;
-unsigned int Controller_1_X;
-unsigned int Controller_1_Y;
-unsigned int Controller_1_Z;
+#define CREATE_CONTROLLER_VARIABLES(player)		\
+	unsigned int Controller_ ## player ## _Type;	\
+	unsigned int Controller_ ## player ## _State;	\
+	unsigned int Controller_ ## player ## _COM;	\
+	unsigned int Controller_ ## player ## _Counter;	\
+	unsigned int Controller_ ## player ## _Delay;	\
+	unsigned int Controller_ ## player ## _Up;	\
+	unsigned int Controller_ ## player ## _Down;	\
+	unsigned int Controller_ ## player ## _Left;	\
+	unsigned int Controller_ ## player ## _Right;	\
+	unsigned int Controller_ ## player ## _Start;	\
+	unsigned int Controller_ ## player ## _Mode;	\
+	unsigned int Controller_ ## player ## _A;	\
+	unsigned int Controller_ ## player ## _B;	\
+	unsigned int Controller_ ## player ## _C;	\
+	unsigned int Controller_ ## player ## _X;	\
+	unsigned int Controller_ ## player ## _Y;	\
+	unsigned int Controller_ ## player ## _Z;	\
 
-unsigned int Controller_1B_Type;
-unsigned int Controller_1B_State;
-unsigned int Controller_1B_COM;
-unsigned int Controller_1B_Counter;
-unsigned int Controller_1B_Delay;
-unsigned int Controller_1B_Up;
-unsigned int Controller_1B_Down;
-unsigned int Controller_1B_Left;
-unsigned int Controller_1B_Right;
-unsigned int Controller_1B_Start;
-unsigned int Controller_1B_Mode;
-unsigned int Controller_1B_A;
-unsigned int Controller_1B_B;
-unsigned int Controller_1B_C;
-unsigned int Controller_1B_X;
-unsigned int Controller_1B_Y;
-unsigned int Controller_1B_Z;
+CREATE_CONTROLLER_VARIABLES(1);
+CREATE_CONTROLLER_VARIABLES(1B);
+CREATE_CONTROLLER_VARIABLES(1C);
+CREATE_CONTROLLER_VARIABLES(1D);
 
-unsigned int Controller_1C_Type;
-unsigned int Controller_1C_State;
-unsigned int Controller_1C_COM;
-unsigned int Controller_1C_Counter;
-unsigned int Controller_1C_Delay;
-unsigned int Controller_1C_Up;
-unsigned int Controller_1C_Down;
-unsigned int Controller_1C_Left;
-unsigned int Controller_1C_Right;
-unsigned int Controller_1C_Start;
-unsigned int Controller_1C_Mode;
-unsigned int Controller_1C_A;
-unsigned int Controller_1C_B;
-unsigned int Controller_1C_C;
-unsigned int Controller_1C_X;
-unsigned int Controller_1C_Y;
-unsigned int Controller_1C_Z;
+CREATE_CONTROLLER_VARIABLES(2);
+CREATE_CONTROLLER_VARIABLES(2B);
+CREATE_CONTROLLER_VARIABLES(2C);
+CREATE_CONTROLLER_VARIABLES(2D);
 
-unsigned int Controller_1D_Type;
-unsigned int Controller_1D_State;
-unsigned int Controller_1D_COM;
-unsigned int Controller_1D_Counter;
-unsigned int Controller_1D_Delay;
-unsigned int Controller_1D_Up;
-unsigned int Controller_1D_Down;
-unsigned int Controller_1D_Left;
-unsigned int Controller_1D_Right;
-unsigned int Controller_1D_Start;
-unsigned int Controller_1D_Mode;
-unsigned int Controller_1D_A;
-unsigned int Controller_1D_B;
-unsigned int Controller_1D_C;
-unsigned int Controller_1D_X;
-unsigned int Controller_1D_Y;
-unsigned int Controller_1D_Z;
-
-unsigned int Controller_2_Type;
-unsigned int Controller_2_State;
-unsigned int Controller_2_COM;
-unsigned int Controller_2_Counter;
-unsigned int Controller_2_Delay;
-unsigned int Controller_2_Up;
-unsigned int Controller_2_Down;
-unsigned int Controller_2_Left;
-unsigned int Controller_2_Right;
-unsigned int Controller_2_Start;
-unsigned int Controller_2_Mode;
-unsigned int Controller_2_A;
-unsigned int Controller_2_B;
-unsigned int Controller_2_C;
-unsigned int Controller_2_X;
-unsigned int Controller_2_Y;
-unsigned int Controller_2_Z;
-
-unsigned int Controller_2B_Type;
-unsigned int Controller_2B_State;
-unsigned int Controller_2B_COM;
-unsigned int Controller_2B_Counter;
-unsigned int Controller_2B_Delay;
-unsigned int Controller_2B_Up;
-unsigned int Controller_2B_Down;
-unsigned int Controller_2B_Left;
-unsigned int Controller_2B_Right;
-unsigned int Controller_2B_Start;
-unsigned int Controller_2B_Mode;
-unsigned int Controller_2B_A;
-unsigned int Controller_2B_B;
-unsigned int Controller_2B_C;
-unsigned int Controller_2B_X;
-unsigned int Controller_2B_Y;
-unsigned int Controller_2B_Z;
-
-unsigned int Controller_2C_Type;
-unsigned int Controller_2C_State;
-unsigned int Controller_2C_COM;
-unsigned int Controller_2C_Counter;
-unsigned int Controller_2C_Delay;
-unsigned int Controller_2C_Up;
-unsigned int Controller_2C_Down;
-unsigned int Controller_2C_Left;
-unsigned int Controller_2C_Right;
-unsigned int Controller_2C_Start;
-unsigned int Controller_2C_Mode;
-unsigned int Controller_2C_A;
-unsigned int Controller_2C_B;
-unsigned int Controller_2C_C;
-unsigned int Controller_2C_X;
-unsigned int Controller_2C_Y;
-unsigned int Controller_2C_Z;
-
-unsigned int Controller_2D_Type;
-unsigned int Controller_2D_State;
-unsigned int Controller_2D_COM;
-unsigned int Controller_2D_Counter;
-unsigned int Controller_2D_Delay;
-unsigned int Controller_2D_Up;
-unsigned int Controller_2D_Down;
-unsigned int Controller_2D_Left;
-unsigned int Controller_2D_Right;
-unsigned int Controller_2D_Start;
-unsigned int Controller_2D_Mode;
-unsigned int Controller_2D_A;
-unsigned int Controller_2D_B;
-unsigned int Controller_2D_C;
-unsigned int Controller_2D_X;
-unsigned int Controller_2D_Y;
-unsigned int Controller_2D_Z;
-
-// Jump Table: Table_Cont (Controller status)
+// Select Line status
 enum SelectLine
 {
 	First_Low	= 0x00,
@@ -176,76 +61,52 @@ enum SelectLine
 #define CONTROLLER_Y		0x00000400
 #define CONTROLLER_Z		0x00000800
 
-// Controller bitfield format:
-// 0000 0000 0000 0000 0000 ZYXM CBAS RLDU
 
 static unsigned char RD_Controller(unsigned int state,
 				   unsigned int type,
 				   unsigned int counter,
 				   unsigned int buttons[4]);
 
+
+#define CREATE_CONTROLLER_BITFIELD(bitfield, player)				\
+{										\
+	bitfield |= CONTROLLER_UP	* Controller_ ## player ## _Up;		\
+	bitfield |= CONTROLLER_DOWN	* Controller_ ## player ## _Down;	\
+	bitfield |= CONTROLLER_LEFT	* Controller_ ## player ## _Left;	\
+	bitfield |= CONTROLLER_RIGHT	* Controller_ ## player ## _Right;	\
+	bitfield |= CONTROLLER_START	* Controller_ ## player ## _Start;	\
+	bitfield |= CONTROLLER_A	* Controller_ ## player ## _A;		\
+	bitfield |= CONTROLLER_B	* Controller_ ## player ## _B;		\
+	bitfield |= CONTROLLER_C	* Controller_ ## player ## _C;		\
+	bitfield |= CONTROLLER_MODE	* Controller_ ## player ## _Mode;	\
+	bitfield |= CONTROLLER_X	* Controller_ ## player ## _X;		\
+	bitfield |= CONTROLLER_Y	* Controller_ ## player ## _Y;		\
+	bitfield |= CONTROLLER_Z	* Controller_ ## player ## _Z;		\
+}
+
+
+void Make_IO_Table(void)
+{
+}
+
+
 unsigned char RD_Controller_1(void)
 {
 	// Read controller 1.
 	
+	if (Controller_1_Type & 0x10)
+	{
+		// TeamPlayer.
+		//return RD_Controller_TP1();
+	}
+	
 	// Create the bitfields.
+	// TODO: This will be unnecessary when controllers are converted to bitfields.
 	unsigned int buttons[4] = {0, 0, 0, 0};
-	
-	// Player A
-	buttons[0] |= CONTROLLER_UP	* Controller_1_Up;
-	buttons[0] |= CONTROLLER_DOWN	* Controller_1_Down;
-	buttons[0] |= CONTROLLER_LEFT	* Controller_1_Left;
-	buttons[0] |= CONTROLLER_RIGHT	* Controller_1_Right;
-	buttons[0] |= CONTROLLER_START	* Controller_1_Start;
-	buttons[0] |= CONTROLLER_A	* Controller_1_A;
-	buttons[0] |= CONTROLLER_B	* Controller_1_B;
-	buttons[0] |= CONTROLLER_C	* Controller_1_C;
-	buttons[0] |= CONTROLLER_MODE	* Controller_1_Mode;
-	buttons[0] |= CONTROLLER_X	* Controller_1_X;
-	buttons[0] |= CONTROLLER_Y	* Controller_1_Y;
-	buttons[0] |= CONTROLLER_Z	* Controller_1_Z;
-	
-	// Player B
-	buttons[1] |= CONTROLLER_UP	* Controller_1B_Up;
-	buttons[1] |= CONTROLLER_DOWN	* Controller_1B_Down;
-	buttons[1] |= CONTROLLER_LEFT	* Controller_1B_Left;
-	buttons[1] |= CONTROLLER_RIGHT	* Controller_1B_Right;
-	buttons[1] |= CONTROLLER_START	* Controller_1B_Start;
-	buttons[1] |= CONTROLLER_A	* Controller_1B_A;
-	buttons[1] |= CONTROLLER_B	* Controller_1B_B;
-	buttons[1] |= CONTROLLER_C	* Controller_1B_C;
-	buttons[1] |= CONTROLLER_MODE	* Controller_1B_Mode;
-	buttons[1] |= CONTROLLER_X	* Controller_1B_X;
-	buttons[1] |= CONTROLLER_Y	* Controller_1B_Y;
-	buttons[1] |= CONTROLLER_Z	* Controller_1B_Z;
-	
-	// Player C
-	buttons[2] |= CONTROLLER_UP	* Controller_1C_Up;
-	buttons[2] |= CONTROLLER_DOWN	* Controller_1C_Down;
-	buttons[2] |= CONTROLLER_LEFT	* Controller_1C_Left;
-	buttons[2] |= CONTROLLER_RIGHT	* Controller_1C_Right;
-	buttons[2] |= CONTROLLER_START	* Controller_1C_Start;
-	buttons[2] |= CONTROLLER_A	* Controller_1C_A;
-	buttons[2] |= CONTROLLER_B	* Controller_1C_B;
-	buttons[2] |= CONTROLLER_C	* Controller_1C_C;
-	buttons[2] |= CONTROLLER_MODE	* Controller_1C_Mode;
-	buttons[2] |= CONTROLLER_X	* Controller_1C_X;
-	buttons[2] |= CONTROLLER_Y	* Controller_1C_Y;
-	buttons[2] |= CONTROLLER_Z	* Controller_1C_Z;
-	
-	// Player D
-	buttons[3] |= CONTROLLER_UP	* Controller_1D_Up;
-	buttons[3] |= CONTROLLER_DOWN	* Controller_1D_Down;
-	buttons[3] |= CONTROLLER_LEFT	* Controller_1D_Left;
-	buttons[3] |= CONTROLLER_RIGHT	* Controller_1D_Right;
-	buttons[3] |= CONTROLLER_START	* Controller_1D_Start;
-	buttons[3] |= CONTROLLER_A	* Controller_1D_A;
-	buttons[3] |= CONTROLLER_B	* Controller_1D_B;
-	buttons[3] |= CONTROLLER_C	* Controller_1D_C;
-	buttons[3] |= CONTROLLER_MODE	* Controller_1D_Mode;
-	buttons[3] |= CONTROLLER_X	* Controller_1D_X;
-	buttons[3] |= CONTROLLER_Y	* Controller_1D_Y;
-	buttons[3] |= CONTROLLER_Z	* Controller_1D_Z;
+	CREATE_CONTROLLER_BITFIELD(buttons[0], 1);
+	CREATE_CONTROLLER_BITFIELD(buttons[1], 1B);
+	CREATE_CONTROLLER_BITFIELD(buttons[2], 1C);
+	CREATE_CONTROLLER_BITFIELD(buttons[3], 1D);
 	
 	// Read the controller data.
 	return RD_Controller(Controller_1_State, Controller_1_Type,
@@ -255,66 +116,15 @@ unsigned char RD_Controller_1(void)
 
 unsigned char RD_Controller_2(void)
 {
-	// Read controller 1.
+	// Read controller 2.
 	
 	// Create the bitfields.
+	// TODO: This will be unnecessary when controllers are converted to bitfields.
 	unsigned int buttons[4] = {0, 0, 0, 0};
-	
-	// Player A
-	buttons[0] |= CONTROLLER_UP	* Controller_2_Up;
-	buttons[0] |= CONTROLLER_DOWN	* Controller_2_Down;
-	buttons[0] |= CONTROLLER_LEFT	* Controller_2_Left;
-	buttons[0] |= CONTROLLER_RIGHT	* Controller_2_Right;
-	buttons[0] |= CONTROLLER_START	* Controller_2_Start;
-	buttons[0] |= CONTROLLER_A	* Controller_2_A;
-	buttons[0] |= CONTROLLER_B	* Controller_2_B;
-	buttons[0] |= CONTROLLER_C	* Controller_2_C;
-	buttons[0] |= CONTROLLER_MODE	* Controller_2_Mode;
-	buttons[0] |= CONTROLLER_X	* Controller_2_X;
-	buttons[0] |= CONTROLLER_Y	* Controller_2_Y;
-	buttons[0] |= CONTROLLER_Z	* Controller_2_Z;
-	
-	// Player B
-	buttons[1] |= CONTROLLER_UP	* Controller_2B_Up;
-	buttons[1] |= CONTROLLER_DOWN	* Controller_2B_Down;
-	buttons[1] |= CONTROLLER_LEFT	* Controller_2B_Left;
-	buttons[1] |= CONTROLLER_RIGHT	* Controller_2B_Right;
-	buttons[1] |= CONTROLLER_START	* Controller_2B_Start;
-	buttons[1] |= CONTROLLER_A	* Controller_2B_A;
-	buttons[1] |= CONTROLLER_B	* Controller_2B_B;
-	buttons[1] |= CONTROLLER_C	* Controller_2B_C;
-	buttons[1] |= CONTROLLER_MODE	* Controller_2B_Mode;
-	buttons[1] |= CONTROLLER_X	* Controller_2B_X;
-	buttons[1] |= CONTROLLER_Y	* Controller_2B_Y;
-	buttons[1] |= CONTROLLER_Z	* Controller_2B_Z;
-	
-	// Player C
-	buttons[2] |= CONTROLLER_UP	* Controller_2C_Up;
-	buttons[2] |= CONTROLLER_DOWN	* Controller_2C_Down;
-	buttons[2] |= CONTROLLER_LEFT	* Controller_2C_Left;
-	buttons[2] |= CONTROLLER_RIGHT	* Controller_2C_Right;
-	buttons[2] |= CONTROLLER_START	* Controller_2C_Start;
-	buttons[2] |= CONTROLLER_A	* Controller_2C_A;
-	buttons[2] |= CONTROLLER_B	* Controller_2C_B;
-	buttons[2] |= CONTROLLER_C	* Controller_2C_C;
-	buttons[2] |= CONTROLLER_MODE	* Controller_2C_Mode;
-	buttons[2] |= CONTROLLER_X	* Controller_2C_X;
-	buttons[2] |= CONTROLLER_Y	* Controller_2C_Y;
-	buttons[2] |= CONTROLLER_Z	* Controller_2C_Z;
-	
-	// Player D
-	buttons[3] |= CONTROLLER_UP	* Controller_2D_Up;
-	buttons[3] |= CONTROLLER_DOWN	* Controller_2D_Down;
-	buttons[3] |= CONTROLLER_LEFT	* Controller_2D_Left;
-	buttons[3] |= CONTROLLER_RIGHT	* Controller_2D_Right;
-	buttons[3] |= CONTROLLER_START	* Controller_2D_Start;
-	buttons[3] |= CONTROLLER_A	* Controller_2D_A;
-	buttons[3] |= CONTROLLER_B	* Controller_2D_B;
-	buttons[3] |= CONTROLLER_C	* Controller_2D_C;
-	buttons[3] |= CONTROLLER_MODE	* Controller_2D_Mode;
-	buttons[3] |= CONTROLLER_X	* Controller_2D_X;
-	buttons[3] |= CONTROLLER_Y	* Controller_2D_Y;
-	buttons[3] |= CONTROLLER_Z	* Controller_2D_Z;
+	CREATE_CONTROLLER_BITFIELD(buttons[0], 2);
+	CREATE_CONTROLLER_BITFIELD(buttons[1], 2B);
+	CREATE_CONTROLLER_BITFIELD(buttons[2], 2C);
+	CREATE_CONTROLLER_BITFIELD(buttons[3], 2D);
 	
 	// Read the controller data.
 	return RD_Controller(Controller_2_State, Controller_2_Type,
@@ -328,12 +138,6 @@ static unsigned char RD_Controller(unsigned int state,
 				   unsigned int buttons[4])
 {
 	// Read the specified controller.
-	
-	if (type & 0x10)
-	{
-		// TODO: Teamplayer
-		return 0x00;
-	}
 	
 	int SelectLine_State = ((state >> 6) & 0x01);
 	if (type & 0x01)
@@ -422,45 +226,41 @@ static unsigned char RD_Controller(unsigned int state,
 
 unsigned char WR_Controller_1(unsigned char data)
 {
-	unsigned short al = data;
-	if (Controller_1_State & 0x40)
-		goto Line1_Down;
+	if (!((Controller_1_State & 0x40) && (!(data & 0x40))))
+		Controller_1_Counter++;
 	
-	if (!(al & 0x40))
-		goto Line1_Down;
-	
-	Controller_1_Counter++;
-	
-Line1_Down:
 	Controller_1_Delay = 0;
 	if (Controller_1_Type & 0x10)
 	{
-		// TODO: Teamplayer support.
+		// Team Player adapter
+		if (!((Controller_1_State & 0x20) && (!(data & 0x20))))
+		{
+			Controller_1_Counter += (1 << 16);
+		}
 	}
-	Controller_1_State = al;
-	return (unsigned char)al;
+	
+	Controller_1_State = data;
+	return data;
 }
 
 
 unsigned char WR_Controller_2(unsigned char data)
 {
-	unsigned short al = data;
-	if (Controller_2_State & 0x40)
-		goto Line1_Down;
+	if (!((Controller_2_State & 0x40) && (!(data & 0x40))))
+		Controller_2_Counter++;
 	
-	if (!(al & 0x40))
-		goto Line1_Down;
-	
-	Controller_2_Counter++;
-	
-Line1_Down:
 	Controller_2_Delay = 0;
 	if (Controller_2_Type & 0x10)
 	{
-		// TODO: Teamplayer support.
+		// Team Player adapter
+		if (!((Controller_2_State & 0x20) && (!(data & 0x20))))
+		{
+			Controller_2_Counter += (1 << 16);
+		}
 	}
-	Controller_2_State = al;
-	return (unsigned char)al;
+	
+	Controller_2_State = data;
+	return data;
 }
 
 
@@ -491,12 +291,5 @@ Delay_1_Expired:
 Delay_2_Expired_2:
 	//Controller_2_Delay = 0;
 	Controller_2_Counter = 0;
-	return;
-}
-
-
-void Make_IO_Table(void)
-{
-	// No, we don't need an I/O table for the C version.
 	return;
 }
