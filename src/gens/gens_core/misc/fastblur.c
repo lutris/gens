f8bc0d63322444c005cc3d25c9499fe4c1559596
David Korth <gerbilsoft@verizon.net>
2008-09-01 01:50:42 -0400

    fastblur.c: Added 15-bit and 16-bit color support.

--- src/gens/gens_core/misc/fastblur.c
+++ src/gens/gens_core/misc/fastblur.c
@@ -2,9 +2,15 @@
  * Gens: Fast Blur function
  */
 
+
 #include "fastblur.h"
 #include "emulator/g_main.h"
 #include "gens_core/vdp/vdp_rend.h"
+
+
+static void Fast_Blur_16(void);
+static void Fast_Blur_32(void);
+
 
 /**
  * Fast_Blur: Apply a fast blurring algorithm to the onscreen image.
@@ -12,7 +18,57 @@
 void Fast_Blur(void)
 {
 	// TODO: Make it so fast blur doesn't apply to screenshots.
+	if (bpp == 15 || bpp == 16)
+		Fast_Blur_16();
+	else // if (bpp == 32)
+		Fast_Blur_32();
+}
+
+
+static void Fast_Blur_16(void)
+{
+	int pixel;
+	unsigned short color = 0, colorRB, colorG;
+	unsigned short blurColorRB = 0, blurColorG = 0;
 	
+	unsigned short maskRB, maskG;
+	
+	// Check bpp.
+	if (bpp == 15)
+	{
+		maskRB = 0x3C0F;
+		maskG = 0x01E0;
+	}
+	else //if (bpp == 16)
+	{
+		maskRB = 0x780F;
+		maskG = 0x03E0;
+	}
+	
+	for (pixel = 1; pixel < (336 * 240); pixel++)
+	{
+		color = MD_Screen[pixel] >> 1;
+		
+		// Split the RB and G components.
+		colorRB = color & maskRB;
+		colorG = color & maskG;
+		
+		// Blur the colors with the previous pixels.
+		blurColorRB += colorRB;
+		blurColorG += colorG;
+		
+		// Draw the new pixel.
+		MD_Screen[pixel - 1] = blurColorRB + blurColorG;
+		
+		// Save the components for the next pixel.
+		blurColorRB = colorRB;
+		blurColorG = colorG;
+	}
+}
+
+
+static void Fast_Blur_32(void)
+{
 	int pixel;
 	int color = 0, colorRB, colorG;
 	int blurColorRB = 0, blurColorG = 0;
