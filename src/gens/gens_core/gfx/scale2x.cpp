1ebb0ffe1cfebcad46826549d30afc6f9ee1a96a
David Korth <gerbilsoft@verizon.net>
2008-09-09 17:33:22 -0400

    scale2x.cpp: Converted the Scale2x renderer to C++ using templates.

--- src/gens/gens_core/gfx/scale2x.cpp
+++ src/gens/gens_core/gfx/scale2x.cpp
@@ -20,24 +20,24 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-
 #include "gens_core/vdp/vdp_rend.h"
 #include "blit.h"
 
-
 /**
- * Blit_Scale2x_16(): (16-bit) Scale2x renderer.
- * @param screen Screen buffer.
+ * Blit_Scale2x: Blits the image to the screen, 2x size, Scale2x filter.
+ * @param screen Pointer to the screen buffer.
+ * @param mdScreen Pointer to the MD screen buffer.
  * @param pitch Number of bytes per line.
  * @param x X coordinate for the image.
  * @param y Y coordinate for the image.
  * @param offset ???
  */
-void Blit_Scale2x_16(unsigned char *screen, int pitch, int x, int y, int offset)
+template<typename pixel>
+static inline void Blit_Scale2x(pixel *screen, pixel *mdScreen, int pitch, int x, int y, int offset)
 {
 	int i, j;
-	unsigned short B, D, E, F, H;
-	unsigned short E0, E1, E2, E3;
+	pixel B, D, E, F, H;
+	pixel E0, E1, E2, E3;
 	
 	// A B C
 	// D E F
@@ -45,9 +45,6 @@ void Blit_Scale2x_16(unsigned char *screen, int pitch, int x, int y, int offset)
 	
 	int ScrWdth, ScrAdd;
 	int SrcOffs, DstOffs;
-	
-	// Use an unsigned short* to make things easier.
-	unsigned short *dst = (unsigned short*)screen;
 	
 	ScrAdd = offset >> 1;
 	ScrWdth = x + ScrAdd;
@@ -57,27 +54,27 @@ void Blit_Scale2x_16(unsigned char *screen, int pitch, int x, int y, int offset)
 	
 	for (i = 0; i < y; i++)
 	{
-		E = MD_Screen[SrcOffs - 1];
-		F = MD_Screen[SrcOffs];
+		E = mdScreen[SrcOffs - 1];
+		F = mdScreen[SrcOffs];
 		
-		DstOffs = i * pitch;
+		DstOffs = i * (pitch / sizeof(pixel)) * 2;
 		
 		for (j = 0; j < x; j++)
 		{
 			D = E; E = F;
-			B = MD_Screen[SrcOffs - ScrWdth];
-			H = MD_Screen[SrcOffs + ScrWdth];
-			F = MD_Screen[++SrcOffs];
+			B = mdScreen[SrcOffs - ScrWdth];
+			H = mdScreen[SrcOffs + ScrWdth];
+			F = mdScreen[++SrcOffs];
 			E0 = D == B && B != F && D != H ? D : E;
 			E1 = B == F && B != D && F != H ? F : E;
 			E2 = D == H && D != B && H != F ? D : E;
 			E3 = H == F && D != H && B != F ? F : E;
 			
 			// Copy the new pixels.
-			dst[DstOffs + 0] = E0;
-			dst[DstOffs + 1] = E1;
-			dst[DstOffs + (pitch / 2) + 0] = E2;
-			dst[DstOffs + (pitch / 2) + 1] = E3;
+			screen[DstOffs + 0] = E0;
+			screen[DstOffs + 1] = E1;
+			screen[DstOffs + (pitch / sizeof(pixel)) + 0] = E2;
+			screen[DstOffs + (pitch / sizeof(pixel)) + 1] = E3;
 			
 			// Next group of pixels.
 			DstOffs += 2;
@@ -87,65 +84,12 @@ void Blit_Scale2x_16(unsigned char *screen, int pitch, int x, int y, int offset)
 	}
 }
 
+void Blit_Scale2x_16(unsigned char *screen, int pitch, int x, int y, int offset)
+{
+	Blit_Scale2x((unsigned short*)screen, MD_Screen, pitch, x, y, offset);
+}
 
-/**
- * Blit_Scale2x_32(): (32-bit) Scale2x renderer.
- * @param screen Screen buffer.
- * @param pitch Number of bytes per line.
- * @param x X coordinate for the image.
- * @param y Y coordinate for the image.
- * @param offset ???
- */
 void Blit_Scale2x_32(unsigned char *screen, int pitch, int x, int y, int offset)
 {
-	int i, j;
-	unsigned int B, D, E, F, H;
-	unsigned int E0, E1, E2, E3;
-	
-	// A B C
-	// D E F
-	// G H I
-	
-	int ScrWdth, ScrAdd;
-	int SrcOffs, DstOffs;
-	
-	// Use an unsigned int* to make things easier.
-	unsigned int *dst = (unsigned int*)screen;
-	
-	ScrAdd = offset >> 1;
-	ScrWdth = x + ScrAdd;
-	
-	SrcOffs = 8;
-	DstOffs = 0;
-	
-	for (i = 0; i < y; i++)
-	{
-		E = MD_Screen32[SrcOffs - 1];
-		F = MD_Screen32[SrcOffs];
-		
-		DstOffs = i * (pitch / 2);
-		
-		for (j = 0; j < x; j++)
-		{
-			D = E; E = F;
-			B = MD_Screen32[SrcOffs - ScrWdth];
-			H = MD_Screen32[SrcOffs + ScrWdth];
-			F = MD_Screen32[++SrcOffs];
-			E0 = D == B && B != F && D != H ? D : E;
-			E1 = B == F && B != D && F != H ? F : E;
-			E2 = D == H && D != B && H != F ? D : E;
-			E3 = H == F && D != H && B != F ? F : E;
-			
-			// Copy the new pixels.
-			dst[DstOffs + 0] = E0;
-			dst[DstOffs + 1] = E1;
-			dst[DstOffs + (pitch / 4) + 0] = E2;
-			dst[DstOffs + (pitch / 4) + 1] = E3;
-			
-			// Next group of pixels.
-			DstOffs += 2;
-		}
-		
-		SrcOffs += ScrAdd;
-	}
+	Blit_Scale2x((unsigned int*)screen, MD_Screen32, pitch, x, y, offset);
 }
