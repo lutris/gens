0bf86d1ab2af40f7a788d5c850323267751b685f
David Korth <gerbilsoft@verizon.net>
2008-09-14 20:47:30 -0400

    g_32x_32bit.c: Variable naming changes. (Pixel -> pixel, etc.)

--- src/gens/emulator/g_32x_32bit.c
+++ src/gens/emulator/g_32x_32bit.c
@@ -28,13 +28,13 @@ void Post_Line_32X(void)
 	int VRam_Ind = ((_32X_VDP.State & 1) << 16);
 	VRam_Ind += VRam_32X[VRam_Ind + VDP_Current_Line];
 	
-	unsigned int Pixel;
+	unsigned int pixel;
 	unsigned char pixC; 
 	unsigned short pixS; 
 	
 	// Used for POST_LINE_32X_M11
 	unsigned char Num;
-	int EndPixel, CurPixel, PixMax;
+	int endPixel, curPixel, pixMax;
 	
 	switch (_32X_Rend_Mode)
 	{
@@ -43,10 +43,10 @@ void Post_Line_32X(void)
 		case 8:
 		case 12:
 			//POST_LINE_32X_M00;
-			for (Pixel = TAB336[VDP_Current_Line] + 8; Pixel < TAB336[VDP_Current_Line] + 336; Pixel++)
+			for (pixel = TAB336[VDP_Current_Line] + 8; pixel < TAB336[VDP_Current_Line] + 336; pixel++)
 			{
-					MD_Screen32[Pixel] = MD_Palette32[MD_Screen[Pixel] & 0xFF];
-					MD_Screen[Pixel] = MD_Palette[MD_Screen[Pixel] & 0xFF];
+					MD_Screen32[pixel] = MD_Palette32[MD_Screen[pixel] & 0xFF];
+					MD_Screen[pixel] = MD_Palette[MD_Screen[pixel] & 0xFF];
 			}
 			
 			break;
@@ -54,20 +54,20 @@ void Post_Line_32X(void)
 		case 1:
 			//POST_LINE_32X_M01;
 			VRam_Ind *= 2;
-			for (Pixel = TAB336[VDP_Current_Line] + 8;
-			     Pixel < (TAB336[VDP_Current_Line] + 336); Pixel++)
+			for (pixel = TAB336[VDP_Current_Line] + 8;
+			     pixel < (TAB336[VDP_Current_Line] + 336); pixel++)
 			{
 				pixC = _32X_VDP_Ram[VRam_Ind++ ^ 1];
 				pixS = _32X_VDP_CRam[pixC];
-				if ((pixS & 0x8000) || !(MD_Screen[Pixel] & 0x0F))
+				if ((pixS & 0x8000) || !(MD_Screen[pixel] & 0x0F))
 				{
-					MD_Screen[Pixel] = _32X_VDP_CRam_Ajusted[pixC];
-					MD_Screen32[Pixel] = _32X_VDP_CRam_Ajusted32[pixC];
+					MD_Screen[pixel] = _32X_VDP_CRam_Ajusted[pixC];
+					MD_Screen32[pixel] = _32X_VDP_CRam_Ajusted32[pixC];
 				}
 				else
 				{
-					MD_Screen32[Pixel] = MD_Palette32[MD_Screen[Pixel] & 0xFF];
-					MD_Screen[Pixel] = MD_Palette[MD_Screen[Pixel] & 0xFF];
+					MD_Screen32[pixel] = MD_Palette32[MD_Screen[pixel] & 0xFF];
+					MD_Screen[pixel] = MD_Palette[MD_Screen[pixel] & 0xFF];
 				}
 			}
 			
@@ -76,18 +76,18 @@ void Post_Line_32X(void)
 		case 2:
 		case 10:
 			//POST_LINE_32X_M10;
-			for (Pixel = TAB336[VDP_Current_Line] + 8; Pixel < TAB336[VDP_Current_Line] + 336; Pixel++)
+			for (pixel = TAB336[VDP_Current_Line] + 8; pixel < TAB336[VDP_Current_Line] + 336; pixel++)
 			{
 				pixS = VRam_32X[VRam_Ind++];
-				if ((pixS & 0x8000) || !(MD_Screen[Pixel] & 0x0F))
+				if ((pixS & 0x8000) || !(MD_Screen[pixel] & 0x0F))
 				{
-					MD_Screen32[Pixel] = _32X_Palette_32B[pixS];
-					MD_Screen[Pixel] = _32X_Palette_16B[pixS];
+					MD_Screen32[pixel] = _32X_Palette_32B[pixS];
+					MD_Screen[pixel] = _32X_Palette_16B[pixS];
 				}
 				else
 				{
-					MD_Screen32[Pixel] = MD_Palette32[MD_Screen[Pixel] & 0xFF];
-					MD_Screen[Pixel] = MD_Palette[MD_Screen[Pixel] & 0xFF];
+					MD_Screen32[pixel] = MD_Palette32[MD_Screen[pixel] & 0xFF];
+					MD_Screen[pixel] = MD_Palette[MD_Screen[pixel] & 0xFF];
 				}
 			}
 			break;
@@ -97,17 +97,17 @@ void Post_Line_32X(void)
 		case 11:
 		case 15:
 			//POST_LINE_32X_M11;
-			CurPixel = TAB336[VDP_Current_Line] + 8;
-			PixMax = TAB336[VDP_Current_Line] + 336;
-			while (CurPixel < PixMax)
+			curPixel = TAB336[VDP_Current_Line] + 8;
+			pixMax = TAB336[VDP_Current_Line] + 336;
+			while (curPixel < pixMax)
 			{
 				pixC = VRam_32X[VRam_Ind] & 0xFF;
 				Num = VRam_32X[VRam_Ind++] >> 8;
-				EndPixel = CurPixel + Num;
-				while (CurPixel <= EndPixel)
+				endPixel = curPixel + Num;
+				while (curPixel <= endPixel)
 				{
-					MD_Screen[CurPixel] = _32X_VDP_CRam_Ajusted[pixC];
-					MD_Screen32[CurPixel++] = _32X_VDP_CRam_Ajusted32[pixC];
+					MD_Screen[curPixel] = _32X_VDP_CRam_Ajusted[pixC];
+					MD_Screen32[curPixel++] = _32X_VDP_CRam_Ajusted32[pixC];
 				}
 			}
 			break;
@@ -115,19 +115,19 @@ void Post_Line_32X(void)
 		case 5:
 			//POST_LINE_32X_M01_P;
 			VRam_Ind *= 2;
-			for (Pixel = TAB336[VDP_Current_Line] + 8; Pixel < (TAB336[VDP_Current_Line] + 336); Pixel++)
+			for (pixel = TAB336[VDP_Current_Line] + 8; pixel < (TAB336[VDP_Current_Line] + 336); pixel++)
 			{
 				pixC = _32X_VDP_Ram[VRam_Ind++ ^ 1];
 				pixS = _32X_VDP_CRam[pixC];
-				if ((pixS & 0x8000) && (MD_Screen[Pixel] & 0x0F))
+				if ((pixS & 0x8000) && (MD_Screen[pixel] & 0x0F))
 				{
-					MD_Screen32[Pixel] = MD_Palette32[MD_Screen[Pixel] & 0xFF];
-					MD_Screen[Pixel] = MD_Palette[MD_Screen[Pixel] & 0xFF];
+					MD_Screen32[pixel] = MD_Palette32[MD_Screen[pixel] & 0xFF];
+					MD_Screen[pixel] = MD_Palette[MD_Screen[pixel] & 0xFF];
 				}
 				else
 				{
-					MD_Screen[Pixel] = _32X_VDP_CRam_Ajusted[pixC];
-					MD_Screen32[Pixel] = _32X_VDP_CRam_Ajusted32[pixC];
+					MD_Screen[pixel] = _32X_VDP_CRam_Ajusted[pixC];
+					MD_Screen32[pixel] = _32X_VDP_CRam_Ajusted32[pixC];
 				}
 			}
 			break;
@@ -135,18 +135,18 @@ void Post_Line_32X(void)
 		case 6:
 		case 14:
 			//POST_LINE_32X_M10_P;
-			for (Pixel = TAB336[VDP_Current_Line] + 8; Pixel < TAB336[VDP_Current_Line] + 336; Pixel++)
+			for (pixel = TAB336[VDP_Current_Line] + 8; pixel < TAB336[VDP_Current_Line] + 336; pixel++)
 			{
 				pixS = VRam_32X[VRam_Ind++];
-				if (!(pixS & 0x8000) && (MD_Screen[Pixel] & 0x0F))
+				if (!(pixS & 0x8000) && (MD_Screen[pixel] & 0x0F))
 				{
-					MD_Screen32[Pixel] = _32X_Palette_32B[pixS];
-					MD_Screen[Pixel] = _32X_Palette_16B[pixS];
+					MD_Screen32[pixel] = _32X_Palette_32B[pixS];
+					MD_Screen[pixel] = _32X_Palette_16B[pixS];
 				}
 				else
 				{
-					MD_Screen32[Pixel] = MD_Palette32[MD_Screen[Pixel] & 0xFF];
-					MD_Screen[Pixel] = MD_Palette[MD_Screen[Pixel] & 0xFF];
+					MD_Screen32[pixel] = MD_Palette32[MD_Screen[pixel] & 0xFF];
+					MD_Screen[pixel] = MD_Palette[MD_Screen[pixel] & 0xFF];
 				}
 			}
 			break;
@@ -154,19 +154,19 @@ void Post_Line_32X(void)
 		case 9:
 			//POST_LINE_32X_SM01;
 			VRam_Ind *= 2;
-			for (Pixel = TAB336[VDP_Current_Line] + 8; Pixel < (TAB336[VDP_Current_Line] + 336); Pixel++)
+			for (pixel = TAB336[VDP_Current_Line] + 8; pixel < (TAB336[VDP_Current_Line] + 336); pixel++)
 			{
 				pixC = _32X_VDP_Ram[VRam_Ind++ ^ 1];
 				pixS = _32X_VDP_CRam[pixC];
-				if ((pixS & 0x8000) || !(MD_Screen[Pixel] & 0x0F))
+				if ((pixS & 0x8000) || !(MD_Screen[pixel] & 0x0F))
 				{
-					MD_Screen[Pixel] = _32X_VDP_CRam_Ajusted[pixC];
-					MD_Screen32[Pixel] = _32X_VDP_CRam_Ajusted32[pixC];
+					MD_Screen[pixel] = _32X_VDP_CRam_Ajusted[pixC];
+					MD_Screen32[pixel] = _32X_VDP_CRam_Ajusted32[pixC];
 				}
 				else
 				{
-					MD_Screen32[Pixel] = MD_Palette32[MD_Screen[Pixel] & 0xFF];
-					MD_Screen[Pixel] = MD_Palette[MD_Screen[Pixel] & 0xFF];
+					MD_Screen32[pixel] = MD_Palette32[MD_Screen[pixel] & 0xFF];
+					MD_Screen[pixel] = MD_Palette[MD_Screen[pixel] & 0xFF];
 				}
 			}
 			break;
@@ -174,19 +174,19 @@ void Post_Line_32X(void)
 		case 13:
 			//POST_LINE_32X_SM01_P;
 			VRam_Ind *= 2;
-			for (Pixel = TAB336[VDP_Current_Line] + 8; Pixel < (TAB336[VDP_Current_Line] + 336); Pixel++)
+			for (pixel = TAB336[VDP_Current_Line] + 8; pixel < (TAB336[VDP_Current_Line] + 336); pixel++)
 			{
 				pixC = _32X_VDP_Ram[VRam_Ind++ ^ 1];
 				pixS = _32X_VDP_CRam[pixC];
-				if ((pixS & 0x8000) && (MD_Screen[Pixel] & 0x0F))
+				if ((pixS & 0x8000) && (MD_Screen[pixel] & 0x0F))
 				{
-					MD_Screen32[Pixel] = MD_Palette32[MD_Screen[Pixel] & 0xFF];
-					MD_Screen[Pixel] = MD_Palette[MD_Screen[Pixel] & 0xFF];
+					MD_Screen32[pixel] = MD_Palette32[MD_Screen[pixel] & 0xFF];
+					MD_Screen[pixel] = MD_Palette[MD_Screen[pixel] & 0xFF];
 				}
 				else
 				{
-					MD_Screen[Pixel] = _32X_VDP_CRam_Ajusted[pixC];
-					MD_Screen32[Pixel] = _32X_VDP_CRam_Ajusted32[pixC];
+					MD_Screen[pixel] = _32X_VDP_CRam_Ajusted[pixC];
+					MD_Screen32[pixel] = _32X_VDP_CRam_Ajusted32[pixC];
 				}
 			}
 			break;
