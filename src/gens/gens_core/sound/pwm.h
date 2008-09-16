e9a3c37ad1c487dcd7343db8dffdafdc92199f09
David Korth <gerbilsoft@verizon.net>
2008-09-16 16:41:13 -0400

    Revert "pwm.h: Remove FASTCALL entirely."
    This reverts commit 65cbb0e3512c2c0eee8ce1a13cb2fa7b77214211.
    
    FASTCALL is apparently required for these functions, so I'll need
    to figure out some other way to fix the Win32 problems.

--- src/gens/gens_core/sound/pwm.h
+++ src/gens/gens_core/sound/pwm.h
@@ -5,6 +5,25 @@
 extern "C" {
 #endif
 
+
+#ifndef __WIN32__
+// FASTCALL is already defined on Win32, so we're only going to
+// define it on non-Win32 systems.
+
+#ifndef __GNUC__
+
+#define FASTCALL				__fastcall
+#define DECL_FASTCALL(type, name)	type FASTCALL name
+
+#else //__GNUC__
+
+#define FASTCALL __attribute__ ((regparm(2)))
+#define DECL_FASTCALL(type, name)	type name __attribute__ ((regparm(2)))
+
+#endif //!__GNUC__
+
+#endif /* __WIN32__ */
+
 extern unsigned short PWM_FIFO_R[8];
 extern unsigned short PWM_FIFO_L[8];
 extern unsigned int PWM_RP_R;
@@ -21,12 +40,12 @@ extern unsigned int PWM_Enable;
 extern unsigned int PWM_Out_R;
 extern unsigned int PWM_Out_L;
 
-void PWM_Init(void);
-void PWM_Set_Cycle(unsigned int cycle);
-void PWM_Set_Int(unsigned int int_time);
-void PWM_Clear_Timer(void);
-void PWM_Update_Timer(unsigned int cycle);
-void PWM_Update(int **buf, int length);
+void FASTCALL PWM_Init(void);
+void FASTCALL PWM_Set_Cycle(unsigned int cycle);
+void FASTCALL PWM_Set_Int(unsigned int int_time);
+void FASTCALL PWM_Clear_Timer(void);
+void FASTCALL PWM_Update_Timer(unsigned int cycle);
+void FASTCALL PWM_Update(int **buf, int length);
 
 #ifdef __cplusplus
 }
