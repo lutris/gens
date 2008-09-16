e9aebceb0c6c8fd5fed7ad9f3f1ed7113593624b
David Korth <gerbilsoft@verizon.net>
2008-09-16 14:07:06 -0400

    ini.hpp: Removed old INI function prototypes.
    These were converted to the INI class in m4, but I forgot to remove
    the function prototypes.

--- src/gens/port/ini.hpp
+++ src/gens/port/ini.hpp
@@ -65,18 +65,6 @@ class INI
 extern "C" {
 #endif
 
-void INI_LoadConfig(const char* filename);
-void INI_Clear(void);
-void INI_SaveConfig(const char* filename);
-
-int INI_GetInt(const char* section, const char* key, const int def);
-bool INI_GetBool(const char* section, const char* key, const bool def);
-void INI_GetString(const char* section, const char* key, const char* def, char* buf, const int size);
-void INI_WriteInt(const char* section, const char* key, const int value);
-void INI_WriteBool(const char* section, const char* key, const bool value);
-void INI_WriteString(const char* section, const char* key, const char* value);
-void INI_SaveConfig(const char* filename);
-
 int GetPrivateProfileInt(const char *section, const char *var, const int def, const char *filename);
 void GetPrivateProfileString(const char *section, const char *var, const char *def, char *get, const int length, const char *filename);
 void WritePrivateProfileString(const char *section, const char *var, const char *var_name, const char *filename);
