320b27de49615faf4f29c961d087e5abf40ca315
David Korth <gerbilsoft@verizon.net>
2008-09-18 15:11:03 -0400

    rom.cpp: Temporarily disable the Zip File Selection Dialog.

--- src/gens/util/file/rom.cpp
+++ src/gens/util/file/rom.cpp
@@ -31,7 +31,8 @@ using std::list;
 #include "gens_core/misc/byteswap.h"
 
 #include "ui/gens_ui.hpp"
-#include "zip_select/zip_select_dialog_misc.hpp"
+// TODO
+ //#include "zip_select/zip_select_dialog_misc.hpp"
 
 // New file compression handler.
 #include "util/file/compress/compressor.hpp"
@@ -548,16 +549,18 @@ ROMType Load_ROM(const char *filename, struct Rom **retROM)
 		*retROM = NULL;
 		return (ROMType)0;
 	}
-	else if (files->size() == 1)
+	else //if (files->size() == 1)
 	{
 		// One file is in the archive. Load it.
 		selFile = &(*files->begin());
 	}
+	/* TODO
 	else
 	{
 		// More than one file is in the archive. Load it.
 		selFile = Open_Zip_Select_Dialog(files);
 	}
+	*/
 	
 	if (!selFile)
 	{
