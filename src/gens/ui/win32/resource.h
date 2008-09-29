d0d3237dc474239ad7da9a0b080f151f681c697c
David Korth <gerbilsoft@verizon.net>
2008-09-28 21:31:44 -0400

    [Win32] Added a resource script for compiling icons and bitmaps into the executable.
    Currently, the two Gens icons (Gens logo and MD), the two Sonic icons, and
    the two Gens logo bitmaps are compiled in.

---
+++ src/gens/ui/win32/resource.h
@@ -0,0 +1,11 @@
+// Resources for Gens/GS Win32
+
+// Icons
+#define IDI_GENS_APP		0x0001
+#define IDI_GENS_MD		0x0002
+#define IDI_SONIC_HEAD		0x0003
+#define IDI_SONIC_WAITING	0x0004
+
+// Bitmaps
+#define IDB_GENS_LOGO_SMALL	0x0101
+#define IDB_GENS_LOGO_BIG	0x0102
