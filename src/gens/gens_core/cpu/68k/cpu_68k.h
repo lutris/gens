a3943e5a92065a3897de015a8155b3cdc6fe1319
David Korth <gerbilsoft@verizon.net>
2008-10-27 11:41:39 -0400

    cpu_68k.c: Cleaned up this file.
    TODO:
    - Convert System_ID to an enum.
    - Don't store pointers in ints.

--- src/gens/gens_core/cpu/68k/cpu_68k.h
+++ src/gens/gens_core/cpu/68k/cpu_68k.h
@@ -8,17 +8,17 @@ extern "C" {
 #endif
 
 extern struct S68000CONTEXT Context_68K;
-	
-int M68K_Init();
-int S68K_Init();
+
+int M68K_Init(void);
+int S68K_Init(void);
 void M68K_Reset(int System_ID);
-void S68K_Reset();
-void M68K_Reset_CPU();
-void S68K_Reset_CPU();
-void M68K_32X_Mode();
-void M68K_Set_32X_Rom_Bank();
-void M68K_Set_Prg_Ram();
-void MS68K_Set_Word_Ram();
+void S68K_Reset(void);
+void M68K_Reset_CPU(void);
+void S68K_Reset_CPU(void);
+void M68K_32X_Mode(void);
+void M68K_Set_32X_Rom_Bank(void);
+void M68K_Set_Prg_Ram(void);
+void MS68K_Set_Word_Ram(void);
 
 #ifdef __cplusplus
 }
