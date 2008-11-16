cbb64b372220e1338dd635c5fb74d4533ee89fce
David Korth <gerbilsoft@verizon.net>
2008-11-16 15:26:13 -0500

    plugins/render/normal/blit1x.cpp: Updated to the new MDP_Render_Info_t struct.

--- src/gens/plugins/render/normal/blit1x.cpp
+++ src/gens/plugins/render/normal/blit1x.cpp
@@ -33,22 +33,24 @@
  * Blit1x: Blits the image to the screen, 1x size, no filtering.
  * @param screen Pointer to the screen buffer.
  * @param mdScreen Pointer to the MD screen buffer.
+ * @param width Width of the image.
+ * @param height Height of the image.
  * @param pitch Number of bytes per line.
- * @param x X coordinate for the image.
- * @param y Y coordinate for the image.
  * @param offset ???
  */
 template<typename pixel>
-static inline void Blit1x_int(pixel *screen, pixel *mdScreen, int pitch, int x, int y, int offset)
+static inline void Blit1x_int(pixel *screen, pixel *mdScreen,
+			      unsigned short width, unsigned short height,
+			      int pitch, int offset)
 {
 	int i;
 	
 	// Adjust for the 8px border on the MD Screen.
 	mdScreen += 8;
 	
-	for (i = 0; i < y; i++)
+	for (i = 0; i < height; i++)
 	{
-		memcpy(screen, mdScreen, x * sizeof(pixel));
+		memcpy(screen, mdScreen, width * sizeof(pixel));
 		
 		// Next line.
 		// TODO: Make this a constant somewhere.
@@ -65,14 +67,14 @@ void Blit1x(MDP_Render_Info_t *renderInfo)
 	
 	if (renderInfo->bpp == 15 || renderInfo->bpp == 16)
 	{
-		Blit1x_int((uint16_t*)renderInfo->screen,
-			   renderInfo->screen16, renderInfo->pitch,
-			   renderInfo->x, renderInfo->y, renderInfo->offset);
+		Blit1x_int((uint16_t*)renderInfo->screen, renderInfo->screen16,
+			    renderInfo->width, renderInfo->height,
+			    renderInfo->pitch, renderInfo->offset);
 	}
 	else
 	{
-		Blit1x_int((uint32_t*)renderInfo->screen,
-			    renderInfo->screen32, renderInfo->pitch,
-			    renderInfo->x, renderInfo->y, renderInfo->offset);
+		Blit1x_int((uint32_t*)renderInfo->screen, renderInfo->screen32,
+			    renderInfo->width, renderInfo->height,
+			    renderInfo->pitch, renderInfo->offset);
 	}
 }
