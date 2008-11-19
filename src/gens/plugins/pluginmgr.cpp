f0a3f15c89063fc6c0a86584eeb67d6632244ed7
David Korth <gerbilsoft@verizon.net>
2008-11-18 19:12:33 -0500

    pluginmgr.cpp: #include "pluginmgr.hpp"

--- src/gens/plugins/pluginmgr.cpp
+++ src/gens/plugins/pluginmgr.cpp
@@ -20,7 +20,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-#include "plugin.h"
+#include "pluginmgr.hpp"
 
 using std::vector;
 
