197569fb21a53b0d3e75f2b584d4bb86d74450c9
David Korth <gerbilsoft@verizon.net>
2008-11-19 12:57:20 -0500

    Initial MDP port of the Scanline renderer. (C++ version only)

--- src/gens/plugins/render/scanline/mdp_render_scanline.hpp
+++ src/gens/plugins/render/scanline/mdp_render_scanline.hpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Gens: Plugin Manager.                                                   *
+ * Gens: [MDP] 2x renderer.                                                *
  *                                                                         *
  * Copyright (c) 1999-2002 by Stéphane Dallongeville                       *
  * Copyright (c) 2003-2004 by Stéphane Akhoun                              *
@@ -20,75 +20,19 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-#include "pluginmgr.hpp"
+#ifndef GENS_MDP_RENDER_SCANLINE_HPP
+#define GENS_MDP_RENDER_SCANLINE_HPP
 
-using std::vector;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-// Render plugins
-#include "render/normal/mdp_render_1x_plugin.h"
-#include "render/double/mdp_render_2x_plugin.h"
-#include "render/scanline/mdp_render_scanline_plugin.h"
+#include "plugins/mdp_render.h"
 
-// Internal plugins
-static MDP_t* mdp_internal[] =
-{
-	&mdp_render_1x,
-	&mdp_render_2x,
-	&mdp_render_scanline,
-	NULL,
-};
+void mdp_render_scanline_cpp(MDP_Render_Info_t *renderInfo);
 
-
-/**
- * vRenderPlugins: Vector containing render plugins.
- */
-vector<MDP_t*> PluginMgr::vRenderPlugins;
-
-
-/**
- * init(): Initialize the plugin system.
- */
-void PluginMgr::init(void)
-{
-	// Make sure all loaded plugins are shut down.
-	end();
-	
-	// Load all internal plugins.
-	unsigned int i = 0;
-	while (mdp_internal[i])
-	{
-		// TODO: Check plugin version information.
-		switch (mdp_internal[i]->type)
-		{
-			case MDPT_RENDER:
-				// Rendering plugin.
-				vRenderPlugins.push_back(mdp_internal[i]);
-				if (mdp_internal[i]->init)
-					mdp_internal[i]->init();
-				break;
-			
-			default:
-				// Unknown plugin type.
-				break;
-		}
-		
-		// Next plugin.
-		i++;
-	}
+#ifdef __cplusplus
 }
+#endif
 
-/**
- * end(): Shut down the plugin system.
- */
-void PluginMgr::end(void)
-{
-	// Shut down all render plugins.
-	for (unsigned int i = 0; i < vRenderPlugins.size(); i++)
-	{
-		if (vRenderPlugins.at(i)->end != NULL)
-			vRenderPlugins.at(i)->end();
-	}
-	
-	// Clear the vector of render plugins.
-	vRenderPlugins.clear();
-}
+#endif /* GENS_MDP_RENDER_SCANLINE_HPP */
