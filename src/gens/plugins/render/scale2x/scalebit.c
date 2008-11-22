3d416e81d4a8344d52836ee299e2f0622f1bab3b
David Korth <gerbilsoft@verizon.net>
2008-11-21 19:27:16 -0500

    plugins/render/scale2x/: Removed unnecessary code.
    Functions removed:
    - All 8-bit color scalers.
    - Scale2x3
    - Scale2x4

--- src/gens/plugins/render/scale2x/scalebit.c
+++ src/gens/plugins/render/scale2x/scalebit.c
@@ -57,49 +57,11 @@ static inline void stage_scale2x(void* dst0, void* dst1, const void* src0, const
 {
 	switch (pixel) {
 #if defined(__GNUC__) && defined(__i386__)
-		case 1 : scale2x_8_mmx(SSDST(8,0), SSDST(8,1), SSSRC(8,0), SSSRC(8,1), SSSRC(8,2), pixel_per_row); break;
 		case 2 : scale2x_16_mmx(SSDST(16,0), SSDST(16,1), SSSRC(16,0), SSSRC(16,1), SSSRC(16,2), pixel_per_row); break;
 		case 4 : scale2x_32_mmx(SSDST(32,0), SSDST(32,1), SSSRC(32,0), SSSRC(32,1), SSSRC(32,2), pixel_per_row); break;
 #else
-		case 1 : scale2x_8_def(SSDST(8,0), SSDST(8,1), SSSRC(8,0), SSSRC(8,1), SSSRC(8,2), pixel_per_row); break;
 		case 2 : scale2x_16_def(SSDST(16,0), SSDST(16,1), SSSRC(16,0), SSSRC(16,1), SSSRC(16,2), pixel_per_row); break;
 		case 4 : scale2x_32_def(SSDST(32,0), SSDST(32,1), SSSRC(32,0), SSSRC(32,1), SSSRC(32,2), pixel_per_row); break;
-#endif
-	}
-}
-
-/**
- * Apply the Scale2x3 effect on a group of rows. Used internally.
- */
-static inline void stage_scale2x3(void* dst0, void* dst1, void* dst2, const void* src0, const void* src1, const void* src2, unsigned pixel, unsigned pixel_per_row)
-{
-	switch (pixel) {
-#if defined(__GNUC__) && defined(__i386__)
-		case 1 : scale2x3_8_mmx(SSDST(8,0), SSDST(8,1), SSDST(8,2), SSSRC(8,0), SSSRC(8,1), SSSRC(8,2), pixel_per_row); break;
-		case 2 : scale2x3_16_mmx(SSDST(16,0), SSDST(16,1), SSDST(16,2), SSSRC(16,0), SSSRC(16,1), SSSRC(16,2), pixel_per_row); break;
-		case 4 : scale2x3_32_mmx(SSDST(32,0), SSDST(32,1), SSDST(32,2), SSSRC(32,0), SSSRC(32,1), SSSRC(32,2), pixel_per_row); break;
-#else
-		case 1 : scale2x3_8_def(SSDST(8,0), SSDST(8,1), SSDST(8,2), SSSRC(8,0), SSSRC(8,1), SSSRC(8,2), pixel_per_row); break;
-		case 2 : scale2x3_16_def(SSDST(16,0), SSDST(16,1), SSDST(16,2), SSSRC(16,0), SSSRC(16,1), SSSRC(16,2), pixel_per_row); break;
-		case 4 : scale2x3_32_def(SSDST(32,0), SSDST(32,1), SSDST(32,2), SSSRC(32,0), SSSRC(32,1), SSSRC(32,2), pixel_per_row); break;
-#endif
-	}
-}
-
-/**
- * Apply the Scale2x4 effect on a group of rows. Used internally.
- */
-static inline void stage_scale2x4(void* dst0, void* dst1, void* dst2, void* dst3, const void* src0, const void* src1, const void* src2, unsigned pixel, unsigned pixel_per_row)
-{
-	switch (pixel) {
-#if defined(__GNUC__) && defined(__i386__)
-		case 1 : scale2x4_8_mmx(SSDST(8,0), SSDST(8,1), SSDST(8,2), SSDST(8,3), SSSRC(8,0), SSSRC(8,1), SSSRC(8,2), pixel_per_row); break;
-		case 2 : scale2x4_16_mmx(SSDST(16,0), SSDST(16,1), SSDST(16,2), SSDST(16,3), SSSRC(16,0), SSSRC(16,1), SSSRC(16,2), pixel_per_row); break;
-		case 4 : scale2x4_32_mmx(SSDST(32,0), SSDST(32,1), SSDST(32,2), SSDST(32,3), SSSRC(32,0), SSSRC(32,1), SSSRC(32,2), pixel_per_row); break;
-#else
-		case 1 : scale2x4_8_def(SSDST(8,0), SSDST(8,1), SSDST(8,2), SSDST(8,3), SSSRC(8,0), SSSRC(8,1), SSSRC(8,2), pixel_per_row); break;
-		case 2 : scale2x4_16_def(SSDST(16,0), SSDST(16,1), SSDST(16,2), SSDST(16,3), SSSRC(16,0), SSSRC(16,1), SSSRC(16,2), pixel_per_row); break;
-		case 4 : scale2x4_32_def(SSDST(32,0), SSDST(32,1), SSDST(32,2), SSDST(32,3), SSSRC(32,0), SSSRC(32,1), SSSRC(32,2), pixel_per_row); break;
 #endif
 	}
 }
@@ -110,7 +72,6 @@ static inline void stage_scale2x4(void* dst0, void* dst1, void* dst2, void* dst3
 static inline void stage_scale3x(void* dst0, void* dst1, void* dst2, const void* src0, const void* src1, const void* src2, unsigned pixel, unsigned pixel_per_row)
 {
 	switch (pixel) {
-		case 1 : scale3x_8_def(SSDST(8,0), SSDST(8,1), SSDST(8,2), SSSRC(8,0), SSSRC(8,1), SSSRC(8,2), pixel_per_row); break;
 		case 2 : scale3x_16_def(SSDST(16,0), SSDST(16,1), SSDST(16,2), SSSRC(16,0), SSSRC(16,1), SSSRC(16,2), pixel_per_row); break;
 		case 4 : scale3x_32_def(SSDST(32,0), SSDST(32,1), SSDST(32,2), SSSRC(32,0), SSSRC(32,1), SSSRC(32,2), pixel_per_row); break;
 	}
@@ -168,96 +129,6 @@ static void scale2x(void* void_dst, unsigned dst_slice, const void* void_src, un
 	}
 
 	stage_scale2x(SCDST(0), SCDST(1), SCSRC(0), SCSRC(1), SCSRC(1), pixel, width);
-
-#if defined(__GNUC__) && defined(__i386__)
-	scale2x_mmx_emms();
-#endif
-}
-
-/**
- * Apply the Scale2x3 effect on a bitmap.
- * The destination bitmap is filled with the scaled version of the source bitmap.
- * The source bitmap isn't modified.
- * The destination bitmap must be manually allocated before calling the function,
- * note that the resulting size is exactly 2x3 times the size of the source bitmap.
- * \param void_dst Pointer at the first pixel of the destination bitmap.
- * \param dst_slice Size in bytes of a destination bitmap row.
- * \param void_src Pointer at the first pixel of the source bitmap.
- * \param src_slice Size in bytes of a source bitmap row.
- * \param pixel Bytes per pixel of the source and destination bitmap.
- * \param width Horizontal size in pixels of the source bitmap.
- * \param height Vertical size in pixels of the source bitmap.
- */
-static void scale2x3(void* void_dst, unsigned dst_slice, const void* void_src, unsigned src_slice, unsigned pixel, unsigned width, unsigned height)
-{
-	unsigned char* dst = (unsigned char*)void_dst;
-	const unsigned char* src = (const unsigned char*)void_src;
-	unsigned count;
-
-	assert(height >= 2);
-
-	count = height;
-
-	stage_scale2x3(SCDST(0), SCDST(1), SCDST(2), SCSRC(0), SCSRC(0), SCSRC(1), pixel, width);
-
-	dst = SCDST(3);
-
-	count -= 2;
-	while (count) {
-		stage_scale2x3(SCDST(0), SCDST(1), SCDST(2), SCSRC(0), SCSRC(1), SCSRC(2), pixel, width);
-
-		dst = SCDST(3);
-		src = SCSRC(1);
-
-		--count;
-	}
-
-	stage_scale2x3(SCDST(0), SCDST(1), SCDST(2), SCSRC(0), SCSRC(1), SCSRC(1), pixel, width);
-
-#if defined(__GNUC__) && defined(__i386__)
-	scale2x_mmx_emms();
-#endif
-}
-
-/**
- * Apply the Scale2x4 effect on a bitmap.
- * The destination bitmap is filled with the scaled version of the source bitmap.
- * The source bitmap isn't modified.
- * The destination bitmap must be manually allocated before calling the function,
- * note that the resulting size is exactly 2x4 times the size of the source bitmap.
- * \param void_dst Pointer at the first pixel of the destination bitmap.
- * \param dst_slice Size in bytes of a destination bitmap row.
- * \param void_src Pointer at the first pixel of the source bitmap.
- * \param src_slice Size in bytes of a source bitmap row.
- * \param pixel Bytes per pixel of the source and destination bitmap.
- * \param width Horizontal size in pixels of the source bitmap.
- * \param height Vertical size in pixels of the source bitmap.
- */
-static void scale2x4(void* void_dst, unsigned dst_slice, const void* void_src, unsigned src_slice, unsigned pixel, unsigned width, unsigned height)
-{
-	unsigned char* dst = (unsigned char*)void_dst;
-	const unsigned char* src = (const unsigned char*)void_src;
-	unsigned count;
-
-	assert(height >= 2);
-
-	count = height;
-
-	stage_scale2x4(SCDST(0), SCDST(1), SCDST(2), SCDST(3), SCSRC(0), SCSRC(0), SCSRC(1), pixel, width);
-
-	dst = SCDST(4);
-
-	count -= 2;
-	while (count) {
-		stage_scale2x4(SCDST(0), SCDST(1), SCDST(2), SCDST(3), SCSRC(0), SCSRC(1), SCSRC(2), pixel, width);
-
-		dst = SCDST(4);
-		src = SCSRC(1);
-
-		--count;
-	}
-
-	stage_scale2x4(SCDST(0), SCDST(1), SCDST(2), SCDST(3), SCSRC(0), SCSRC(1), SCSRC(1), pixel, width);
 
 #if defined(__GNUC__) && defined(__i386__)
 	scale2x_mmx_emms();
@@ -491,12 +362,6 @@ void scale(unsigned scale, void* void_dst, unsigned dst_slice, const void* void_
 	case 202 :
 	case 2 :
 		scale2x(void_dst, dst_slice, void_src, src_slice, pixel, width, height);
-		break;
-	case 203 :
-		scale2x3(void_dst, dst_slice, void_src, src_slice, pixel, width, height);
-		break;
-	case 204 :
-		scale2x4(void_dst, dst_slice, void_src, src_slice, pixel, width, height);
 		break;
 	case 303 :
 	case 3 :
