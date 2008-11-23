4efb0bd2bae38b78a481e1577a46db95fd1c4c9e
David Korth <gerbilsoft@verizon.net>
2008-11-23 00:19:06 -0500

    plugins/render/hq2x/mdp_render_hq2x_(16|32)_x86.asm: Removed some extra spaces.

--- src/gens/plugins/render/hq2x/mdp_render_hq2x_32_x86.asm
+++ src/gens/plugins/render/hq2x/mdp_render_hq2x_32_x86.asm
@@ -501,13 +501,13 @@ arg_offset	equ 28
 	shr	eax, 16
 	mov	[w3], eax
 	movd	eax, mm6
-	movzx	edx, ax  
+	movzx	edx, ax
 	mov	[w4], edx
 	mov	[w5], edx
 	shr	eax, 16
 	mov	[w6], eax
 	movd	eax, mm7
-	movzx	edx, ax  
+	movzx	edx, ax
 	mov	[w7], edx
 	mov	[w8], edx
 	shr	eax, 16
@@ -521,31 +521,31 @@ arg_offset	equ 28
 	mov	ebx, [nextline]
 	movq	mm7, [esi + ebx - 2]
 	movd	eax, mm5
-	movzx	edx, ax  
+	movzx	edx, ax
 	mov	[w1], edx
 	shr	eax, 16
 	mov	[w2], eax
 	psrlq	mm5, 32
 	movd	eax, mm5
-	movzx	edx, ax  
+	movzx	edx, ax
 	mov	[w3], edx
 	movd	eax, mm6
-	movzx	edx, ax  
+	movzx	edx, ax
 	mov	[w4], edx
 	shr	eax, 16
 	mov	[w5], eax
 	psrlq	mm6, 32
 	movd	eax, mm6
-	movzx	edx, ax  
+	movzx	edx, ax
 	mov	[w6], edx
 	movd	eax, mm7
-	movzx	edx, ax  
+	movzx	edx, ax
 	mov	[w7], edx
 	shr	eax, 16
 	mov	[w8], eax
 	psrlq	mm7, 32
 	movd	eax, mm7
-	movzx	edx, ax  
+	movzx	edx, ax
 	mov	[w9], edx
 	
 .flags:
