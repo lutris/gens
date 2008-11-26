1fb043bcdd8689bc1a65213b7c39b1e7fa77dec3
David Korth <gerbilsoft@verizon.net>
2008-11-26 11:46:11 -0500

    fastblur_16_x86.asm: Optimized Fast_Blur_16_x86_mmx().
    The main optimization is using a single mask for all three color components
    instead of using one mask for RB and one for G. The same optimization was
    done with Fast_Blur_16_x86().
    
    1,000 iterations of old Fast_Blur_16_x86_mmx(): 0.124s
    1,000 iterations of new Fast_Blur_16_x86_mmx(): 0.059s
    
    Also, removed push/pop for registers that aren't used in this function.

--- src/gens/gens_core/gfx/fastblur_16_x86.asm
+++ src/gens/gens_core/gfx/fastblur_16_x86.asm
@@ -37,16 +37,13 @@ section .data align=64
 	extern _MD_Screen
 	extern _bppMD
 	
-	; 64-bit masks used for the MMX version.
-	Mask:		dd 0x00000000, 0x00000000
-	Mask_GG_15:	dd 0x03E003E0, 0x03E003E0
-	Mask_RBRB_15:	dd 0x7C1F7C1F, 0x7C1F7C1F
-	Mask_GG_16:	dd 0x07C007C0, 0x07C007C0
-	Mask_RBRB_16:	dd 0xF81FF81F, 0xF81FF81F
+	; 64-bit masks used for the mmx version.
+	MASK_DIV2_15_MMX:	dd 0x3DEF3DEF, 0x3DEF3DEF
+	MASK_DIV2_16_MMX:	dd 0x7BEF7BEF, 0x7BEF7BEF
 	
 section .text align=64
 	
-	; void Fast_Blur_16_x86()
+	; void Fast_Blur_16_x86_mmx()
 	; 15/16-bit color Fast Blur function, non-MMX.
 	global _Fast_Blur_16_x86
 	_Fast_Blur_16_x86:
@@ -98,60 +95,47 @@ section .text align=64
 	global _Fast_Blur_16_x86_mmx
 	_Fast_Blur_16_x86_mmx:
 		
-		push ebx
-		push ecx
-		push edx
-		push edi
-		push esi
+		push	ebx
+		push	ecx
+		push	esi
 		
-		mov esi, _MD_Screen
-		mov ecx, MD_SCREEN_SIZE / 4
-		xor edi, edi
-		xor edx, edx
+		; Start at the beginning of the actual display data.
+		mov	esi, _MD_Screen + (8*2)
+		mov	ecx, (MD_SCREEN_SIZE - 8) / 4
 		
-		; If 15-bit, apply the 15-bit color masks.
-		cmp byte [_bppMD], 15
-		je short .Mask_555
+		; Default to 16-bit color.
+		movq	mm7, [MASK_DIV2_16_MMX]
+		cmp	byte [_bppMD], 15
+		jne	short .Loop
 		
-		; 16-bit masks
-		movq mm6, [Mask_RBRB_16]
-		movq mm7, [Mask_GG_16]
-		jmp short .MMX_Loop
-		
-	.Mask_555:
-		; 15-bit masks
-		movq mm6, [Mask_RBRB_15]
-		movq mm7, [Mask_GG_15]
+		; 15-bit color is in use.
+		movq	mm7, [MASK_DIV2_15_MMX]
+		jmp	short .Loop
 	
 	align 32
 	
-	.MMX_Loop:
-			movq mm0, [esi]
-			movq mm1, mm0
-			movq mm2, [esi + 2]
-			movq mm3, mm2
-			pand mm0, mm6
-			pand mm1, mm7
-			pand mm2, mm6
-			pand mm3, mm7
-			psrlw mm0, 1
-			psrlw mm1, 1
-			psrlw mm2, 1
-			psrlw mm3, 1
-			paddusw mm0, mm2
-			paddusw mm1, mm3
-			pand mm0, mm6
-			pand mm1, mm7
-			add esi, 8
-			paddw mm0, mm1
-			dec ecx
-			movq [esi - 8], mm0
-			jnz short .MMX_Loop
+	.Loop:
+			; Get source pixels.
+			movq	mm0, [esi]
+			movq	mm1, [esi + 2]
+			
+			; Blur source pixels.
+			psrlw	mm0, 1
+			psrlw	mm1, 1
+			pand	mm0, mm7
+			pand	mm1, mm7
+			paddw	mm0, mm1
+			
+			; Put destination pixels.
+			movq	[esi], mm0
+			
+			; Next group of pixels.
+			add	esi, 8
+			dec	ecx
+			jnz short .Loop
 		
+		pop	esi
+		pop	ecx
+		pop	ebx
 		emms
-		pop esi
-		pop edi
-		pop edx
-		pop ecx
-		pop ebx
 		ret
