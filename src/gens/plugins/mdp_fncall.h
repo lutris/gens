ffa47aa01d74c83c2989d88f3640d9a78c8a69e6
David Korth <gerbilsoft@verizon.net>
2008-11-28 13:42:48 -0500

    mdp_fncall.h: Split GENS_FNCALL from mdp.h into a separate file.

--- src/gens/plugins/mdp_fncall.h
+++ src/gens/plugins/mdp_fncall.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Gens: MDP: Mega Drive Plugin - Base Interface Definitions.              *
+ * Gens: MDP: Mega Drive Plugin - Function Call Definitions.               *
  *                                                                         *
  * Copyright (c) 1999-2002 by Stéphane Dallongeville                       *
  * Copyright (c) 2003-2004 by Stéphane Akhoun                              *
@@ -20,86 +20,29 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-#ifndef GENS_MDP_H
-#define GENS_MDP_H
+#ifndef GENS_MDP_FNCALL_H
+#define GENS_MDP_FNCALL_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <stdint.h>
-
-// Function call definitions.
-#include "mdp_fncall.h"
-
-// Version number macros.
-#include "mdp_version.h"
-
-// MDP interface version.
-#define MDP_INTERFACE_VERSION MDP_VERSION(0, 1, 0)
-
-
-// Licenses
-#define MDP_LICENSE_GPL_2	"GPL-2"
-#define MDP_LICENSE_GPL_3	"GPL-3"
-#define MDP_LICENSE_LGPL_2	"LGPL-2"
-#define MDP_LICENSE_LGPL_21	"LGPL-2.1"
-#define MDP_LICENSE_LGPL_3	"LGPL-3"
-#define MDP_LICENSE_BSD		"BSD"
-
-
-// Plugin description struct.
-typedef struct
-{
-	const char* name;
-	const char* author_mdp;
-	const char* author_orig;
-	const char* description;
-	const char* website;
-	const char* license;
-} MDP_Desc_t;
-
-
-// Plugin types.
-enum MDP_Type
-{
-	MDPT_NULL = 0,
-	MDPT_RENDER,
-};
-
-
-// Basic plugin definition struct.
-typedef void (GENS_FNCALL *mdp_init)(void);
-typedef void (GENS_FNCALL *mdp_end)(void);
-
-typedef struct
-{
-	const uint32_t interfaceVersion;
-	const uint32_t pluginVersion;
-	const uint32_t type;
-	const uint32_t reserved1;
-	
-	// UUID - each plugin must have a unique ID.
-	const unsigned char uuid[16];
-	
-	// CPU flag information.
-	const uint32_t cpuFlagsSupported;
-	const uint32_t cpuFlagsRequired;
-	
-	// Plugin description.
-	MDP_Desc_t *desc;
-	
-	// Init/Shutdown functions
-	mdp_init init;
-	mdp_end end;
-	
-	// Struct definition for the specified plugin type.
-	void *plugin_t;
-} MDP_t;
-
+// Function pointer calling conventions from SDL's begin_code.h
+// TODO: Combine this with the #defines from compress.h
+#ifndef GENS_FNCALL
+#if defined(__WIN32__) && !defined(__GNUC__)
+#define GENS_FNCALL __cdecl
+#else
+#ifdef __OS2__
+#define GENS_FNCALL _System
+#else
+#define GENS_FNCALL
+#endif
+#endif
+#endif
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* GENS_MDP_H */
+#endif /* GENS_MDP_FNCALL_H */
