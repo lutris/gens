71ef7739de79e8e6c2b959dc2bec650db5df45fb
David Korth <gerbilsoft@verizon.net>
2008-12-08 16:56:59 -0500

    mdp/mdp_fncall.h: Added visibility defines.
    See http://gcc.gnu.org/wiki/Visibility for more information.

--- src/mdp/mdp_fncall.h
+++ src/mdp/mdp_fncall.h
@@ -41,6 +41,32 @@ extern "C" {
 #endif
 #endif
 
+// DLL import/export.
+// See http://gcc.gnu.org/wiki/Visibility
+#if defined(_WIN32) || defined(__CYGWIN__)
+	#ifdef BUILDING_DLL
+		#ifdef __GNUC__
+			#define DLL_PUBLIC __attribute__((dllexport))
+		#else
+			#define DLL_PUBLIC __declspec(dllexport)
+		#endif
+	#else
+		#ifdef __GNUC__
+			#define DLL_PUBLIC __attribute__((dllimport))
+		#else
+			#define DLL_PUBLIC __declspec(dllimport)
+		#endif
+	#endif
+#else
+	#if __GNUC__ >= 4
+		#define DLL_PUBLIC __attribute__ ((visibility("default")))
+		#define DLL_LOCAL  __attribute__ ((visibility("hidden")))
+	#else
+		#define DLL_PUBLIC
+		#define DLL_LOCAL
+	#endif
+#endif
+
 #ifdef __cplusplus
 }
 #endif
