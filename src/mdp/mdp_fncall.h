72dc3f4c3060ad81ee54fe2d4c627f6558a08f36
David Korth <gerbilsoft@verizon.net>
2008-12-08 17:48:11 -0500

    mdp/mdp_fncall.h: DLL_LOCAL wasn't being defined on Win32.

--- src/mdp/mdp_fncall.h
+++ src/mdp/mdp_fncall.h
@@ -46,17 +46,18 @@ extern "C" {
 #if defined(_WIN32) || defined(__CYGWIN__)
 	#ifdef BUILDING_DLL
 		#ifdef __GNUC__
-			#define DLL_PUBLIC __attribute__((dllexport))
+			#define DLL_PUBLIC __attribute__ ((dllexport))
 		#else
 			#define DLL_PUBLIC __declspec(dllexport)
 		#endif
 	#else
 		#ifdef __GNUC__
-			#define DLL_PUBLIC __attribute__((dllimport))
+			#define DLL_PUBLIC __attribute__ ((dllimport))
 		#else
 			#define DLL_PUBLIC __declspec(dllimport)
 		#endif
 	#endif
+	#define DLL_LOCAL
 #else
 	#if __GNUC__ >= 4
 		#define DLL_PUBLIC __attribute__ ((visibility("default")))
