38946080d3b0354573e297c2eb262e39448f6433
David Korth <gerbilsoft@verizon.net>
2008-12-08 21:47:03 -0500

    mdp_render_scanline_x86.h: Added symbol visibility information.

--- src/mdp/render/scanline/mdp_render_scanline_x86.h
+++ src/mdp/render/scanline/mdp_render_scanline_x86.h
@@ -29,21 +29,21 @@ extern "C" {
 
 #include <stdint.h>
 
-void mdp_render_scanline_16_x86(uint16_t *destScreen, uint16_t *mdScreen,
-				int destPitch, int srcPitch,
-				int width, int height);
+void DLL_LOCAL mdp_render_scanline_16_x86(uint16_t *destScreen, uint16_t *mdScreen,
+					  int destPitch, int srcPitch,
+					  int width, int height);
 
-void mdp_render_scanline_16_x86_mmx(uint16_t *destScreen, uint16_t *mdScreen,
-				    int destPitch, int srcPitch,
-				    int width, int height);
+void DLL_LOCAL mdp_render_scanline_16_x86_mmx(uint16_t *destScreen, uint16_t *mdScreen,
+					      int destPitch, int srcPitch,
+					      int width, int height);
 
-void mdp_render_scanline_32_x86(uint32_t *destScreen, uint32_t *mdScreen,
-				int destPitch, int srcPitch,
-				int width, int height);
+void DLL_LOCAL mdp_render_scanline_32_x86(uint32_t *destScreen, uint32_t *mdScreen,
+					  int destPitch, int srcPitch,
+					  int width, int height);
 
-void mdp_render_scanline_32_x86_mmx(uint32_t *destScreen, uint32_t *mdScreen,
-				    int destPitch, int srcPitch,
-				    int width, int height);
+void DLL_LOCAL mdp_render_scanline_32_x86_mmx(uint32_t *destScreen, uint32_t *mdScreen,
+					      int destPitch, int srcPitch,
+					      int width, int height);
 
 #ifdef __cplusplus
 }
