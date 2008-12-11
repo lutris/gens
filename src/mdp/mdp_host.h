cebb98ae3af3b77d7b4a13f1a9221a244d50732e
David Korth <gerbilsoft@verizon.net>
2008-12-10 23:33:33 -0500

    mdp_host.h: Version bump mdp_host.h to MDP_VERSION(0, 2, 0).
    MDP Host Services is still binary-compatible with v0.1, but it will
    be version bumped to MDP_VERSION(1, 0, 0) for the m6.1 release.

--- src/mdp/mdp_host.h
+++ src/mdp/mdp_host.h
@@ -34,7 +34,7 @@ extern "C" {
 #include "mdp_render.h"
 
 // MDP Host Services interface version.
-#define MDP_HOST_INTERFACE_VERSION MDP_VERSION(0, 1, 0)
+#define MDP_HOST_INTERFACE_VERSION MDP_VERSION(0, 2, 0)
 
 // MDP_PTR: Pointer IDs.
 typedef enum
