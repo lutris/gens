31c42918297f0dd0f19c9403dfd639a220dc4b63
David Korth <gerbilsoft@verizon.net>
2008-12-11 20:03:24 -0500

    strip_fPIC.sh: Convert -fpic and -fPIC into -D __PIC.
    This will allow me to write nasm macros that allow for both PIC
    and non-PIC code.

--- strip_fPIC.sh
+++ strip_fPIC.sh
@@ -2,11 +2,16 @@
 #
 # libtool assumes that the compiler can handle the -fPIC flag
 # This isn't always true (for example, nasm can't handle it)
+pic="no"
 command=""
 while [ $# -gt 0 ]; do
     case "$1" in
-        -?PIC)
-            # Ignore -fPIC and -DPIC options
+        -?[pP][iI][cC])
+            # PIC - define __PIC in nasm.
+            if test "x$pic" = "xno"; then
+                command="$command -D __PIC"
+                pic="yes"
+            fi
             ;;
         -I)
             command="$command $1 $2/"
