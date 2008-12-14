9d46f72c155eca3a025d2ca8398eae8495797ee8
David Korth <gerbilsoft@verizon.net>
2008-12-13 21:36:35 -0500

    mdp_host.h: menu_item_get_text(): text_buf should be char*, not char.

--- src/mdp/mdp_host.h
+++ src/mdp/mdp_host.h
@@ -77,7 +77,7 @@ typedef struct
 	int (MDP_FNCALL *menu_item_add)(struct MDP_t *plugin, mdp_menu_handler_fn handler, int menu_id, const char *text);
 	int (MDP_FNCALL *menu_item_remove)(struct MDP_t *plugin, int menu_item_id);
 	int (MDP_FNCALL *menu_item_set_text)(struct MDP_t *plugin, int menu_item_id, const char *text);
-	int (MDP_FNCALL *menu_item_get_text)(struct MDP_t *plugin, int menu_item_id, char text_buf, int size);
+	int (MDP_FNCALL *menu_item_get_text)(struct MDP_t *plugin, int menu_item_id, char *text_buf, int size);
 	int (MDP_FNCALL *menu_item_set_checked)(struct MDP_t *plugin, int menu_item_id, int checked);
 	int (MDP_FNCALL *menu_item_get_checked)(struct MDP_t *plugin, int menu_item_id);
 } MDP_Host_t;
