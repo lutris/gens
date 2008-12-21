d857f3a6c4708f8d150973ec8ee9f0bd87dcd751
David Korth <gerbilsoft@verizon.net>
2008-12-20 23:09:39 -0500

    fw_32x.c: Added the reverse-engineered 32X firmware to Gens/GS.
    It isn't used yet.

---
+++ src/gens/emulator/fw_32x.h
@@ -0,0 +1,31 @@
+/***************************************************************************
+ * Gens: Reverse-engineered 32X firmware.                                  *
+ *                                                                         *
+ * Copyright (c) 2005 by DevSter (Joseph Norman)                           *
+ * Visit DevSter's website: http://devster.monkeeh.com/segapage.html       *
+ *                                                                         *
+ * Gens/GS port Copyright (c) 2008 by David Korth                          *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef GENS_FW_32X_H
+#define GENS_FW_32X_H
+
+const unsigned char fw_32X_mc68000[272];
+const unsigned char fw_32X_msh2[712];
+const unsigned char fw_32X_ssh2[532];
+
+#endif /* GENS_FW_32X_H */
