3df081bf586d57afb75a6e02bb7bfed783442c9c
David Korth <gerbilsoft@verizon.net>
2009-01-14 17:09:48 -0500

    mdZ80_opcodes.h: Added an extra space at the top of the file.

--- src/mdZ80/mdZ80_opcodes.h
+++ src/mdZ80/mdZ80_opcodes.h
@@ -21,6 +21,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
+
 /**
  * Z80 opcodes: 1-byte opcodes.
  */
