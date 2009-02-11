76073753743c2b37e659281d2c245948ac7fb206
David Korth <gerbilsoft@verizon.net>
2009-02-10 22:52:01 -0500

    input_sdl_key_names.c: Added all of the SDL key names.

--- src/gens/input/input_sdl_key_names.c
+++ src/gens/input/input_sdl_key_names.c
@@ -208,50 +208,111 @@ int input_sdl_gdk_to_sdl_keyval(int gdk_key)
  */
 int input_sdl_get_key_name(uint16_t key, char* buf, int size)
 {
-	const char* key_name;
-	
-	// TODO: Add more comprehensive SDL key mapping.
-	switch (key)
+	static const char* const sdl_keys[] =
 	{
-		case SDLK_UP:
-			key_name = "Up";
-			break;
-		case SDLK_DOWN:
-			key_name = "Down";
-			break;
-		case SDLK_LEFT:
-			key_name = "Left";
-			break;
-		case SDLK_RIGHT:
-			key_name = "Right";
-			break;
-		case SDLK_RETURN:
-			key_name = "Enter";
-			break;
-		case SDLK_LSHIFT:
-			key_name = "Left Shift";
-			break;
-		case SDLK_RSHIFT:
-			key_name = "Right Shift";
-			break;
-		default:
-			if (key >= 'A' && key <= 'Z')
-			{
-				buf[0] = key;
-				buf[1] = 0x00;
-				return 0;
-			}
-			else if (key >= 'a' && key <= 'z')
-			{
-				buf[0] = key - ('a' - 'A');
-				buf[1] = 0x00;
-				return 0;
-			}
-			
-			key_name = NULL;
-			break;
+		// 0x00 - 0x1F
+		NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+		"Backspace", "Tab", NULL, NULL, "Clear", "Enter", NULL, NULL,
+		NULL, NULL, NULL, "Pause", NULL, NULL, NULL, NULL,
+		NULL, NULL, NULL, "Escape", NULL, NULL, NULL, NULL,
+		
+		// 0x20 - 0x3F
+		"Space", "!", "\"", "#", "$", NULL, "&", "'",
+		"(", ")", "*", "+", ",", "-", ".", "/",
+		"0", "1", "2", "3", "4", "5", "6", "7",
+		"8", "9", ":", ";", "<", "=", ">", "?",
+		
+		// 0x40 - 0x5F
+		// Uppercase letters are ignored by SDL.
+		"@", NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+		NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+		NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+		NULL, NULL, NULL, "[", "\\", "]", "^", "_",
+		
+		// 0x60 - 0x7F
+		"`", "A", "B", "C", "D", "E", "F", "G",
+		"H", "I", "J", "K", "L", "M", "N", "O",
+		"P", "Q", "R", "S", "T", "U", "V", "W",
+		"X", "Y", "Z", NULL, NULL, NULL, NULL, "Delete",
+		
+		// 0x80 - 0x9F
+		NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+		NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+		NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+		NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+		
+		// 0xA0 - 0xAF
+		// International keyboard symbols.
+		"World 0x00", "World 0x01", "World 0x02", "World 0x03",
+		"World 0x04", "World 0x05", "World 0x06", "World 0x07",
+		"World 0x08", "World 0x09", "World 0x0A", "World 0x0B",
+		"World 0x0C", "World 0x0D", "World 0x0E", "World 0x0F",
+		
+		// 0xB0 - 0xBF
+		// International keyboard symbols.
+		"World 0x10", "World 0x11", "World 0x12", "World 0x13",
+		"World 0x14", "World 0x15", "World 0x16", "World 0x17",
+		"World 0x18", "World 0x19", "World 0x1A", "World 0x1B",
+		"World 0x1C", "World 0x1D", "World 0x1E", "World 0x1F",
+		
+		// 0xC0 - 0xCF
+		// International keyboard symbols.
+		"World 0x20", "World 0x21", "World 0x22", "World 0x23",
+		"World 0x24", "World 0x25", "World 0x26", "World 0x27",
+		"World 0x28", "World 0x29", "World 0x2A", "World 0x2B",
+		"World 0x2C", "World 0x2D", "World 0x2E", "World 0x2F",
+		
+		// 0xD0 - 0xDF
+		// International keyboard symbols.
+		"World 0x30", "World 0x31", "World 0x32", "World 0x33",
+		"World 0x34", "World 0x35", "World 0x36", "World 0x37",
+		"World 0x38", "World 0x39", "World 0x3A", "World 0x3B",
+		"World 0x3C", "World 0x3D", "World 0x3E", "World 0x3F",
+		
+		// 0xE0 - 0xEF
+		// International keyboard symbols.
+		"World 0x40", "World 0x41", "World 0x42", "World 0x43",
+		"World 0x44", "World 0x45", "World 0x46", "World 0x47",
+		"World 0x48", "World 0x49", "World 0x4A", "World 0x4B",
+		"World 0x4C", "World 0x4D", "World 0x4E", "World 0x4F",
+		
+		// 0xF0 - 0xFF
+		// International keyboard symbols.
+		"World 0x50", "World 0x51", "World 0x52", "World 0x53",
+		"World 0x54", "World 0x55", "World 0x56", "World 0x57",
+		"World 0x58", "World 0x59", "World 0x5A", "World 0x5B",
+		"World 0x5C", "World 0x5D", "World 0x5E", "World 0x5F",
+
+		// 0x100 - 0x10F
+		// Numeric Keypad
+		"NumPad 0", "NumPad 1", "NumPad 2", "NumPad 3",
+		"NumPad 4", "NumPad 5", "NumPad 6", "NumPad 7",
+		"NumPad 8", "NumPad 9", "NumPad .", "NumPad /",
+		"NumPad *", "NumPad -", "NumPad +", "NumPad Enter",
+		
+		// 0x110 - 0x12F
+		"NumPad =", "Up", "Down", "Right", "Left", "Insert", "Home", "End",
+		"Page Up", "PageDown", "F1", "F2", "F3", "F4", "F5", "F6",
+		"F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14",
+		"F15", NULL, NULL, NULL, "Num Lock", "Caps Lock", "Scroll Lock", "Right Shift",
+		
+		// 0x130 - 0x13F
+		"Left Shift", "Right Ctrl", "Left Ctrl", "Right Alt",
+		"Left Alt", "Right Meta", "Left Meta", "Right Super",
+		"Left Super", "Alt Gr", "Compose", "Help",
+		"Print Screen", "SysRq", "Break", "Menu",
+		
+		// 0x140 - 0x142
+		"Power", "Euro", "Undo"
+	};
+
+	if (key > (sizeof(sdl_keys) / sizeof(const char*)))
+	{
+		// Unknown key.
+		return -1;
 	}
-	
+
+	const char* key_name = sdl_keys[key];
 	if (!key_name)
 	{
 		// Unknown key.
