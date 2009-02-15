a7ce14b12927af2fd48d4705bbcb6e32b8e3a8f4
David Korth <gerbilsoft@verizon.net>
2009-02-15 02:11:14 -0500

    macros/debug_msg.h: Swapped the order of DEBUG_CHANNEL_psg and DEBUG_CHANNEL_pcm.

--- src/gens/macros/debug_msg.h
+++ src/gens/macros/debug_msg.h
@@ -28,8 +28,8 @@
 #define DEBUG_CHANNEL_gens	1
 #define DEBUG_CHANNEL_mdp	1
 #define DEBUG_CHANNEL_ym2612	0
-#define DEBUG_CHANNEL_pcm	0
 #define DEBUG_CHANNEL_psg	0
+#define DEBUG_CHANNEL_pcm	0
 
 /**
  * DEBUG_MSG(): Output a debug message.
