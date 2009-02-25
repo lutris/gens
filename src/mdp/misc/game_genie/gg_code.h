513980851dc764f13b5fc62a5577522f3e80c3b9
David Korth <gerbilsoft@verizon.net>
2009-02-24 21:34:31 -0500

    mdp/misc/game_genie/: Updated copyright headers.

--- src/mdp/misc/game_genie/gg_code.h
+++ src/mdp/misc/game_genie/gg_code.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- * [MDP] Game Genie - Code Handling Functions.                             *
+ * MDP: Game Genie - Code Handling Functions.                              *
  *                                                                         *
  * Copyright (c) 1999-2002 by Stéphane Dallongeville                       *
  * Copyright (c) 2003-2004 by Stéphane Akhoun                              *
--- src/mdp/misc/game_genie/gg_icon.h
+++ src/mdp/misc/game_genie/gg_icon.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Gens: [MDP] Game Genie. (Icon Data File)                                *
+ * MDP: Game Genie. (Icon Data File)                                       *
  *                                                                         *
  * Copyright (c) 1999-2002 by Stéphane Dallongeville                       *
  * Copyright (c) 2003-2004 by Stéphane Akhoun                              *
