8d1ac38cd0b5db4bb847b1a95af3bfabf764218c
David Korth <gerbilsoft@verizon.net>
2009-03-05 11:53:42 -0500

    Revert "cc_window.c: Don't enable a horizontal scrollbar in the "Detected Input Devices" listbox."
    This reverts commit c5729414535ef0837a0dfa756d86c922db03b3c1.
    
    The horizontal scrollbar is needed in order to decrease the width of the
    "Detected Input Devices" box.

--- src/gens/ui/gtk/controller_config/cc_window.c
+++ src/gens/ui/gtk/controller_config/cc_window.c
@@ -366,7 +366,7 @@ static void cc_window_create_input_devices_frame(GtkWidget *container)
 	GtkWidget *scrlInputDevices = gtk_scrolled_window_new(NULL, NULL);
 	gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrlInputDevices), GTK_SHADOW_IN);
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrlInputDevices),
-				       GTK_POLICY_NEVER,
+				       GTK_POLICY_AUTOMATIC,
 				       GTK_POLICY_AUTOMATIC);
 	gtk_container_set_border_width(GTK_CONTAINER(scrlInputDevices), 4);
 	gtk_widget_show(scrlInputDevices);
