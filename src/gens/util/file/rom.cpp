bc7966098a4ddc529101ea7698477dbfe0ec065c
David Korth <gerbilsoft@verizon.net>
2009-03-05 18:26:13 -0500

    src/gens/util/file/rom.cpp: Use LOG_MSG() to log messages.
    Also, added copyright headers.

--- src/gens/util/file/rom.cpp
+++ src/gens/util/file/rom.cpp
@@ -1,9 +1,33 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cassert>
-#include <ctype.h>
+/***************************************************************************
+ * Gens: ROM handler.                                                      *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville                       *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun                              *
+ * Copyright (c) 2008-2009 by David Korth                                  *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
 
+// Message logging.
+#include "macros/log_msg.h"
+
+// C includes.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include <fcntl.h>
 
 // C++ includes
@@ -518,7 +542,8 @@ unsigned int ROM::loadROM(const string& filename, ROM_t** retROM)
 	if (!cmp)
 	{
 		// No usable decompressors found.
-		fprintf(stderr, "ROM::loadROM(): No usable decompressors found.\n");
+		LOG_MSG(gens, LOG_MSG_LEVEL_WARNING,
+			"No usable decompressors found.");
 		fclose(fROM);
 		Game = NULL;
 		*retROM = NULL;
@@ -569,6 +594,7 @@ unsigned int ROM::loadROM(const string& filename, ROM_t** retROM)
 	}
 	
 	// Determine the ROM type.
+	// TODO: Show an error if the ROM can't be opened.
 	unsigned char detectBuf[2048];
 	cmp->get_file(fROM, filename.c_str(), sel_file, detectBuf, sizeof(detectBuf));
 	romType = detectFormat(detectBuf);
