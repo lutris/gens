33d29e836c843052db8318397ce9a9153ac289e8
David Korth <gerbilsoft@verizon.net>
2009-04-08 21:00:14 -0400

    cpuflags.c: #include <config.h> - getCPUFlags() checks the GENS_X86_ASM define.

--- src/gens/gens_core/misc/cpuflags.c
+++ src/gens/gens_core/misc/cpuflags.c
@@ -20,6 +20,10 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include "cpuflags.h"
 
 #include <stdint.h>
