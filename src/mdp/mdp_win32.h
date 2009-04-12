dda65ece99b99738fa40ee473c2f399ecdad8da0
David Korth <gerbilsoft@verizon.net>
2009-04-11 21:25:09 -0400

    src/mdp/*.h: Adjusted copyrights for most of the MDP headers.
    Most of the MDP stuff is my original work, so I removed the copyrights
    for other people who didn't create the MDP stuff. mdp_render still has
    the old copyrights, since the MDP renderer code is directly based on
    the old Gens renderer subsystem.

--- src/mdp/mdp_win32.h
+++ src/mdp/mdp_win32.h
@@ -1,8 +1,6 @@
 /***************************************************************************
  * MDP: Mega Drive Plugins - Win32 convenience functions.                  *
  *                                                                         *
- * Copyright (c) 1999-2002 by Stéphane Dallongeville                       *
- * Copyright (c) 2003-2004 by Stéphane Akhoun                              *
  * Copyright (c) 2008-2009 by David Korth                                  *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
