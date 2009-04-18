c1f757e2e2bdf543c7caf3557b55634f557144a4
David Korth <gerbilsoft@verizon.net>
2009-04-18 16:06:26 -0400

    mdp_host_gens_reg.c: Added M68K register get.

--- src/gens/plugins/mdp_host_gens_reg.c
+++ src/gens/plugins/mdp_host_gens_reg.c
@@ -28,6 +28,7 @@
 #include "mdp/mdp_error.h"
 #include "mdp/mdp_reg.h"
 
+#include "gens_core/cpu/68k/star_68k.h"
 /*
 #include "gens_core/mem/mem_m68k.h"
 #include "gens_core/mem/mem_s68k.h"
@@ -41,10 +42,67 @@
 extern int ice;
 
 
+/** Register Read Functions. **/
+
+static int mdp_host_reg_get_68k(struct S68000CONTEXT *context, int regID, uint32_t *ret_value)
+{
+	switch (regID)
+	{
+		case MDP_REG_68K_D0:
+		case MDP_REG_68K_D1:
+		case MDP_REG_68K_D2:
+		case MDP_REG_68K_D3:
+		case MDP_REG_68K_D4:
+		case MDP_REG_68K_D5:
+		case MDP_REG_68K_D6:
+		case MDP_REG_68K_D7:
+			*ret_value = context->dreg[regID - MDP_REG_68K_D0];
+			break;
+			
+		case MDP_REG_68K_A0:
+		case MDP_REG_68K_A1:
+		case MDP_REG_68K_A2:
+		case MDP_REG_68K_A3:
+		case MDP_REG_68K_A4:
+		case MDP_REG_68K_A5:
+		case MDP_REG_68K_A6:
+		case MDP_REG_68K_A7:
+		/*case MDP_REG_68K_SP:*/
+			*ret_value = context->areg[regID - MDP_REG_68K_A0];
+			break;
+		
+		case MDP_REG_68K_ASP:
+			*ret_value = context->asp;
+			break;
+		
+		case MDP_REG_68K_PC:
+			*ret_value = context->pc;
+			break;
+		
+		case MDP_REG_68K_SR:
+			*ret_value = context->sr;
+			break;
+		
+		default:
+			return -MDP_ERR_REG_INVALID_REGID;
+	}
+	
+	return MDP_ERR_OK;
+}
+
+
 int MDP_FNCALL mdp_host_reg_get(int icID, int regID, uint32_t *ret_value)
 {
-	// TODO
-	return -MDP_ERR_FUNCTION_NOT_IMPLEMENTED;
+	if (!ret_value)
+		return -MDP_ERR_INVALID_PARAMETERS;
+	
+	switch (icID)
+	{
+		case MDP_REG_IC_M68K:
+			return mdp_host_reg_get_68k(&main68k_context, regID, ret_value);
+		default:
+			return -MDP_ERR_REG_INVALID_ICID;
+	}
 }
 
 
