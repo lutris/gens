1a5020372fcd6825292fd08c7472ca053633c15c
David Korth <gerbilsoft@verizon.net>
2009-04-18 16:19:21 -0400

    mdp_host_gens_reg.c: Added VDP register get.

--- src/gens/plugins/mdp_host_gens_reg.c
+++ src/gens/plugins/mdp_host_gens_reg.c
@@ -28,21 +28,23 @@
 #include "mdp/mdp_error.h"
 #include "mdp/mdp_reg.h"
 
+#include "util/file/rom.hpp"
+
 #include "gens_core/cpu/68k/star_68k.h"
+#include "gens_core/vdp/vdp_io.h"
 /*
 #include "gens_core/mem/mem_m68k.h"
 #include "gens_core/mem/mem_s68k.h"
 #include "gens_core/mem/mem_z80.h"
 #include "gens_core/mem/mem_sh2.h"
-#include "gens_core/vdp/vdp_io.h"
 #include "gens_core/vdp/vdp_rend.h"
-#include "util/file/rom.hpp"
 */
 
 extern int ice;
 
 
 /** Register Read Functions. **/
+
 
 static int mdp_host_reg_get_68k(struct S68000CONTEXT *context, int regID, uint32_t *ret_value)
 {
@@ -91,8 +93,22 @@ static int mdp_host_reg_get_68k(struct S68000CONTEXT *context, int regID, uint32
 }
 
 
+static int mdp_host_reg_get_vdp(int regID, uint32_t *ret_value)
+{
+	if (regID < 0 || regID >= 24)
+		return -MDP_ERR_REG_INVALID_REGID;
+	
+	// VDP_Reg is an array of 24 unsigned ints.
+	unsigned int *vdp_reg_int = (unsigned int*)(&VDP_Reg);
+	*ret_value = vdp_reg_int[regID] & 0xFF;
+	return MDP_ERR_OK;
+}
+
+
 int MDP_FNCALL mdp_host_reg_get(int icID, int regID, uint32_t *ret_value)
 {
+	if (!Game)
+		return -MDP_ERR_ROM_NOT_LOADED;
 	if (!ret_value)
 		return -MDP_ERR_INVALID_PARAMETERS;
 	
@@ -100,6 +116,8 @@ int MDP_FNCALL mdp_host_reg_get(int icID, int regID, uint32_t *ret_value)
 	{
 		case MDP_REG_IC_M68K:
 			return mdp_host_reg_get_68k(&main68k_context, regID, ret_value);
+		case MDP_REG_IC_VDP:
+			return mdp_host_reg_get_vdp(regID, ret_value);
 		default:
 			return -MDP_ERR_REG_INVALID_ICID;
 	}
