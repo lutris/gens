4d2177bff4ce84455fd41270910049e4238d982a
David Korth <gerbilsoft@verizon.net>
2009-04-30 14:52:57 -0400

    md_palette.cpp: Rewrote Recalculate_Palettes() to use templated functions based on color depth.

--- src/gens/emulator/md_palette.cpp
+++ src/gens/emulator/md_palette.cpp
@@ -34,27 +34,27 @@ int Invert_Color;
 
 /**
  * Constrain_Color_Component(): Constrains a color component.
- * @param c Color component.
- * @return Constrained color component.
+ * @param mask Color component mask. (max value)
+ * @param c Color component to constrain.
  */
-static inline short constrainColorComponent(short c)
+template<int mask>
+static inline void T_constrainColorComponent(int& c)
 {
 	if (c < 0)
-		return 0;
-	else if (c > 0x3F)
-		return 0x3F;
-	return c;
+		c = 0;
+	else if (c > mask)
+		c = mask;
 }
 
 
 /**
- * CalculateGrayScale(): Calculates grayscale color values.
+ * calculateGrayScale(): Calculates grayscale color values.
  * @param r Red component.
  * @param g Green component.
  * @param b Blue component.
  * @return Grayscale value.
  */
-static inline int CalculateGrayScale(int r, int g, int b)
+static inline int calculateGrayScale(int r, int g, int b)
 {
 	// Standard grayscale computation: Y = R*0.30 + G*0.59 + B*0.11
 	r = (r * (unsigned int) (0.30 * 65536.0)) >> 16;
@@ -64,178 +64,154 @@ static inline int CalculateGrayScale(int r, int g, int b)
 }
 
 
-#define PALETTE_ADJUST_BRIGHTNESS(r, g, b, brightness)	\
-{							\
-	if (brightness != 0)				\
-	{						\
-		r += brightness;			\
-		g += brightness;			\
-		b += brightness;			\
-							\
-		r = constrainColorComponent(r);		\
-		g = constrainColorComponent(g);		\
-		b = constrainColorComponent(b);		\
-	}						\
-}
-
-
-#define PALETTE_ADJUST_CONTRAST(r, g, b, contrast)	\
-{							\
-	if (contrast != 100)				\
-	{						\
-		r = (r * contrast) / 100;		\
-		g = (g * contrast) / 100;		\
-		b = (b * contrast) / 100;		\
-							\
-		r = constrainColorComponent(r);		\
-		g = constrainColorComponent(g);		\
-		b = constrainColorComponent(b);		\
-	}						\
+static inline void adjustContrast(int& r, int& g, int& b, int contrast)
+{
+	if (contrast == 100)
+		return;
+	
+	r = (r * contrast) / 100;
+	g = (g * contrast) / 100;
+	b = (b * contrast) / 100;
 }
 
 
 /**
  * Recalculate_Palettes(): Recalculates the MD and 32X palettes for brightness, contrast, and various effects.
  */
-void Recalculate_Palettes(void)
+template<typename pixel, pixel *palMD, pixel *pal32X, pixel *cramAdjusted32X,
+	 int RBits, int GBits, int BBits,
+	 int RMask, int GMask, int BMask>
+static inline void T_Recalculate_Palettes(void)
 {
-	int i;
-	short r, g, b;
-	short rf, gf, bf;
-	unsigned short color;
+	int r, g, b;
 	
 	// Brightness / Contrast
 	// These values are scaled to positive numbers.
 	// Normal brightness: (Brightness_Level == 100)
 	// Normal contrast:   (  Contrast_Level == 100)
-	const short brightness = ((Brightness_Level - 100) * 32) / 100;
-	const short contrast = Contrast_Level;
+	const int brightness = (Brightness_Level - 100);
+	const int brightness_R = (brightness * (RMask+1)) / 100;
+	const int brightness_G = (brightness * (GMask+1)) / 100;
+	const int brightness_B = (brightness * (BMask+1)) / 100;
 	
+	const int contrast = Contrast_Level;
+
 	// Calculate the MD palette.
-	for (r = 0; r < 0x10; r++)
+	for (unsigned int i = 0x0000; i < 0x1000; i++)
 	{
-		for (g = 0; g < 0x10; g++)
+		// Mask off the LSB of each MD color component.
+		unsigned int color = (i & 0x0EEE);
+		
+		r = (color & 0xF) << (RBits - 4);
+		g = ((color >> 4) & 0x0F) << (GBits - 4);
+		b = ((color >> 8) & 0x0F) << (BBits - 4);
+		
+		// Adjust brightness.
+		if (brightness != 0)
 		{
-			for (b = 0; b < 0x10; b++)
-			{
-				color = (b << 8) | (g << 4) | r;
-				
-				rf = (r & 0xE) << 2;
-				gf = (g & 0xE) << 2;
-				bf = (b & 0xE) << 2;
-				
-				// Compute colors here (64 levels)
-				PALETTE_ADJUST_BRIGHTNESS(rf, gf, bf, brightness);
-				PALETTE_ADJUST_CONTRAST(rf, gf, bf, contrast);
-				
-				// 32-bit palette
-				Palette32[color] = (rf << 18) | (gf << 10) | (bf << 2);
-				
-				// 16-bit palette
-				if (bppMD == 15)
-				{
-					rf = ((rf >> 1) & 0x1F) << 10;
-					gf = ((gf >> 1) & 0x1F) << 5;
-				}
-				else //if (bppMD == 16)
-				{
-					rf = ((rf >> 1) & 0x1F) << 11;
-					gf = ((gf >> 0) & 0x3F) << 5;
-				}
-				bf = (bf >> 1) & 0x1F;
-				
-				Palette[color] = rf | gf | bf;
-			}
+			r += brightness_R;
+			g += brightness_G;
+			b += brightness_B;
 		}
+		
+		// Adjust contrast.
+		adjustContrast(r, g, b, contrast);
+		
+		// Constrain the color components.
+		T_constrainColorComponent<RMask>(r);
+		T_constrainColorComponent<GMask>(g);
+		T_constrainColorComponent<BMask>(b);
+		
+		// Create the color.
+		palMD[i] = (r << (GBits + BBits)) |
+			   (g << (BBits)) |
+			   (b);
 	}
 	
 	// Calculate the 32X palette.
-	for (i = 0; i < 0x10000; i++)
+	for (unsigned int i = 0; i < 0x10000; i++)
 	{
-		bf = ((i >> 10) & 0x1F) << 1;
-		gf = ((i >> 5) & 0x1F) << 1;
-		rf = ((i >> 0) & 0x1F) << 1;
+		r = (i & 0x1F) << (RBits - 5);
+		g = ((i >> 5) & 0x1F) << (GBits - 5);
+		b = ((i >> 10) & 0x1F) << (BBits - 5);
 		
-		// Compute colors here (64 levels)
-		PALETTE_ADJUST_BRIGHTNESS(rf, gf, bf, brightness);
-		PALETTE_ADJUST_CONTRAST(rf, gf, bf, contrast);
-		
-		// 32-bit palette
-		_32X_Palette_32B[i] = (rf << 18) | (gf << 10) | (bf << 2);
-		
-		// 16-bit palette
-		if (bppMD == 15)
+		// Adjust brightness.
+		if (brightness != 0)
 		{
-			rf = ((rf >> 1) & 0x1F) << 10;
-			gf = ((gf >> 1) & 0x1F) << 5;
+			r += brightness_R;
+			g += brightness_G;
+			b += brightness_B;
 		}
-		else //if (bppMD == 16)
-		{
-			rf = ((rf >> 1) & 0x1F) << 11;
-			gf = ((gf >> 0) & 0x3F) << 5;
-		}
-		bf = (bf >> 1) << 0;
 		
-		_32X_Palette_16B[i] = rf | gf | bf;
+		// Adjust contrast.
+		adjustContrast(r, g, b, contrast);
+		
+		// Constrain the color components.
+		T_constrainColorComponent<RMask>(r);
+		T_constrainColorComponent<GMask>(g);
+		T_constrainColorComponent<BMask>(b);
+		
+		// Create the color.
+		pal32X[i] = (r << (GBits + BBits)) |
+			    (g << (BBits)) |
+			    (b);
 	}
 	
 	// Convert colors to grayscale, if necessary.
 	if (Greyscale)
 	{
+		int gray;
+		
 		// MD palette.
-		for (i = 0; i < 0x1000; i++)
+		for (unsigned int i = 0; i < 0x1000; i++)
 		{
-			r = (Palette32[i] >> 16) & 0xFF;
-			g = (Palette32[i] >> 8) & 0xFF;
-			b = Palette32[i] & 0xFF;
+			r = (palMD[i] >> (GBits + BBits)) & RMask;
+			g = (palMD[i] >> (BBits)) & GMask;
+			b = palMD[i] & BMask;
 			
-			r = g = b = CalculateGrayScale(r, g, b);
+			// Convert to 24-bit color.
+			r <<= (8 - RBits);
+			g <<= (8 - GBits);
+			b <<= (8 - BBits);
 			
-			// 32-bit color
-			Palette32[i] = (r << 16) | (g << 8) | b;
+			// Calculate the grayscale value.
+			gray = calculateGrayScale(r, g, b);
 			
-			// 15/16-bit color
-			if (bppMD == 15)
-			{
-				r = ((r >> 3) & 0x1F) << 10;
-				g = ((g >> 3) & 0x1F) << 5;
-			}
-			else //if (bppMD == 16)
-			{
-				r = ((r >> 3) & 0x1F) << 11;
-				g = ((g >> 2) & 0x3F) << 5;
-			}
-			b = (b >> 3) & 0x1F;
+			// Convert back to the original color depth.
+			r = (gray >> (8 - RBits)) & RMask;
+			g = (gray >> (8 - GBits)) & GMask;
+			b = (gray >> (8 - BBits)) & BMask;
 			
-			Palette[i] = r | g | b;
+			// Create the color.
+			palMD[i] = (r << (GBits + BBits)) |
+				   (g << (BBits)) |
+				   (b);
 		}
 		
 		// 32X palette.
-		for (i = 0; i < 0x10000; i++)
+		for (unsigned i = 0; i < 0x10000; i++)
 		{
-			r = (_32X_Palette_32B[i] >> 16) & 0xFF;
-			g = (_32X_Palette_32B[i] >> 8) & 0xFF;
-			b = _32X_Palette_32B[i] & 0xFF;
+			r = (pal32X[i] >> (GBits + BBits)) & RMask;
+			g = (pal32X[i] >> (BBits)) & GMask;
+			b = pal32X[i] & BMask;
 			
-			r = g = b = CalculateGrayScale(r, g, b);
+			// Convert to 24-bit color.
+			r <<= (8 - RBits);
+			g <<= (8 - GBits);
+			b <<= (8 - BBits);
 			
-			// 32-bit color
-			_32X_Palette_32B[i] = (r << 16) | (g << 8) | b;
+			// Calculate the grayscale value.
+			gray = calculateGrayScale(r, g, b);
 			
-			// 15/16-bit color
-			if (bppMD == 15)
-			{
-				r = ((r >> 3) & 0x1F) << 10;
-				g = ((g >> 3) & 0x1F) << 5;
-			}
-			else //if (bppMD == 16)
-			{
-				r = ((r >> 3) & 0x1F) << 11;
-				g = ((g >> 2) & 0x3F) << 5;
-			}
-			b = (b >> 3) & 0x1F;
+			// Convert back to the original color depth.
+			r = (gray >> (8 - RBits)) & RMask;
+			g = (gray >> (8 - GBits)) & GMask;
+			b = (gray >> (8 - BBits)) & BMask;
 			
-			_32X_Palette_16B[i] = r | g | b;
+			// Create the color.
+			pal32X[i] = (r << (GBits + BBits)) |
+				    (g << (BBits)) |
+				    (b);
 		}
 	}
 	
@@ -243,27 +219,36 @@ void Recalculate_Palettes(void)
 	if (Invert_Color)
 	{
 		// MD palette.
-		for (i = 0; i < 0x1000; i++)
+		for (unsigned int i = 0; i < 0x1000; i++)
 		{
-			Palette[i] ^= 0xFFFF;
-			Palette32[i] ^= 0xFFFFFF;
+			palMD[i] = ~palMD[i];
 		}
 		
 		// 32X palette.
-		for (i = 0; i < 0x10000; i++)
+		for (unsigned int i = 0; i < 0x10000; i++)
 		{
-			_32X_Palette_16B[i] ^= 0xFFFF;
-			_32X_Palette_32B[i] ^= 0xFFFFFF;
+			pal32X[i] = ~pal32X[i];
 		}
 	}
 	
-	// Adjust 32X VDP CRAM.
-	for (i = 0; i < 0x100; i++)
+	// Adjust the 32X VDP CRAM.
+	for (unsigned int i = 0; i < 0x100; i++)
 	{
-		_32X_VDP_CRam_Ajusted[i] = _32X_Palette_16B[_32X_VDP_CRam[i]];
-		_32X_VDP_CRam_Ajusted32[i] = _32X_Palette_32B[_32X_VDP_CRam[i]];
+		cramAdjusted32X[i] = pal32X[_32X_VDP_CRam[i]];
 	}
 	
 	// Set CRam_Flag.
 	CRam_Flag = 1;
+}
+
+
+void Recalculate_Palettes(void)
+{
+	if (bppMD == 15)
+		T_Recalculate_Palettes<uint16_t, MD_Palette, _32X_Palette_16B, _32X_VDP_CRam_Ajusted, 5, 5, 5, 0x1F, 0x1F, 0x1F>();
+	else //if (bppMD == 16)
+		T_Recalculate_Palettes<uint16_t, MD_Palette, _32X_Palette_16B, _32X_VDP_CRam_Ajusted, 5, 6, 5, 0x1F, 0x3F, 0x1F>();
+	
+	// 32-bit color.
+	T_Recalculate_Palettes<uint32_t, MD_Palette32, _32X_Palette_32B, _32X_VDP_CRam_Ajusted32, 8, 8, 8, 0xFF, 0xFF, 0xFF>();
 }
