80a7e288bd3bed52d386a82d16b4b3c554db4d11
David Korth <gerbilsoft@verizon.net>
2009-05-23 18:36:37 -0400

    mdp_render_blargg_ntsc.c: Initial configuration saving support.

--- src/mdp/render/blargg_ntsc/mdp_render_blargg_ntsc.c
+++ src/mdp/render/blargg_ntsc/mdp_render_blargg_ntsc.c
@@ -25,8 +25,10 @@
 
 #include "mdp_render_blargg_ntsc.h"
 #include "mdp_render_blargg_ntsc_plugin.h"
-#include "ntsc_window.h"
 #include "md_ntsc.hpp"
+
+#include "ntsc_window.h"
+#include "ntsc_window_common.h"
 
 // MDP includes.
 #include "mdp/mdp_stdint.h"
@@ -37,6 +39,7 @@ mdp_host_t *ntsc_host_srv = NULL;
 static int ntsc_menuItemID = 0;
 
 static int MDP_FNCALL ntsc_menu_handler(int menu_item_id);
+static int MDP_FNCALL ntsc_event_handler(int event_id, void *event_info);
 
 
 /**
@@ -64,6 +67,10 @@ int MDP_FNCALL mdp_render_blargg_ntsc_init(mdp_host_t *host_srv)
 		ntsc_menuItemID = 0;
 	}
 	
+	// Register configuration load/save events.
+	ntsc_host_srv->event_register(&mdp, MDP_EVENT_LOAD_CONFIG, ntsc_event_handler);
+	ntsc_host_srv->event_register(&mdp, MDP_EVENT_SAVE_CONFIG, ntsc_event_handler);
+	
 	// Plugin has been initialized.
 	return MDP_ERR_OK;
 }
@@ -77,6 +84,10 @@ int MDP_FNCALL mdp_render_blargg_ntsc_end(void)
 {
 	if (!ntsc_host_srv)
 		return MDP_ERR_OK;
+	
+	// Unregister configuration load/save events.
+	ntsc_host_srv->event_unregister(&mdp, MDP_EVENT_LOAD_CONFIG, ntsc_event_handler);
+	ntsc_host_srv->event_unregister(&mdp, MDP_EVENT_SAVE_CONFIG, ntsc_event_handler);
 	
 	// Remove the menu item.
 	if (ntsc_menuItemID != 0)
@@ -108,5 +119,62 @@ static int MDP_FNCALL ntsc_menu_handler(int menu_item_id)
 	
 	// Show the NTSC Plugin Options window.
 	ntsc_window_show(ntsc_host_srv->window_get_main());
+	return MDP_ERR_OK;
+}
+
+
+/**
+ * ntsc_event_handler(): Event handler function.
+ * @param event_id Event ID.
+ * @param event_info Event information.
+ * @return MDP error code.
+ */
+static int MDP_FNCALL ntsc_event_handler(int event_id, void *event_info)
+{
+	int i;
+	char buf[128];
+	
+	switch (event_id)
+	{
+		case MDP_EVENT_LOAD_CONFIG:
+			// Load NTSC configuration.
+			// TODO
+			break;
+		
+		case MDP_EVENT_SAVE_CONFIG:
+			// Save NTSC configuration.
+			
+			// Check if the current configuration is a preset.
+			for (i = 0; i < NTSC_PRESETS_COUNT; i++)
+			{
+				if (!ntsc_presets[i].setup)
+				{
+					// "Custom". This is the last item in the predefined list.
+					// Since the current setup doesn't match anything else,
+					// it must be a custom setup.
+					ntsc_host_srv->config_set(&mdp, "Preset", ntsc_presets[i].name);
+					break;
+				}
+				else
+				{
+					// Check if this preset matches the current setup.
+					if (!memcmp(&mdp_md_ntsc_setup, ntsc_presets[i].setup, sizeof(mdp_md_ntsc_setup)))
+					{
+						// Match found!
+						ntsc_host_srv->config_set(&mdp, "Preset", ntsc_presets[i].name);
+						break;
+					}
+				}
+			}
+			
+			// TODO: Save individual values.
+			
+			break;
+		
+		default:
+			// Unhandled event.
+			return -MDP_ERR_EVENT_NOT_REGISTERED;
+	}
+	
 	return MDP_ERR_OK;
 }
