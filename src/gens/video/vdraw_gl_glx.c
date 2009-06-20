d5db485cbb46070c64fd4fa0879f16134f70a508
David Korth <gerbilsoft@verizon.net>
2009-06-20 12:30:50 -0400

    vdraw_gl_glx.c: Improved VSync functionality.
    - Extensions are checked via glXQueryExtensionsString() to make sure
      they are available before they are used.
    
    - Both MESA and SGI swap control methods are supported, with
      MESA being the preferred method.

--- src/gens/video/vdraw_gl_glx.c
+++ src/gens/video/vdraw_gl_glx.c
@@ -22,6 +22,12 @@
 
 #include "vdraw_gl.h"
 #include "emulator/g_main.hpp"
+#include "macros/log_msg.h"
+
+#include <stdlib.h>
+
+// X11 includes.
+#include <X11/Xlib.h>
 
 // OpenGL includes.
 #include <GL/gl.h>
@@ -37,23 +43,133 @@
 
 #ifndef GLX_MESA_swap_control
 typedef GLint (*PFNGLXSWAPINTERVALMESAPROC)(unsigned interval);
-typedef GLint (*PFNGLXGETSWAPINTERVALMESAPROC)(void);
-#endif
-
-#if !defined(GLX_OML_sync_control) && defined(_STDINT_H)
-#define GLX_OML_sync_control 1
-typedef Bool (*PFNGLXGETMSCRATEOMLPROC)(Display *dpy, GLXDrawable drawable, int32_t *numerator, int32_t *denominator);
-#endif
-
-#ifndef GLX_MESA_swap_frame_usage
-#define GLX_MESA_swap_frame_usage 1
-typedef int (*PFNGLXGETFRAMEUSAGEMESAPROC)(Display *dpy, GLXDrawable drawable, float *usage);
 #endif
 
 // OpenGL VSync stuff
 // Copied from Mesa's glxswapcontrol.c
-static PFNGLXSWAPINTERVALMESAPROC set_swap_interval = NULL;
-static PFNGLXGETSWAPINTERVALMESAPROC get_swap_interval = NULL;
+static PFNGLXSWAPINTERVALMESAPROC set_swap_interval_sgi = NULL;
+static PFNGLXSWAPINTERVALMESAPROC set_swap_interval_mesa = NULL;
+
+// make_extension_table() and is_extension_supported() were
+// copied from Mesa's glxswapcontrol.c
+
+#define NUL '\0'
+static char **extension_table = NULL;
+static unsigned int num_extensions;
+
+
+/**
+ * Fill in the table of extension strings from a supplied extensions string
+ * (as returned by glXQueryExtensionsString).
+ *
+ * \param string   String of GLX extensions.
+ * \sa is_extension_supported
+ */
+static void make_extension_table(const char* string)
+{
+	char **string_tab;
+	unsigned int num_strings;
+	unsigned int base;
+	unsigned int idx;
+	unsigned int i;
+	
+	/* Count the number of spaces in the string.  That gives a base-line
+	 * figure for the number of extension in the string.
+	 */
+	
+	num_strings = 1;
+	for (i = 0; string[i] != NUL; i++)
+	{
+		if (string[i] == ' ')
+		{
+			num_strings++;
+		}
+	}
+	
+	string_tab = (char**)malloc(sizeof(char*) * num_strings);
+	if (string_tab == NULL)
+		return;
+	
+	base = 0;
+	idx = 0;
+	
+	while (string[base] != NUL)
+	{
+		/* Determine the length of the next extension string.
+		 */
+		
+		for (i = 0; (string[base + i] != NUL) && (string[base + i] != ' '); i++)
+		{
+			/* empty */ ;
+		}
+		
+		if (i > 0)
+		{
+			/* If the string was non-zero length, add it to the table.  We
+			 * can get zero length strings if there is a space at the end of
+			 * the string or if there are two (or more) spaces next to each
+			 * other in the string.
+			 */
+
+			string_tab[idx] = (char*)malloc(sizeof(char) * (i + 1));
+			if (string_tab[idx] == NULL)
+				return;
+			
+			memcpy(string_tab[idx], &string[base], i);
+			string_tab[idx][i] = NUL;
+			idx++;
+		}
+		
+		/* Skip to the start of the next extension string.
+		 */
+
+		for (base += i; (string[ base ] == ' ') && (string[ base ] != NUL); base++ )
+		{
+			/* empty */ ;
+		}
+	}
+	
+	extension_table = string_tab;
+	num_extensions = idx;
+}
+
+
+/**
+ * Determine if an extension is supported.  The extension string table
+ * must have already be initialized by calling \c make_extension_table.
+ *
+ * \praram ext  Extension to be tested.
+ * \return GL_TRUE of the extension is supported, GL_FALSE otherwise.
+ * \sa make_extension_table
+ */
+static GLboolean is_extension_supported(const char* ext)
+{
+	unsigned int i;
+	
+	for (i = 0; i < num_extensions; i++)
+	{
+		if (strcmp(ext, extension_table[i]) == 0)
+		{
+			return GL_TRUE;
+		}
+	}
+	
+	return GL_FALSE;
+}
+
+
+static void clear_extension_table(void)
+{
+	if (!extension_table)
+		return;
+	
+	for (unsigned int i = 0; i < num_extensions; i++)
+	{
+		free(extension_table[i]);
+	}
+	
+	free(extension_table);
+}
 
 
 /**
@@ -61,15 +177,40 @@ static PFNGLXGETSWAPINTERVALMESAPROC get_swap_interval = NULL;
  */
 void vdraw_gl_init_vsync(void)
 {
-	// Linux/UNIX: Get the VSync functions.
-	// TODO: Copy functions to test for VSync from Mesa's glxswapcontrol.c
-	/*
-	has_OML_sync_control = is_extension_supported("GLX_OML_sync_control");
-	has_SGI_swap_control = is_extension_supported("GLX_SGI_swap_control");
-	has_MESA_swap_control = is_extension_supported("GLX_MESA_swap_control");
-	*/
-	set_swap_interval = (PFNGLXSWAPINTERVALMESAPROC)glXGetProcAddressARB((const GLubyte*)"glXSwapIntervalSGI");
-	get_swap_interval = (PFNGLXGETSWAPINTERVALMESAPROC)glXGetProcAddressARB((const GLubyte*)"glXGetSwapIntervalSGI");
+	// Get the X11 display.
+	Display *dpy;
+	char *dpyname = getenv("DISPLAY");
+	dpy = XOpenDisplay(dpyname);
+	if (!dpy)
+	{
+		LOG_MSG(video, LOG_MSG_LEVEL_WARNING,
+			"Could not open X11 display. VSync will not be enabled.");
+		return;
+	}
+	
+	// Create the extension table.
+	make_extension_table((const char*)glXQueryExtensionsString(dpy, DefaultScreen(dpy)));
+	
+	// Close the X11 display.
+	XCloseDisplay(dpy);
+	
+	// TODO: Allow the user to select a VSync method.
+	// Currently, MESA is preferred, followed by SGI.
+	
+	// Check for MESA swap control.
+	if (is_extension_supported("GLX_MESA_swap_control"))
+	{
+		set_swap_interval_mesa = (PFNGLXSWAPINTERVALMESAPROC)glXGetProcAddressARB((const GLubyte*)"glXSwapIntervalMESA");
+	}
+	
+	// Check for SGI swap control.
+	if (is_extension_supported("GLX_SGI_swap_control"))
+	{
+		set_swap_interval_sgi = (PFNGLXSWAPINTERVALMESAPROC)glXGetProcAddressARB((const GLubyte*)"glXSwapIntervalSGI");
+	}
+	
+	// Clear the extension tbale.
+	clear_extension_table();
 }
 
 
@@ -85,11 +226,15 @@ void vdraw_gl_update_vsync(const int fromInitSDLGL)
 	GLint vsync = (vdraw_get_fullscreen() ? Video.VSync_FS : Video.VSync_W);
 	
 	// Linux/UNIX.
-	if (set_swap_interval != NULL)
+	if (set_swap_interval_mesa)
 	{
-		if (vsync)
-			set_swap_interval(1);
-		if (!fromInitSDLGL)
-			vdraw_refresh_video();
+		set_swap_interval_mesa(vsync);
 	}
+	else if (set_swap_interval_sgi)
+	{
+		set_swap_interval_sgi(vsync);
+	}
+	
+	if (!fromInitSDLGL)
+		vdraw_refresh_video();
 }
