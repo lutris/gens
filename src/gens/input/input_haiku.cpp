5521d8658bb1f08bc05c4a2268a3559e2566f4d7
David Korth <gerbilsoft@verizon.net>
2009-06-30 19:13:00 -0400

    input_haiku.cpp: Renamed input_haiku.c to input_haiku.cpp.
    As per request by Phil Costin.

--- src/gens/input/input_haiku.cpp
+++ src/gens/input/input_haiku.cpp
@@ -21,7 +21,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-
 #include "input_haiku.hpp"
 
 // Message logging.
