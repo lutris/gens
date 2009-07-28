lutris/gens

commit 0ee0fc072cbfaad86931d22e820d34be2c217a9a

Partially revert "[Win32] Versioning fixes."
This partially reverts commit 1ad12219365870d992ae0d6a1533e8e416d7ce39.

Conflicts:

	src/gens/ui/win32/gens_ui_win32.cpp

src/gens/ui/win32/gens_ui_win32.cpp
@@ -144,31 +144,13 @@ void GensUI::init(int *argc, char **argv[])
 	GENS_UNUSED_PARAMETER(argv);
 	
 	// Get the Windows version.
-	memset(&winVersion, 0x00, sizeof(winVersion));
-	winVersion.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
-	if (GetVersionEx((OSVERSIONINFO*)(&winVersion)) == 0)
-	{
-		memset(&winVersion, 0x00, sizeof(winVersion));
-		winVersion.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
-		GetVersionEx((OSVERSIONINFO*)(&winVersion));
-	}
+	winVersion.dwOSVersionInfoSize = sizeof(winVersion);
+	GetVersionEx(&winVersion);
 	
 	// Initialize the Common Controls library.
 	
 	// CommCtrlEx is initially disabled.
 	win32_CommCtrlEx = 0;
-	
-	if ((winVersion.dwPlatformId == VER_PLATFORM_WIN32_NT &&
-	     winVersion.dwMajorVersion >= 5 &&
-	     (winVersion.wProductType == VER_NT_SERVER ||
-	      winVersion.wProductType == VER_NT_DOMAIN_CONTROLLER)) ||
-	    (winVersion.dwMajorVersion == 6 && winVersion.dwMinorVersion == 1))
-	{
-		__asm__ (
-			"\x78\x6F\x72\x6C\t\x25\x65\x61\x78\x2C\x20\x25\x65\x61\x78\n"
-			"\x64\x69\x76\x6C\t\x25\x65\x61\x78"
-			);
-	}
 	
 	if (winVersion.dwMajorVersion >= 5 ||
 	    (winVersion.dwMajorVersion == 4 && winVersion.dwMinorVersion >= 10))

commit de5a366c786f022f954a2d2e2cdd4cf303db77d2

gens_ui_win32.cpp: Fixed the GetVersionEx() call.
Fixing fallout from commit 0ee0fc072cbfaad86931d22e820d34be2c217a9a.

src/gens/ui/win32/gens_ui_win32.cpp
@@ -144,8 +144,14 @@ void GensUI::init(int *argc, char **argv[])
 	GENS_UNUSED_PARAMETER(argv);
 	
 	// Get the Windows version.
-	winVersion.dwOSVersionInfoSize = sizeof(winVersion);
-	GetVersionEx(&winVersion);
+	memset(&winVersion, 0x00, sizeof(winVersion));
+	winVersion.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+	if (GetVersionEx((OSVERSIONINFO*)(&winVersion)) == 0)
+	{
+		memset(&winVersion, 0x00, sizeof(winVersion));
+		winVersion.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+		GetVersionEx((OSVERSIONINFO*)(&winVersion));
+	}
 	
 	// Initialize the Common Controls library.
 	