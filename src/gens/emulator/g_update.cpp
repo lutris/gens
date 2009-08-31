lutris/gens

commit e0e79365de9ff098c26ba646a9cd1ced769a8e94

debugger.cpp: Moved the "Debug" value from gens.cpp to debugger.cpp, and renamed it "debug_mode".
Added some macros, IS_DEBUGGING() and STOP_DEBUGGING(), to make it easier
to check if a debugger is running and to stop it. These macros expand to
no-ops if the debugger is disabled in the ./configure script.

src/gens/emulator/g_update.cpp
@@ -31,6 +31,7 @@
 #include "g_main.hpp"
 #include "gens_core/mem/mem_m68k.h"
 #include "ui/gens_ui.hpp"
+#include "debugger/debugger.hpp"
 
 #ifndef GENS_OS_WIN32
 #include "port/timer.h"
@@ -97,9 +98,7 @@ int Update_Emulation(void)
 #endif /* GENS_OS_WIN32 */
 			Frame_Number = 0;
 			Update_Frame();
-#ifdef GENS_DEBUGGER
-			if (!Debug)
-#endif /* GENS_DEBUGGER */
+			if (!IS_DEBUGGING())
 				vdraw_flip();
 		}
 	}
@@ -123,9 +122,7 @@ int Update_Emulation(void)
 			// Audio buffer is empty.
 			input_update_controllers();
 			Update_Frame();
-#ifdef GENS_DEBUGGER
-			if (!Debug)
-#endif /* GENS_DEBUGGER */
+			if (!IS_DEBUGGING())
 				vdraw_flip();
 		}
 		else
@@ -153,9 +150,7 @@ int Update_Emulation(void)
 			{
 				input_update_controllers();
 				Update_Frame();
-#ifdef GENS_DEBUGGER
-				if (!Debug)
-#endif /* GENS_DEBUGGER */
+				if (IS_DEBUGGING())
 					vdraw_flip();
 			}
 			else
@@ -197,9 +192,7 @@ int Update_Emulation_One(void)
 	input_update_controllers();
 	Update_Frame();
 	
-#ifdef GENS_DEBUGGER
-	if (!Debug)
-#endif /* GENS_DEBUGGER */
+	if (IS_DEBUGGING())
 		vdraw_flip();
 	
 	return 1;

src/gens/debugger/debugger.cpp
@@ -50,6 +50,11 @@ static int Current_PC;
 
 static char Dbg_Out_Str[GENS_PATH_MAX];
 
+// Debug mode settings.
+DEBUG_MODE debug_mode = DEBUG_NONE;
+int debug_show_vdp = 0;
+
+
 // Macro used to print a constant string.
 // Print_Text() doesn't directly support string constants, and since it's
 // written in assembly language, it's too cumbersome to fix.
@@ -70,9 +75,9 @@ void Debug_Event(int key, int mod)
 	int i, steps;
 	SH2_CONTEXT *sh;
 	// Determine the appropriate SH2 context.
-	if (Debug == DEBUG_MAIN_SH2 || Debug == DEBUG_32X_VDP)
+	if (debug_mode == DEBUG_MAIN_SH2 || debug_mode == DEBUG_32X_VDP)
 		sh = &M_SH2;
-	else if (Debug == DEBUG_SUB_SH2)
+	else if (debug_mode == DEBUG_SUB_SH2)
 		sh = &S_SH2;
 	else
 		sh = NULL;
@@ -112,26 +117,26 @@ void Debug_Event(int key, int mod)
 			
 			for (i = 0; i < steps; i++)
 			{
-				if ((Debug == DEBUG_MAIN_68000) || (Debug == DEBUG_GENESIS_VDP))
+				if ((debug_mode == DEBUG_MAIN_68000) || (debug_mode == DEBUG_GENESIS_VDP))
 				{
 					main68k_tripOdometer();
 					main68k_exec(1);
 				}
-				else if (Debug == DEBUG_Z80)
+				else if (debug_mode == DEBUG_Z80)
 				{
 					mdZ80_clear_odo(&M_Z80);
 					z80_Exec(&M_Z80, 1);
 				}
-				else if (Debug == DEBUG_SUB_68000_REG ||
-					 Debug == DEBUG_SUB_68000_CDC ||
-					 Debug == DEBUG_WORD_RAM_PATTERN)
+				else if (debug_mode == DEBUG_SUB_68000_REG ||
+					 debug_mode == DEBUG_SUB_68000_CDC ||
+					 debug_mode == DEBUG_WORD_RAM_PATTERN)
 				{
 					sub68k_tripOdometer();
 					sub68k_exec(1);
 				}
-				else if (Debug == DEBUG_MAIN_SH2 ||
-					 Debug == DEBUG_SUB_SH2 ||
-					 Debug == DEBUG_32X_VDP)
+				else if (debug_mode == DEBUG_MAIN_SH2 ||
+					 debug_mode == DEBUG_SUB_SH2 ||
+					 debug_mode == DEBUG_32X_VDP)
 				{
 					SH2_Clear_Odo(sh);
 					SH2_Exec(sh, 1);
@@ -165,23 +170,23 @@ void Debug_Event(int key, int mod)
 			break;
 		
 		case GENS_KEY_h:
-			if (Debug == DEBUG_MAIN_68000)
+			if (debug_mode == DEBUG_MAIN_68000)
 			{
 				main68k_interrupt(4, -1);
 			}
-			else if (Debug == DEBUG_Z80)
+			else if (debug_mode == DEBUG_Z80)
 			{
 				mdZ80_interrupt(&M_Z80, 0xFF);
 			}
-			else if (Debug == DEBUG_SUB_68000_REG ||
-				 Debug == DEBUG_SUB_68000_CDC ||
-				 Debug == DEBUG_WORD_RAM_PATTERN)
+			else if (debug_mode == DEBUG_SUB_68000_REG ||
+				 debug_mode == DEBUG_SUB_68000_CDC ||
+				 debug_mode == DEBUG_WORD_RAM_PATTERN)
 			{
 				sub68k_interrupt(5, -1);
 			}
-			else if (Debug == DEBUG_MAIN_SH2 ||
-				 Debug == DEBUG_SUB_SH2 ||
-				 Debug == DEBUG_32X_VDP)
+			else if (debug_mode == DEBUG_MAIN_SH2 ||
+				 debug_mode == DEBUG_SUB_SH2 ||
+				 debug_mode == DEBUG_32X_VDP)
 			{
 				SH2_Interrupt(sh, 8);
 			}
@@ -189,23 +194,23 @@ void Debug_Event(int key, int mod)
 			break;
 		
 		case GENS_KEY_j:
-			if (Debug == DEBUG_MAIN_68000)
+			if (debug_mode == DEBUG_MAIN_68000)
 			{
 				main68k_interrupt(6, -1);
 			}
-			else if (Debug == DEBUG_Z80)
+			else if (debug_mode == DEBUG_Z80)
 			{
 				mdZ80_interrupt(&M_Z80, 0xFF);
 			}
-			else if (Debug == DEBUG_SUB_68000_REG ||
-				 Debug == DEBUG_SUB_68000_CDC ||
-				 Debug == DEBUG_WORD_RAM_PATTERN)
+			else if (debug_mode == DEBUG_SUB_68000_REG ||
+				 debug_mode == DEBUG_SUB_68000_CDC ||
+				 debug_mode == DEBUG_WORD_RAM_PATTERN)
 			{
 				sub68k_interrupt(4, -1);
 			}
-			else if (Debug == DEBUG_MAIN_SH2 ||
-				 Debug == DEBUG_SUB_SH2 ||
-				 Debug == DEBUG_32X_VDP)
+			else if (debug_mode == DEBUG_MAIN_SH2 ||
+				 debug_mode == DEBUG_SUB_SH2 ||
+				 debug_mode == DEBUG_32X_VDP)
 			{
 				SH2_Interrupt(sh, 12);
 			}
@@ -225,11 +230,11 @@ void Debug_Event(int key, int mod)
 			break;
 		
 		case GENS_KEY_x:
-			Debug ^= 0x0100;
+			debug_show_vdp = !debug_show_vdp;
 			break;
 		
 		case GENS_KEY_c:
-			if (Debug == DEBUG_SUB_68000_REG || Debug == DEBUG_SUB_68000_CDC)
+			if (debug_mode == DEBUG_SUB_68000_REG || debug_mode == DEBUG_SUB_68000_CDC)
 				SCD.Cur_LBA++;
 			VDP_Status ^= 0x8;
 			break;
@@ -240,20 +245,20 @@ void Debug_Event(int key, int mod)
 			break;
 		
 		case GENS_KEY_n:
-			if (Debug == DEBUG_MAIN_SH2 || Debug == DEBUG_SUB_SH2)
+			if (debug_mode == DEBUG_MAIN_SH2 || debug_mode == DEBUG_SUB_SH2)
 			{
 				sh->PC += 2;
 				sh->Status &= 0xFFFFFFF0;
 			}
-			else if (Debug == DEBUG_SUB_68000_REG || Debug == DEBUG_SUB_68000_CDC)
+			else if (debug_mode == DEBUG_SUB_68000_REG || debug_mode == DEBUG_SUB_68000_CDC)
 			{
 				sub68k_context.pc += 2;
 			}
-			else if (Debug == DEBUG_MAIN_68000)
+			else if (debug_mode == DEBUG_MAIN_68000)
 			{
 				main68k_context.pc += 2;
 			}
-			else if (Debug == DEBUG_Z80)
+			else if (debug_mode == DEBUG_Z80)
 			{
 				mdZ80_set_PC(&M_Z80, mdZ80_get_PC(&M_Z80) + 1);
 			}
@@ -261,30 +266,33 @@ void Debug_Event(int key, int mod)
 			break;
 		
 		case GENS_KEY_w:
-			if (Debug == DEBUG_SUB_68000_REG || Debug == DEBUG_SUB_68000_CDC)
+			if (debug_mode == DEBUG_SUB_68000_REG || debug_mode == DEBUG_SUB_68000_CDC)
 				Check_CD_Command();
 			
 			break;
 		
 		case GENS_KEY_SPACE:
-			if (Debug)
+			if (debug_mode)
 			{
-				Debug++;
+				int tmp_debug = (int)debug_mode;
+				tmp_debug++;
 				
 				if (SegaCD_Started)
 				{
-					if (Debug > 6)
-						Debug = 1;
+					if (tmp_debug > 6)
+						tmp_debug = 1;
 				}
 				else if (_32X_Started)
 				{
-					if ((Debug > 3) && (Debug < 7))
-						Debug = 7;
-					if (Debug > 9)
-						Debug = 1;
+					if ((tmp_debug > 3) && (tmp_debug < 7))
+						tmp_debug = 7;
+					if (tmp_debug > 9)
+						tmp_debug = 1;
 				}
-				else if (Debug > 3)
-					Debug = 1;
+				else if (tmp_debug > 3)
+					tmp_debug = 1;
+				
+				debug_mode = (DEBUG_MODE)tmp_debug;
 			}
 			
 			break;
@@ -327,16 +335,16 @@ void Debug_Event(int key, int mod)
 		
 		case GENS_KEY_NUM_PLUS:
 			// Scroll down in pattern debugging.
-			if (Debug == DEBUG_MAIN_68000 ||
-			    Debug == DEBUG_Z80 ||
-			    Debug == DEBUG_GENESIS_VDP)
+			if (debug_mode == DEBUG_MAIN_68000 ||
+			    debug_mode == DEBUG_Z80 ||
+			    debug_mode == DEBUG_GENESIS_VDP)
 			{
 				if (pattern_adr < 0xDA00)
 					pattern_adr = (pattern_adr + 0x200) & 0xFFFF;
 				if (pattern_adr >= 0xDA00) // Make sure it doesn't go out of bounds.
 					pattern_adr = 0xDA00 - 0x200;
 			}
-			else if (Debug == DEBUG_WORD_RAM_PATTERN)
+			else if (debug_mode == DEBUG_WORD_RAM_PATTERN)
 			{
 				if (cd_pattern_adr < 0x3D000)
 					cd_pattern_adr = (pattern_adr + 0x800) & 0x3FFFF;
@@ -348,16 +356,16 @@ void Debug_Event(int key, int mod)
 		
 		case GENS_KEY_NUM_MINUS:
 			// Scroll up in pattern debugging.
-			if (Debug == DEBUG_MAIN_68000 ||
-			    Debug == DEBUG_Z80 ||
-			    Debug == DEBUG_GENESIS_VDP)
+			if (debug_mode == DEBUG_MAIN_68000 ||
+			    debug_mode == DEBUG_Z80 ||
+			    debug_mode == DEBUG_GENESIS_VDP)
 			{
 				if (pattern_adr > 0)
 					pattern_adr = (pattern_adr - 0x200) & 0xFFFF;
 				if (pattern_adr < 0) // Make sure it doesn't go out of bounds.
 					pattern_adr = 0;
 			}
-			else if (Debug == DEBUG_WORD_RAM_PATTERN)
+			else if (debug_mode == DEBUG_WORD_RAM_PATTERN)
 			{
 				if (cd_pattern_adr > 0)
 					cd_pattern_adr = cd_pattern_adr - 0x800;
@@ -378,9 +386,9 @@ unsigned short Next_Word(void)
 {
 	unsigned short val = 0;
 	
-	if (Debug == DEBUG_MAIN_68000)
+	if (debug_mode == DEBUG_MAIN_68000)
 		val = M68K_RW(Current_PC);
-	else if (Debug >= DEBUG_Z80)
+	else if (debug_mode >= DEBUG_Z80)
 		val = S68K_RW(Current_PC);
 	
 	Current_PC += 2;
@@ -396,13 +404,13 @@ unsigned int Next_Long(void)
 {
 	unsigned int val = 0;
 	
-	if (Debug == DEBUG_MAIN_68000)
+	if (debug_mode == DEBUG_MAIN_68000)
 	{
 		val = M68K_RW(Current_PC);
 		val <<= 16;
 		val |= M68K_RW(Current_PC + 2);
 	}
-	else if (Debug >= DEBUG_Z80)
+	else if (debug_mode >= DEBUG_Z80)
 	{
 		val = S68K_RW(Current_PC);
 		val <<= 16;
@@ -1054,7 +1062,7 @@ void Update_Debug_Screen(void)
 	else
 		memset(MD_Screen, 0x00, sizeof(MD_Screen));
 	
-	if (Debug & 0x100)
+	if (debug_show_vdp)
 	{
 		if (_32X_Started)
 			Do_32X_VDP_Only();
@@ -1063,7 +1071,7 @@ void Update_Debug_Screen(void)
 	}
 	else
 	{
-		switch (Debug)
+		switch (debug_mode)
 		{
 			default:
 			case DEBUG_MAIN_68000:

src/gens/debugger/debugger.hpp
@@ -1,12 +1,12 @@
 #ifndef GENS_DEBUGGER_HPP
 #define GENS_DEBUGGER_HPP
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #ifdef HAVE_CONFIG_H
 #include <config.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
 #endif
 
 #ifdef GENS_DEBUGGER
@@ -14,7 +14,7 @@ extern "C" {
 void Debug_Event(int key, int mod);
 void Update_Debug_Screen(void);
 
-enum DEBUG_MODE
+typedef enum _DEBUG_MODE
 {
 	DEBUG_NONE		= 0,
 	DEBUG_MAIN_68000	= 1,
@@ -25,8 +25,25 @@ enum DEBUG_MODE
 	DEBUG_WORD_RAM_PATTERN	= 6,
 	DEBUG_MAIN_SH2		= 7,
 	DEBUG_SUB_SH2		= 8,
-	DEBUG_32X_VDP		= 9
-};
+	DEBUG_32X_VDP		= 9,
+	
+	DEBUG_MAX
+} DEBUG_MODE;
+
+extern DEBUG_MODE debug_mode;
+extern int debug_show_vdp;
+
+#define IS_DEBUGGING() (debug_mode != DEBUG_NONE)
+#define STOP_DEBUGGING() \
+do { \
+	debug_mode = DEBUG_NONE; \
+	debug_show_vdp = 0; \
+} while (0)
+
+#else
+
+#define IS_DEBUGGING() 0
+#define STOP_DEBUGGING() do { } while (0)
 
 #endif /* GENS_DEBUGGER */
 

src/gens/emulator/g_32x.cpp
@@ -26,6 +26,7 @@
 #include "gens_core/io/io.h"
 #include "util/file/save.hpp"
 #include "gens_core/sound/pwm.h"
+#include "debugger/debugger.hpp"
 
 #include "util/sound/wave.h"
 #include "util/sound/gym.hpp"
@@ -122,9 +123,10 @@ int Init_32X(ROM_t* MD_ROM)
 	ROM_ByteSwap_State |= ROM_BYTESWAPPED_32X_FW_SSH2;
 	
 	Flag_Clr_Scr = 1;
-	Debug = Paused = Frame_Number = 0;
+	Paused = Frame_Number = 0;
 	SRAM_Start = SRAM_End = SRAM_ON = SRAM_Write = 0;
 	Controller_1_COM = Controller_2_COM = 0;
+	STOP_DEBUGGING();
 	
 	if (!Kaillera_Client_Running)
 		Init_Genesis_SRAM(MD_ROM);

src/gens/emulator/g_main.cpp
@@ -56,13 +56,10 @@
 #include "gens_core/cpu/z80/cpu_z80.h"
 #include "gens_core/sound/psg.h"
 #include "gens_core/sound/pwm.h"
+#include "debugger/debugger.hpp"
 
 // INI handling.
 #include "port/ini.hpp"
-
-#ifdef GENS_DEBUGGER
-#include "debugger/debugger.hpp"
-#endif /* GENS_DEBUGGER */
 
 // CD-ROM drive access
 #ifdef GENS_CDROM
@@ -166,9 +163,7 @@ int Init_Settings(void)
 	SegaCD_Started = 0;
 	_32X_Started = 0;
 	CPU_Mode = 0;
-	
-	// This is needed even if the built-in debugger isn't compiled in.
-	Debug = 0;
+	STOP_DEBUGGING();
 	
 	// Get the default save path.
 	get_default_save_path(PathNames.Gens_Path, sizeof(PathNames.Gens_Path));
@@ -409,7 +404,7 @@ void GensMainLoop(void)
 		input_update();
 		
 #ifdef GENS_DEBUGGER
-		if (Debug)
+		if (IS_DEBUGGING())
 		{
 			// DEBUG
 			Update_Debug_Screen();

src/gens/emulator/g_mcd.cpp
@@ -2,6 +2,10 @@
  * Gens: Sega CD (Mega CD) initialization and main loop code.
  */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 // C includes.
 #include <stdint.h>
 #include <string.h>
@@ -32,6 +36,7 @@
 #include "gens_core/sound/pcm.h"
 #include "segacd/cd_sys.hpp"
 #include "segacd/cd_file.h"
+#include "debugger/debugger.hpp"
 
 #include "util/sound/wave.h"
 #include "util/sound/gym.hpp"
@@ -159,10 +164,11 @@ int Init_SegaCD(const char* iso_name)
 	Options::setGameName(1);
 	
 	Flag_Clr_Scr = 1;
-	Debug = Paused = Frame_Number = 0;
+	Paused = Frame_Number = 0;
 	SRAM_Start = SRAM_End = SRAM_ON = SRAM_Write = 0;
 	BRAM_Ex_State &= 0x100;
 	Controller_1_COM = Controller_2_COM = 0;
+	STOP_DEBUGGING();
 	
 	// Set clock rates depending on the CPU mode (NTSC / PAL).
 	Set_Clock_Freq(1);

src/gens/emulator/g_md.cpp
@@ -2,6 +2,10 @@
  * Gens: Genesis (Mega Drive) initialization and main loop code.
  */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <string.h>
 
 #include "gens.hpp"
@@ -19,6 +23,7 @@
 #include "gens_core/vdp/vdp_32x.h"
 #include "gens_core/io/io.h"
 #include "util/file/save.hpp"
+#include "debugger/debugger.hpp"
 
 #include "util/sound/wave.h"
 #include "util/sound/gym.hpp"
@@ -248,9 +253,10 @@ int Init_Genesis(ROM_t* MD_ROM)
 	audio_clear_sound_buffer();
 	
 	Flag_Clr_Scr = 1;
-	Debug = Paused = Frame_Number = 0;
+	Paused = Frame_Number = 0;
 	SRAM_Start = SRAM_End = SRAM_ON = SRAM_Write = 0;
 	Controller_1_COM = Controller_2_COM = 0;
+	STOP_DEBUGGING();
 	
 	if (!Kaillera_Client_Running)
 		Init_Genesis_SRAM(MD_ROM);

src/gens/emulator/gens.cpp
@@ -23,7 +23,6 @@
 #include "macros/math_m.h"
 #include "audio/audio.h"
 
-int Debug;
 int Frame_Skip;
 int Frame_Number;
 

commit 36bec6d9f5942b7c7af9a880ce57014bde7224bd

g_update.cpp: Fixed an incorrect IS_DEBUGGING() check.
Fallout from commit e0e79365de9ff098c26ba646a9cd1ced769a8e94.
This broke the emulator when sound was disabled.

Also, removed Sleep_Time. It has no use anywhere.

src/gens/emulator/g_update.cpp
@@ -53,7 +53,6 @@
 clock_t Last_Time = 0;
 clock_t New_Time = 0;
 clock_t Used_Time = 0;
-int Sleep_Time;
 
 
 /**
@@ -127,6 +126,8 @@ int Update_Emulation(void)
 		}
 		else
 		{
+			// Sound is not enabled.
+			
 			if (CPU_Mode)
 				current_div = 20;
 			else
@@ -150,15 +151,10 @@ int Update_Emulation(void)
 			{
 				input_update_controllers();
 				Update_Frame();
-				if (IS_DEBUGGING())
+				if (!IS_DEBUGGING())
 					vdraw_flip(1);
 			}
-			else
-			{
-				GensUI::sleep(Sleep_Time);
-			}
-		} //If sound is not enabled
-		
+		}
 	}
 	
 	if (ice >= 1)

src/gens/util/file/config_file.cpp
@@ -487,7 +487,6 @@ int Config::load(const string& filename, void* gameActive)
 	Window_Pos.y = cfg.getInt("General", "Window Y", 0);
 	Intro_Style = cfg.getInt("General", "Intro Style", 0);
 	vdraw_set_intro_effect_color(cfg.getInt("General", "Free Mode Color", 7));
-	Sleep_Time = cfg.getInt("General", "Allow Idle", 0) & 1;
 	
 	// Video adjustments.
 	Contrast_Level = cfg.getInt("Graphics", "Contrast", 100);