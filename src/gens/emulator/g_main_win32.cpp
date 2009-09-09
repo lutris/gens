lutris/gens

commit 1558532fb72427ced8572af61c4e6e4176572b80

g_main_win32.cpp: Use debug_window_close() instead of DestroyWindow(debug_window).

src/gens/emulator/g_main_win32.cpp
@@ -204,7 +204,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
 	if (startup->enable_debug_window)
 		ShowWindow(debug_window, nCmdShow);
 	else
-		DestroyWindow(debug_window);
+		debug_window_close();
 #endif
 	
 	// Initialize the video backend.
@@ -249,12 +249,11 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
 	Config::save(cfg_filename);
 	
 	End_All();
-	DestroyWindow(gens_window);
 	
 #if !defined(GENS_WIN32_CONSOLE)
 	// Close the Debug window.
 	if (debug_window)
-		DestroyWindow(debug_window);
+		debug_window_close();
 #endif
 		
 	// Delete the fonts.

commit 168b59d827cb1905e8aaf5506eab209fa6ce7f6d

g_main_win32.cpp: Call DestroyWindow(gens_window) after End_All().
This is an accidental regression from commit 1558532fb72427ced8572af61c4e6e4176572b80.
It broke quitting while in fullscreen, since the Gens window would
be receiving a bunch of messages that weren't handled, causing the
"Empty the message queue" loop to run infinitely. (Sometimes, it
resulted in a crash. Not sure why.).

src/gens/emulator/g_main_win32.cpp
@@ -249,13 +249,14 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
 	Config::save(cfg_filename);
 	
 	End_All();
+	DestroyWindow(gens_window);
 	
 #if !defined(GENS_WIN32_CONSOLE)
 	// Close the Debug window.
 	if (debug_console)
 		debug_console_close();
 #endif
-		
+	
 	// Delete the fonts.
 	fonts_end();
 	
@@ -273,6 +274,5 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
 #endif
 	
 	TerminateProcess(GetCurrentProcess(), 0); //Modif N
-	
 	return 0;
 }