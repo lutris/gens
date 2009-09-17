783debfa466dbb82ae1eeef0a39f6a04635a7309
David Korth <gerbilsoft@verizon.net>
2009-09-17 03:20:51 -0400

    src/mdZ80/mdZ80.h: Use libgsft/gsft_fastcall.h for fastcall definitions.

--- src/mdZ80/mdZ80.h
+++ src/mdZ80/mdZ80.h
@@ -10,23 +10,7 @@
 #define __MDZ80_H
 
 
-#ifdef _WIN32
-#define FASTCALL	__fastcall
-#else
-
-/* gcc-3.4 or later is required on non-Windows systems for __attribute__ ((fastcall)). */
-#if defined(__GNUC__) && defined(__GNUC_MINOR__)
-#if (__GNUC__ < 3) || ((__GNUC__ == 3) && (__GNUC_MINOR__) < 4)
-#error mdZ80 uses __attribute__ ((fastcall)), which requires gcc-3.4 or later.
-#endif
-#endif
-
-#define __fastcall	__attribute__ ((fastcall))
-#define FASTCALL	__attribute__ ((fastcall))
-
-#endif
-
-
+#include "libgsft/gsft_fastcall.h"
 #include <stdint.h>
 
 #ifdef __cplusplus
