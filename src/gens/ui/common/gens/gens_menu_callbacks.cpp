lutris/gens

commit 97e3c676c58fbf6222e92f53bba97823002b9186

[Common] gens_menu_callbacks.cpp: Don't change menu bar visibility if in fullscreen.
This option has no effect in fullscreen, so it shouldn't be modifyable
in fullscreen.

src/gens/ui/common/gens/gens_menu_callbacks.cpp
@@ -332,6 +332,9 @@ static int gens_menu_callback_GraphicsMenu(uint16_t menuID, uint16_t state)
 			break;
 		
 		case IDM_GRAPHICS_MENUBAR:
+			if (vdraw_get_fullscreen)
+				break;
+			
 			Settings.showMenuBar = !state;
 			
 			// Rebuild the Gens Menu Bar.

commit bd08685de0b0e308ac1765531f66062815f17f32

[WTF] [Common] gens_menu_callbacks.cpp: Check the result of the function call, not the function address.
Oops. :( (regression from commit 97e3c676c58fbf6222e92f53bba97823002b9186)

src/gens/ui/common/gens/gens_menu_callbacks.cpp
@@ -332,7 +332,7 @@ static int gens_menu_callback_GraphicsMenu(uint16_t menuID, uint16_t state)
 			break;
 		
 		case IDM_GRAPHICS_MENUBAR:
-			if (vdraw_get_fullscreen)
+			if (vdraw_get_fullscreen())
 				break;
 			
 			Settings.showMenuBar = !state;