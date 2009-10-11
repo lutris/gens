685c6bd4b6e9d9e58553839075b89ef50bb9ede6
David Korth <gerbilsoft@verizon.net>
2009-10-11 16:36:04 -0400

    libgsft/gsft_strz.h: New header containing string function macros.
    These macros (szprintf(), vszprintf(), and strzcpy()) are wrappers around
    the corresponding libc functions (snprintf(), vsnprintf(), strncpy()).
    They automatically NULL-terminate the destination string.
    
    This idiom is used quite a bit in Gens/GS, so this will help to reduce the
    number of source lines of code.

---
+++ src/libgsft/gsft_strz.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+ * libgsft: Common functions.                                              *
+ * gsft_strz.h: C string functions with automatic null termination.        *
+ *                                                                         *
+ * Copyright (c) 2008-2009 by David Korth                                  *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __GSFT_STRZ_H
+#define __GSFT_STRZ_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <string.h>
+
+/**
+ * szprintf(): snprintf() wrapper with automatic NULL termination.
+ */
+#define szprintf(str, size, format, ...) \
+do { \
+	snprintf(str, size, format, ##__VA_ARGS__); \
+	str[size-1] = 0x00; \
+} while (0)
+
+/**
+ * vszprintf(): vsnprintf() wrapper with automatic NULL termination.
+ */
+#define vszprintf(str, size, format, ap) \
+do { \
+	vsnprintf(str, size, format, ap); \
+	str[size-1] = 0x00; \
+} while (0)
+
+/**
+ * strzcpy(): strncpy() wrapper with automatic NULL termination.
+ * TODO: Port BSD's strlcpy(), since it has optimizations.
+ */
+#define strzcpy(dest, src, n) \
+do { \
+	strncpy(dest, src, n); \
+	dest[n-1] = 0x00; \
+} while (0)
+
+#endif /* __GSFT_STRZ_H */
