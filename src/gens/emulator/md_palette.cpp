lutris/gens

commit cf3c7dc15d9c1d83468d6ab502b2f2a0fc3cf9e3

md_palette.cpp, vdp_rend.cpp: Updated the color scaling algorithm.
The full RGB color scaling algorithm wasn't taking Shadow/Highlight
into account. The new algorithm maps color components like this:

MD color component: xxx
Normal: xxx0 (raw CRAM value)
Shadow: 0xxx
Highlight: xxx1

Thus, a CRAM value of 0xEEE is mapped to CRAM 0xEEE for normal tiles;
CRAM 0x777 for shadowed tiles; and 0xFFF for highlighted tiles. The
old scaling algorithm is still used for linearly mapping the palettes
to the full RGB color space, since this algorithm appeared to work
correctly.

BUG: If RGB color scaling is enabled, and Gens/GS is rendering in either
15-bit or 16-bit color, the resulting screenshots will have weird color
values. This needs to be fixed!

TODO: The command line parser for the "Scale Colors" option has been
temporarily disabled, and the "Color Adjust" UI needs to be updated
for the new color scaling algorithm.

src/gens/emulator/md_palette.cpp
@@ -36,7 +36,7 @@ int Contrast_Level;
 int Brightness_Level;
 int Greyscale;
 int Invert_Color;
-int Scale_Colors;	// If set, scales colors to full RGB.
+ColorScaleMethod_t ColorScaleMethod;
 
 
 /**
@@ -102,23 +102,41 @@ static inline void T_Recalculate_Palettes(void)
 	const int brightness_B = (brightness * (BMask+1)) / 100;
 	
 	const int contrast = Contrast_Level;
-
+	
+	int mdColorMask, mdComponentScale;
+	switch (ColorScaleMethod)
+	{
+		case COLSCALE_RAW:
+			mdColorMask = 0x0EEE;
+			mdComponentScale = 0;
+			break;
+		case COLSCALE_FULL:
+			mdColorMask = 0x0EEE;
+			mdComponentScale = 0xE;
+			break;
+		case COLSCALE_FULL_HS:
+		default:
+			mdColorMask = 0x0FFF;
+			mdComponentScale = 0xF;
+			break;
+	}
+	
 	// Calculate the MD palette.
 	for (unsigned int i = 0x0000; i < 0x1000; i++)
 	{
 		// Mask off the LSB of each MD color component.
-		unsigned int color = (i & 0x0EEE);
+		unsigned int color = (i & mdColorMask);
 		
 		r = (color & 0xF) << (RBits - 4);
 		g = ((color >> 4) & 0x0F) << (GBits - 4);
 		b = ((color >> 8) & 0x0F) << (BBits - 4);
 		
 		// Scale the colors to full RGB.
-		if (Scale_Colors)
+		if (ColorScaleMethod != COLSCALE_RAW)
 		{
-			r = (r * RMask) / (0xE << (RBits - 4));
-			g = (g * GMask) / (0xE << (GBits - 4));
-			b = (b * BMask) / (0xE << (BBits - 4));
+			r = (r * RMask) / (mdComponentScale << (RBits - 4));
+			g = (g * GMask) / (mdComponentScale << (GBits - 4));
+			b = (b * BMask) / (mdComponentScale << (BBits - 4));
 		}
 		
 		// Adjust brightness.
@@ -151,7 +169,7 @@ static inline void T_Recalculate_Palettes(void)
 		b = ((i >> 10) & 0x1F) << (BBits - 5);
 		
 		// Scale the colors to full RGB.
-		if (Scale_Colors)
+		if (ColorScaleMethod != COLSCALE_RAW)
 		{
 			r = (r * RMask) / (0x1F << (RBits - 5));
 			g = (g * GMask) / (0x1F << (GBits - 5));

src/gens/emulator/md_palette.hpp
@@ -31,7 +31,15 @@ extern int Contrast_Level;
 extern int Brightness_Level;
 extern int Greyscale;
 extern int Invert_Color;
-extern int Scale_Colors;
+
+typedef enum _ColorScaleMethod_t
+{
+	COLSCALE_RAW = 0,	// Raw colors: 0xEEE -> 0xE0E0E0
+	COLSCALE_FULL = 1,	// Full colors: 0xEEE -> 0xFFFFFF
+	COLSCALE_FULL_HS = 2,	// Full colors with Highlight/Shadow: 0xEEE -> 0xEEEEEE for highlight
+} ColorScaleMethod_t;
+
+extern ColorScaleMethod_t ColorScaleMethod;
 
 void Recalculate_Palettes(void);
 

src/gens/emulator/parse.cpp
@@ -514,7 +514,7 @@ Gens_StartupInfo_t* parse_args(int argc, char *argv[])
 		
 		TEST_OPTION_ENABLE(optBarg_str[OPTB_GREYSCALE], Greyscale);
 		TEST_OPTION_ENABLE(optBarg_str[OPTB_INVERT], Invert_Color);
-		TEST_OPTION_ENABLE(optBarg_str[OPTB_SCALE], Scale_Colors);
+		//TEST_OPTION_ENABLE(optBarg_str[OPTB_SCALE], Scale_Colors);
 		TEST_OPTION_ENABLE(optBarg_str[OPTB_SPRITELIMIT], Sprite_Over);
 		TEST_OPTION_NUMERIC(opt1arg_str[OPT1_FRAMESKIP].option, Frame_Skip);
 		TEST_OPTION_ENABLE(optBarg_str[OPTB_Z80], Z80_State);

src/gens/gens_core/vdp/vdp_rend.cpp
@@ -37,12 +37,11 @@ uint32_t Palette32[0x1000];
 /**
  * T_VDP_Update_Palette(): VDP palette update function.
  * @param hs If true, updates highlight/shadow.
- * @param mask Color mask.
  * @param MD_palette MD color palette.
  * @param palette Full color palette.
  */
 template<bool hs, typename pixel>
-static inline void T_VDP_Update_Palette(pixel mask, pixel *MD_palette, const pixel *palette)
+static inline void T_VDP_Update_Palette(pixel *MD_palette, const pixel *palette)
 {
 	if (VDP_Layers & VDP_LAYER_PALETTE_LOCK)
 		return;
@@ -56,21 +55,17 @@ static inline void T_VDP_Update_Palette(pixel mask, pixel *MD_palette, const pix
 	// Color mask. Depends on VDP register 0, bit 2 (Palette Select).
 	// If set, allows full MD palette.
 	// If clear, only allows the LSB of each color component.
-	const uint16_t color_mask = (VDP_Reg.Set1 & 0x04) ? 0x0FFF : 0x0222;
+	const uint16_t color_mask = (VDP_Reg.Set1 & 0x04) ? 0x0EEE : 0x0222;
 	
 	// Update all 64 colors.
 	for (int i = 62; i >= 0; i -= 2)
 	{
-		pixel color1 = cram_16[i];
-		pixel color2 = cram_16[i + 1];
-		
-		// Mask the color using the color_mask.
-		color1 &= color_mask;
-		color2 &= color_mask;
+		uint16_t color1_raw = cram_16[i] & color_mask;
+		uint16_t color2_raw = cram_16[i + 1] & color_mask;
 		
 		// Get the palette color.
-		color1 = palette[color1];
-		color2 = palette[color2];
+		pixel color1 = palette[color1_raw];
+		pixel color2 = palette[color2_raw];
 		
 		// Set the new color.
 		MD_palette[i]     = color1;
@@ -79,43 +74,42 @@ static inline void T_VDP_Update_Palette(pixel mask, pixel *MD_palette, const pix
 		if (hs)
 		{
 			// Update the highlight and shadow colors.
+			// References:
+			// - http://www.tehskeen.com/forums/showpost.php?p=71308&postcount=1077
+			// - http://forums.sonicretro.org/index.php?showtopic=17905
 			
-			// Normal color.
+			// Normal color. (xxx0)
 			MD_palette[i + 192]	= color1;
 			MD_palette[i + 1 + 192]	= color2;
 			
-			// Shadow color.
-			color1 = (color1 >> 1) & mask;
-			color2 = (color2 >> 1) & mask;
-			MD_palette[i + 64]	= color1;
-			MD_palette[i + 1 + 64]	= color2;
+			color1_raw >>= 1;
+			color2_raw >>= 1;
 			
-			// Highlight color.
-			color1 += mask;
-			color2 += mask;
-			MD_palette[i + 128]	= color1;
-			MD_palette[i + 1 + 128]	= color2;
+			// Shadow color. (0xxx)
+			MD_palette[i + 64]	= palette[color1_raw];
+			MD_palette[i + 1 + 64]	= palette[color2_raw];
+			
+			// Highlight color. (1xxx)
+			MD_palette[i + 128]	= palette[0x888 | color1_raw];
+			MD_palette[i + 1 + 128]	= palette[0x888 | color2_raw];
 		}
 	}
 	
 	// Update the background color.
-	pixel color_bg = MD_palette[VDP_Reg.BG_Color & 0x3F];
-	MD_palette[0] = color_bg;
+	MD_palette[0] = MD_palette[VDP_Reg.BG_Color & 0x3F];
 	
 	if (hs)
 	{
 		// Update the background color for highlight and shadow.
 		
 		// Normal color.
-		MD_palette[192] = color_bg;
+		MD_palette[192] = MD_palette[VDP_Reg.BG_Color & 0x3F];
 		
 		// Shadow color.
-		color_bg = (color_bg >> 1) & mask;
-		MD_palette[64] = color_bg;
+		MD_palette[64] = MD_palette[(VDP_Reg.BG_Color & 0x3F) + 64];
 		
 		// Highlight color.
-		color_bg += mask;
-		MD_palette[128] = color_bg;
+		MD_palette[128] = MD_palette[(VDP_Reg.BG_Color & 0x3F) + 128];
 	}
 }
 
@@ -125,9 +119,8 @@ static inline void T_VDP_Update_Palette(pixel mask, pixel *MD_palette, const pix
  */
 void VDP_Update_Palette(void)
 {
-	const uint16_t mask16 = (bppMD == 15 ? 0x3DEF : 0x7BEF);
-	T_VDP_Update_Palette<false>(mask16, MD_Palette, Palette);
-	T_VDP_Update_Palette<false>((uint32_t)0x7F7F7F, MD_Palette32, Palette32);
+	T_VDP_Update_Palette<false>(MD_Palette, Palette);
+	T_VDP_Update_Palette<false>(MD_Palette32, Palette32);
 }
 
 /**
@@ -135,7 +128,6 @@ void VDP_Update_Palette(void)
  */
 void VDP_Update_Palette_HS(void)
 {
-	const uint16_t mask16 = (bppMD == 15 ? 0x3DEF : 0x7BEF);
-	T_VDP_Update_Palette<true>(mask16, MD_Palette, Palette);
-	T_VDP_Update_Palette<true>((uint32_t)0x7F7F7F, MD_Palette32, Palette32);
+	T_VDP_Update_Palette<true>(MD_Palette, Palette);
+	T_VDP_Update_Palette<true>(MD_Palette32, Palette32);
 }

src/gens/ui/gtk/color_adjust/ca_window.c
@@ -238,7 +238,7 @@ static void ca_window_init(void)
 	
 	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(chkGrayscale), Greyscale);
 	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(chkInverted), Invert_Color);
-	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(chkScaleColors), Scale_Colors);
+	//gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(chkScaleColors), Scale_Colors);
 	
 	// Disable the "Apply" button initially.
 	gtk_widget_set_sensitive(btnApply, FALSE);
@@ -255,7 +255,7 @@ static void ca_window_save(void)
 	
 	Greyscale = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(chkGrayscale));
 	Invert_Color = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(chkInverted));
-	Scale_Colors = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(chkScaleColors));
+	//Scale_Colors = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(chkScaleColors));
 	
 	// Recalculate palettes.
 	Recalculate_Palettes();

src/gens/util/file/config_file.cpp
@@ -239,7 +239,7 @@ int Config::save(const string& filename)
 	cfg.writeInt("Graphics", "Brightness", Brightness_Level);
 	cfg.writeInt("Graphics", "Greyscale", Greyscale & 1);
 	cfg.writeInt("Graphics", "Invert", Invert_Color & 1);
-	cfg.writeInt("Graphics", "Scale Colors", Scale_Colors & 1);
+	cfg.writeInt("Graphics", "Color Scale Method", (int)ColorScaleMethod);
 	
 	// Video settings
 	cfg.writeBool("Graphics", "Full Screen", vdraw_get_fullscreen());
@@ -524,7 +524,7 @@ int Config::load(const string& filename, void* gameActive)
 	Brightness_Level = cfg.getInt("Graphics", "Brightness", 100);
 	Greyscale = cfg.getInt("Graphics", "Greyscale", 0);
 	Invert_Color = cfg.getInt("Graphics", "Invert", 0);
-	Scale_Colors = cfg.getInt("Graphics", "Scale Colors", 1);
+	ColorScaleMethod = (ColorScaleMethod_t)cfg.getInt("Graphics", "Color Scale Method", COLSCALE_FULL_HS);
 	
 	// Video settings.
 	Video.VSync_FS = cfg.getInt("Graphics", "Full Screen VSync", 0) & 1;

commit 23803e7a340a7630148c790e98ead4ba38978f56

emulator/md_palette.cpp: Fixed an S/H regression with "Raw" and "Full" color scaling methods.
This regression was introduced in commit cf3c7dc15d9c1d83468d6ab502b2f2a0fc3cf9e3.
All colors need to be set correctly, even if "Raw" is being used, since
vdp_rend.cpp::T_VDP_Update_Palette() now uses the extra colors for determining
the Shadow/Highlight values.

src/gens/emulator/md_palette.cpp
@@ -103,20 +103,17 @@ static inline void T_Recalculate_Palettes(void)
 	
 	const int contrast = Contrast_Level;
 	
-	int mdColorMask, mdComponentScale;
+	int mdComponentScale;
 	switch (ColorScaleMethod)
 	{
 		case COLSCALE_RAW:
-			mdColorMask = 0x0EEE;
 			mdComponentScale = 0;
 			break;
 		case COLSCALE_FULL:
-			mdColorMask = 0x0EEE;
 			mdComponentScale = 0xE;
 			break;
 		case COLSCALE_FULL_HS:
 		default:
-			mdColorMask = 0x0FFF;
 			mdComponentScale = 0xF;
 			break;
 	}
@@ -125,7 +122,7 @@ static inline void T_Recalculate_Palettes(void)
 	for (unsigned int i = 0x0000; i < 0x1000; i++)
 	{
 		// Mask off the LSB of each MD color component.
-		unsigned int color = (i & mdColorMask);
+		unsigned int color = (i & 0x0FFF);
 		
 		r = (color & 0xF) << (RBits - 4);
 		g = ((color >> 4) & 0x0F) << (GBits - 4);