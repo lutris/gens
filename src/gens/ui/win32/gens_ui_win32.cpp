lutris/gens

commit c256cac8a8a396a90b0349d38961146172ec9a13

configure.in: Added --without-7z to disable internal 7z support.
Internal 7z support can be disabled; however, disabling it does not re-add
the external 7z support that was previously implemented in Gens/GS.

src/gens/ui/win32/gens_ui_win32.cpp
@@ -71,10 +71,22 @@ using std::string;
 static const char* UI_Win32_FileFilter_AllFiles =
 	"All Files\0*.*\0\0";
 
+// File extensions.
+#ifdef GENS_ZLIB
+	#define ZLIB_EXT "*.zip;*.zsg;*.gz"
+#else
+	#define ZLIB_EXT
+#endif
+#ifdef GENS_LZMA
+	#define LZMA_EXT "*.7z"
+#else
+	#define LZMA_EXT
+#endif
+
 static const char* UI_Win32_FileFilter_ROMFile =
-	"SegaCD / 32X / Genesis ROMs\0*.bin;*.smd;*.gen;*.32x;*.cue;*.iso;*.raw;*.zip;*.zsg;*.gz;*.7z;*.rar\0"
+	"SegaCD / 32X / Genesis ROMs\0*.bin;*.smd;*.gen;*.32x;*.cue;*.iso;*.raw;" ZLIB_EXT LZMA_EXT ";*.rar\0"
 	"Genesis ROMs\0*.smd;*.bin;*.gen;*.zip;*.zsg;*.gz;*.7z;*.rar\0"
-	"32X ROMs\0*.32x;*.zip;*.gz;*.7z;*.rar\0"
+	"32X ROMs\0*.32x;" ZLIB_EXT LZMA_EXT ";*.rar"
 	"SegaCD Disc Images\0*.cue;*.iso;*.bin;*.raw\0"
 	"All Files\0*.*\0\0";
 

configure.in
@@ -379,13 +379,26 @@ fi
 fi
 
 if test x$with_png = xyes; then
-AC_DEFINE(GENS_PNG, 1, PNG support via libpng)
-if test x$with_internal_png = xyes; then
-AC_DEFINE(GENS_PNG_INTERNAL, 1, Use internal libpng instead of system libpng)
-fi # GENS_PNG_INTERNAL
+	AC_DEFINE(GENS_PNG, 1, PNG support via libpng)
+	if test x$with_internal_png = xyes; then
+		AC_DEFINE(GENS_PNG_INTERNAL, 1, Use internal libpng instead of system libpng)
+	fi # GENS_PNG_INTERNAL
 fi # GENS_PNG
+
 AM_CONDITIONAL([GENS_PNG], [test x$with_png = xyes])
 AM_CONDITIONAL([GENS_PNG_INTERNAL], [test x$with_internal_png = xyes])
+
+
+###
+# LZMA / 7z. (internal only)
+###
+
+AC_ARG_WITH(7z, AC_HELP_STRING([--without-7z], [Build without internal 7z support [[default=no]]]), with_7z=$withval, with_7z=yes)
+if test x$with_7z = xyes; then
+	AC_DEFINE(GENS_LZMA, 1, LZMA/7z support via internal LZMA SDK)
+fi # GENS_LZMA
+
+AM_CONDITIONAL([GENS_LZMA], [test x$with_7z = xyes])
 
 
 ###

src/Makefile.am
@@ -1,6 +1,6 @@
 AUTOMAKE_OPTIONS = foreign
 
-SUBDIRS = starscream mdZ80 lzma
+SUBDIRS = starscream mdZ80
 
 if GENS_MP3
 SUBDIRS += mp3_dec
@@ -18,5 +18,9 @@ if GENS_PNG_INTERNAL
 SUBDIRS += libpng
 endif # GENS_PNG_INTERNAL
 endif # GENS_PNG
+
+if GENS_LZMA
+SUBDIRS += lzma
+endif # GENS_LZMA
 
 SUBDIRS += gens mdp

src/gens/Makefile.am
@@ -126,7 +126,6 @@ gens_SOURCES = \
 		util/file/file.cpp \
 		util/file/decompressor/decompressor.c \
 		util/file/decompressor/dummy.c \
-		util/file/decompressor/md_7z.c \
 		util/file/decompressor/md_rar.cpp \
 		util/file/decompressor/md_rar_t.c \
 		util/gfx/imageutil.cpp \
@@ -166,6 +165,11 @@ if GENS_ZLIB
 gens_SOURCES += \
 		util/file/decompressor/md_gzip.c \
 		util/file/decompressor/md_zip.c
+endif
+
+if GENS_LZMA
+gens_SOURCES += \
+		util/file/decompressor/md_7z.c
 endif
 
 noinst_HEADERS = \
@@ -228,7 +232,6 @@ noinst_HEADERS = \
 		util/file/decompressor/decompressor.h \
 		util/file/decompressor/popen_wrapper.h \
 		util/file/decompressor/dummy.h \
-		util/file/decompressor/md_7z.h \
 		util/file/decompressor/md_rar.hpp \
 		util/file/decompressor/md_rar_t.h \
 		util/gfx/imageutil.hpp \
@@ -274,6 +277,11 @@ noinst_HEADERS += \
 		util/file/decompressor/md_zip.h
 endif
 
+if GENS_LZMA
+noinst_HEADERS += \
+		util/file/decompressor/md_7z.h
+endif
+
 
 # Statically-linked MDP plugins.
 # TODO: Add built-in renderers automatically.
@@ -286,7 +294,6 @@ gens_LDADD = \
 		${gens_mdp_static} \
 		../starscream/libstarscream.a \
 		../mdZ80/libmdZ80.a \
-		../lzma/liblzma.a \
 		-lm $(LIBLTDL)
 
 if GENS_MP3
@@ -310,6 +317,11 @@ else
 gens_LDADD += -lpng
 endif # GENS_PNG_INTERNAL
 endif # GENS_PNG
+
+if GENS_LZMA
+gens_LDADD += ../lzma/liblzma.a
+endif # GENS_LZMA
+
 
 ## ARGH!! Get me some C4 and the address of the programmer who made me 
 ## have to do this!!
@@ -338,6 +350,10 @@ if GENS_PNG_INTERNAL
 gens_DEPENDENCIES += ../libpng/libpng.a
 endif # GENS_PNG_INTERNAL
 endif # GENS_PNG
+
+if GENS_LZMA
+gens_DEPENDENCIES += ../lzma/liblzma.a
+endif # GENS_LZMA
 
 CLEANFILES = ${ASMFILES}
 

src/gens/ui/gtk/gens_ui_gtk.cpp
@@ -385,8 +385,10 @@ static void UI_GTK_AddFilter_ROMFile(GtkWidget* dialog)
 	static const char* filter_zip  = "*.[zZ][iI][pP]";
 	static const char* filter_gz   = "*.[gG][zZ]";
 	static const char* filter_zsg  = "*.[zZ][sS][gG]";
-#endif /* GENS_ZLIB */
+#endif
+#ifdef GENS_LZMA
 	static const char* filter_7z  = "*.7[zZ]";
+#endif
 	static const char* filter_rar  = "*.[rR][aA][rR]";
 	
 	GtkFileFilter *filter = gtk_file_filter_new();
@@ -403,8 +405,10 @@ static void UI_GTK_AddFilter_ROMFile(GtkWidget* dialog)
 	gtk_file_filter_add_pattern(filter, filter_zip);
 	gtk_file_filter_add_pattern(filter, filter_gz);
 	gtk_file_filter_add_pattern(filter, filter_zsg);
-#endif /* GENS_ZLIB */
+#endif
+#ifdef GENS_LZMA
 	gtk_file_filter_add_pattern(filter, filter_7z);
+#endif
 	gtk_file_filter_add_pattern(filter, filter_rar);
 	gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);
 	
@@ -418,20 +422,25 @@ static void UI_GTK_AddFilter_ROMFile(GtkWidget* dialog)
 	gtk_file_filter_add_pattern(filter, filter_zip);
 	gtk_file_filter_add_pattern(filter, filter_gz);
 	gtk_file_filter_add_pattern(filter, filter_zsg);
-#endif /* GENS_ZLIB */
+#endif
+#ifdef GENS_LZMA
 	gtk_file_filter_add_pattern(filter, filter_7z);
+#endif
 	gtk_file_filter_add_pattern(filter, filter_rar);
 	gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);	
 	
 	// 32X ROM files only
 	filter = gtk_file_filter_new();
 	gtk_file_filter_set_name(filter, "32X ROMs");
+	gtk_file_filter_add_pattern(filter, filter_32x);
 #ifdef GENS_ZLIB
 	gtk_file_filter_add_pattern(filter, filter_zip);
 	gtk_file_filter_add_pattern(filter, filter_gz);
-#endif /* GENS_ZLIB */
-	gtk_file_filter_add_pattern(filter, filter_32x);
+	gtk_file_filter_add_pattern(filter, filter_zsg);
+#endif
+#ifdef GENS_LZMA
 	gtk_file_filter_add_pattern(filter, filter_7z);
+#endif
 	gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog), filter);
 	
 	// SegaCD disc image files only

src/gens/util/file/rom.cpp
@@ -41,9 +41,13 @@ using std::deque;
 // File Decompressors.
 #include "util/file/decompressor/decompressor.h"
 #include "util/file/decompressor/dummy.h"
-#include "util/file/decompressor/md_gzip.h"
-#include "util/file/decompressor/md_zip.h"
-#include "util/file/decompressor/md_7z.h"
+#ifdef GENS_ZLIB
+	#include "util/file/decompressor/md_gzip.h"
+	#include "util/file/decompressor/md_zip.h"
+#endif
+#ifdef GENS_LZMA
+	#include "util/file/decompressor/md_7z.h"
+#endif
 #include "util/file/decompressor/md_rar_t.h"
 
 #include "mdp/mdp_constants.h"
@@ -468,9 +472,13 @@ unsigned int ROM::loadROM(const string& filename, ROM_t** retROM)
 	// Array of decompressors.
 	static const decompressor_t* const decompressors[] =
 	{
-		&decompressor_gzip,
-		&decompressor_zip,
-		&decompressor_7z,
+		#ifdef GENS_ZLIB
+			&decompressor_gzip,
+			&decompressor_zip,
+		#endif
+		#ifdef GENS_LZMA
+			&decompressor_7z,
+		#endif
 		&decompressor_rar,
 		
 		// Last decompressor is the Dummy decompressor.

commit c26277b12e2a27d9014409afb059a2fdc6066bf1

gens_ui_win32.cpp: Added a missing "\0" in UI_Win32_FileFilter_ROMFile[].
This was broken in commit c256cac8a8a396a90b0349d38961146172ec9a13,
which was before r7_pre1 but after m6.

src/gens/ui/win32/gens_ui_win32.cpp
@@ -95,7 +95,7 @@ static const char UI_Win32_FileFilter_AllFiles[] =
 static const char UI_Win32_FileFilter_ROMFile[] =
 	"SegaCD / 32X / Genesis ROMs\0*.bin;*.smd;*.gen;*.32x;*.cue;*.iso;*.raw;" ZLIB_EXT LZMA_EXT "*.rar\0"
 	"Genesis ROMs\0*.smd;*.bin;*.gen;*.zip;*.zsg;*.gz;*.7z;*.rar\0"
-	"32X ROMs\0*.32x;" ZLIB_EXT LZMA_EXT "*.rar"
+	"32X ROMs\0*.32x;" ZLIB_EXT LZMA_EXT "*.rar\0"
 	"SegaCD Disc Images\0*.cue;*.iso;*.bin;*.raw\0"
 	"All Files\0*.*\0\0";
 