79c9754456acebd5a3ca48cdf50258bc1b44bde8
David Korth <gerbilsoft@verizon.net>
2009-11-26 11:49:38 -0500

    [Win32] bmf_window.cpp: Removed #include "libgsft/w32u/w32u_commctrl.h".

--- src/gens/ui/win32/bios_misc_files/bmf_window.cpp
+++ src/gens/ui/win32/bios_misc_files/bmf_window.cpp
@@ -42,7 +42,6 @@ using std::string;
 // Win32 includes.
 #include "libgsft/w32u/w32u.h"
 #include "libgsft/w32u/w32u_windowsx.h"
-#include "libgsft/w32u/w32u_commctrl.h"
 #include "ui/win32/fonts.h"
 #include "ui/win32/resource.h"
 
