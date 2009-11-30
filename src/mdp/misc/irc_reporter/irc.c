3338627ac4c2055bda1ac717391e8056b4b39c9f
David Korth <gerbilsoft@verizon.net>
2009-11-29 22:15:58 -0500

    irc_reporter/irc.c: Added lock-on support.
    If the word "SEGA" is present at 0x200100, the IRC Reporter plugin will
    assume that the ROM has been locked on to another ROM, in which case
    both ROM header names will be displayed.

--- src/mdp/misc/irc_reporter/irc.c
+++ src/mdp/misc/irc_reporter/irc.c
@@ -45,6 +45,7 @@ const mdp_host_t *irc_host_srv = NULL;
 
 static int MDP_FNCALL irc_event_handler(int event_id, void *event_info);
 static void MDP_FNCALL irc_init_rom(int system_id);
+static void irc_md_get_rom_name(char *buf, const char *def, uint32_t addr1, uint32_t addr2);
 
 // Data for the IPC modules.
 time_t	irc_last_modified;
@@ -146,9 +147,19 @@ static void MDP_FNCALL irc_init_rom(int system_id)
 		system = sysIDs[0];
 	}
 	
+	// Determine if we should use Overseas or Domestic as first priority.
+	// Overseas address: 0x150
+	// Domestic address: 0x120
+	// TODO: Get the current country code from MDP.
+	// For now, assume Overseas, then Domestic.
+	uint32_t cc_prio[2] = {0x150, 0x120};
+	
 	// Get the ROM name from the ROM header.
-	char rom_name_raw[128];
-	char rom_name[129];
+	static const char rom_name_unknown[] = "(ROM Name Unknown)";
+	char rom_name[128];
+	char rom_name2[128];
+	int is_locked_on = 0;
+	
 	switch (system_id)
 	{
 		case MDP_SYSTEM_MD:
@@ -156,35 +167,40 @@ static void MDP_FNCALL irc_init_rom(int system_id)
 		case MDP_SYSTEM_MCD:
 		case MDP_SYSTEM_MCD32X:
 		case MDP_SYSTEM_PICO:
+		{
 			// MD or 32X ROM. Read the header.
 			// TODO: Get the emulated country code to determine if we should try 
 			// Domestic or Overseas first. For now, use Overseas first, then Domestic.
 			// TODO: MCD and MCD32X will show the SegaCD firmware ROM name instead of the
 			// actual game name, since MDP v1.0 doesn't support reading data from
 			// the CD-ROM. This may be added in MDP v1.1.
-			irc_host_srv->mem_read_block_8(MDP_MEM_MD_ROM, 0x150, (uint8_t*)rom_name_raw, 0x30);
-			gsft_space_elim(rom_name_raw, 0x30, rom_name);
-			if (rom_name[0] == 0x00)
-			{
-				// Overseas name is blank. Try Domestic instead.
-				irc_host_srv->mem_read_block_8(MDP_MEM_MD_ROM, 0x120, (uint8_t*)rom_name_raw, 0x30);
-				gsft_space_elim(rom_name_raw, 0x30, rom_name);
-				if (rom_name[0] == 0x00)
-				{
-					// Domestic name is blank.
-					// TODO: Show the ROM filename.
-					strlcpy(rom_name, "(ROM Name Unknown)", sizeof(rom_name));
-				}
-			}
+			irc_md_get_rom_name(rom_name, rom_name_unknown,
+					    cc_prio[0], cc_prio[1]);
+			
+			// Check if this ROM has another ROM locked on at 2 MB.
+			// TODO: Use more sophisticated header checks, e.g. checking the ROM Size fields.
+			uint8_t lockon_hdr[4];
+			static const uint8_t lockon_hdr_def[4] = {'S', 'E', 'G', 'A'};
+			if (irc_host_srv->mem_read_block_8(MDP_MEM_MD_ROM, 0x200100, &lockon_hdr[0], sizeof(lockon_hdr)) != MDP_ERR_OK)
+				break;
+			
+			if (memcmp(lockon_hdr_def, lockon_hdr, sizeof(lockon_hdr)) != 0)
+				break;
+			
+			// ROM is locked on. Get the locked-on ROM name
+			irc_md_get_rom_name(rom_name2, rom_name_unknown,
+					    0x200000 | cc_prio[0], 0x200000 | cc_prio[1]);
+			is_locked_on = 1;
 			break;
+		}
 		default:
 			// Not sure how to get the ROM name for this system ID.
-			strlcpy(rom_name, "(ROM Name Unknown)", sizeof(rom_name));
+			strlcpy(rom_name, rom_name_unknown, sizeof(rom_name));
 			break;
 	}
 	
 	// Get the ROM build date from the ROM header.
-	char build_date[512];
+	char build_date[16];
 	switch (system_id)
 	{
 		case MDP_SYSTEM_MD:
@@ -212,11 +228,43 @@ static void MDP_FNCALL irc_init_rom(int system_id)
 	
 	// Create the string.
 	// TODO: Allow the user to customize this.
-	szprintf(irc_rom_string, sizeof(irc_rom_string), "[%s] %s (%s%s%d megabit%s)",
-			system, rom_name, build_date, (build_date[0] != 0 ? "; " : ""),
+	szprintf(irc_rom_string, sizeof(irc_rom_string), "[%s] %s%s%s (%s%s%d megabit%s)",
+			system, rom_name,
+			(!is_locked_on ? "" : " + "),
+			(!is_locked_on ? "" : rom_name2),
+			build_date, (build_date[0] != 0 ? "; " : ""),
 			mbits, (mbits != 1 ? "s" : ""));
 	
 	// Indicate that a ROM is loaded.
 	irc_is_rom_loaded = 1;
 	irc_last_modified = time(NULL);
+}
+
+
+/**
+ * irc_md_get_rom_name(): Get an MD ROM name.
+ * @param buf Destination buffer. (should be 128 bytes)
+ * @param def Default ROM name.
+ * @param addr1 First address to try. (0x30 bytes)
+ * @param addr2 Second address to try. (0x30 bytes)
+ */
+static void irc_md_get_rom_name(char *buf, const char *def, uint32_t addr1, uint32_t addr2)
+{
+	char rom_name_raw[128];	// Only 127 usable; using 128 for alignment.
+	
+	// Check the first address.
+	irc_host_srv->mem_read_block_8(MDP_MEM_MD_ROM, addr1, (uint8_t*)rom_name_raw, 0x30);
+	gsft_space_elim(rom_name_raw, 0x30, buf);
+	if (buf[0] == 0x00)
+	{
+		// Name at first address is blank. Try second address.
+		irc_host_srv->mem_read_block_8(MDP_MEM_MD_ROM, addr2, (uint8_t*)rom_name_raw, 0x30);
+		gsft_space_elim(rom_name_raw, 0x30, buf);
+		if (buf[0] == 0x00)
+		{
+			// Domestic name is blank.
+			// TODO: Show the ROM filename.
+			strlcpy(buf, def, 128);
+		}
+	}
 }
