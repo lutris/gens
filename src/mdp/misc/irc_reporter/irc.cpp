0b23264fb7a35acd2ec5f26f93f562294708565a
David Korth <gerbilsoft@verizon.net>
2009-11-30 00:24:41 -0500

    irc_reporter/irc.cpp: Set the default format string to match the old one.
    %D isn't implemented yet, so the build date won't show up.
    Everything else works perfectly. :)

--- src/mdp/misc/irc_reporter/irc.cpp
+++ src/mdp/misc/irc_reporter/irc.cpp
@@ -241,7 +241,7 @@ static void MDP_FNCALL irc_init_rom(int system_id)
 			build_date, (build_date[0] != 0 ? "; " : ""),
 			mbits, (mbits != 1 ? "s" : ""));
 #endif
-	string fmt_str = irc_format(system_id, "ROMs: [%T] - %N%[; [%lT] - %lN%]");
+	string fmt_str = irc_format(system_id, "[%adS] %T%[ + %lT%] (%D; %zZ)");
 	printf("str: %s\n", fmt_str.c_str());
 	strlcpy(irc_rom_string, fmt_str.c_str(), sizeof(irc_rom_string));
 	
