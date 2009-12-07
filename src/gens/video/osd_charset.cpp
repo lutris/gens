4be6c6c88290ae9fbf543f830fc9aee98684bb03
David Korth <gerbilsoft@verizon.net>
2009-12-06 21:36:52 -0500

    osd_charset.cpp: Added initial OSD font loading.
    osd_font.bin isn't part of the Gens/GS source tree. I'm probably
    going to improve bdf2osd3 (yes, yet another version of bdf2osd)
    such that it can be used like CBOB to convert the original BDF
    fonts to OSD binaries in the build system.
    
    TODO:
    - Fullwidth fonts aren't displayed correctly.
    - Add GZip support for osd_font.bin.

--- src/gens/video/osd_charset.cpp
+++ src/gens/video/osd_charset.cpp
@@ -27,6 +27,23 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+
+// LOG_MSG() support.
+#include "macros/log_msg.h"
+
+// libgsft includes.
+#include "libgsft/gsft_byteswap.h"
+
+// Needed for SetCurrentDirectory.
+// TODO: Use libgsft/w32u/ once win32-unicode is merged to master.
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <windows.h>
+#endif /* _WIN32 */
 
 // Character used if a character cannot be found.
 static const uint8_t chr_err[16] =
@@ -52,14 +69,140 @@ void osd_init(void)
 	memset(chr_font_flags, 0x00, sizeof(chr_font_flags));
 	
 	// Initialize character data with the internal VGA character set.
-	// TODO: Copy the data into memory so it can be replaced by external fonts.
 	for (unsigned int chr = 0x20; chr < 0x80; chr++)
 	{
-		chr_font_data[chr] = (void*)&VGA_charset_ASCII[chr-0x20][0];
+		chr_font_data[chr] = malloc(16);
+		memcpy(chr_font_data[chr], VGA_charset_ASCII[chr-0x20], 16);
 		chr_font_flags[chr] = CHR_FLAG_HALFWIDTH;
 	}
 	
-	// TODO: Load font data.
+#ifdef _WIN32
+	// TODO: Use pSetCurrentDirectoryU once win32-unicode is merged to master.
+	SetCurrentDirectory(PathNames.Gens_EXE_Path);
+#endif
+	
+	// Load font data from "osd_font.bin"
+	// TODO: Make it customizable?
+	// TODO: GZipped file support?
+	FILE *f_osd = fopen("osd_font.bin", "rb");
+	if (!f_osd)
+	{
+		// Couldn't open the font file.
+		LOG_MSG(gens, LOG_MSG_LEVEL_WARNING,
+			"Couldn't open 'osd_font.bin': %s", strerror(errno));
+		LOG_MSG(gens, LOG_MSG_LEVEL_WARNING,
+			"OSD will only display ASCII characters.");
+		return;
+	}
+	
+	// OSD file format stuff.
+	static const uint8_t osd_header[] = {'M', 'D', 'F', 'o', 'n', 't', '1', 0x0A};
+	static const uint8_t osd_eof[] = {'_', 'E', 'O', 'F'};
+	uint8_t buf[32];
+	
+	// Get the file header.
+	fread(buf, 1, 8, f_osd);
+	if (memcmp(osd_header, buf, sizeof(osd_header)) != 0)
+	{
+		// Invalid file header.
+		fclose(f_osd);
+		LOG_MSG(gens, LOG_MSG_LEVEL_WARNING,
+			"'osd_font.bin' is not a valid Gens/GS OSD font.");
+		LOG_MSG(gens, LOG_MSG_LEVEL_WARNING,
+			"OSD will only display ASCII characters.");
+		return;
+	}
+	
+	unsigned int num_chrs = 0;
+	unsigned int num_chrs_nonbmp = 0;
+	
+	// Read the file.
+	while (!feof(f_osd))
+	{
+		uint32_t chr;
+		uint16_t flags;
+		
+		fread(&chr, 1, sizeof(chr), f_osd);
+		if (memcmp(osd_eof, &chr, sizeof(osd_eof)) == 0)
+		{
+			// End of file.
+			break;
+		}
+		
+		fread(&flags, 1, sizeof(flags), f_osd);
+		
+		// Convert the values from little-endian to CPU format.
+		chr = le32_to_cpu(chr);
+		flags = le16_to_cpu(flags);
+		
+		if (chr > 0xFFFF)
+		{
+			// Characters outside of the BMP aren't supported.
+			if (chr > 0x10FFFF)
+			{
+				// Invalid Unicode character. Font may be corrupted.
+				LOG_MSG(gens, LOG_MSG_LEVEL_WARNING,
+					"'osd_font.bin' contains invalid character U+%04X. Font processing aborted.", chr);
+				break;
+			}
+			else
+			{
+				// Simply skip the character.
+				fseek(f_osd, 16, SEEK_CUR);
+				if (flags & CHR_FLAG_FULLWIDTH)
+					fseek(f_osd, 16, SEEK_CUR);
+				
+				num_chrs_nonbmp++;
+			}
+		}
+		
+		// Check if this is a halfwidth or fullwidth character.
+		if (!(flags & CHR_FLAG_FULLWIDTH))
+		{
+			// Halfwidth. Read 16 bytes.
+			fread(buf, 1, 16, f_osd);
+			if (chr_font_data[chr] != NULL)
+			{
+				// Character already exists. Skip it.
+				continue;
+			}
+			
+			// Copy this character to chr_font_data.
+			chr_font_data[chr] = malloc(16);
+			memcpy(chr_font_data[chr], buf, 16);
+			chr_font_flags[chr] = flags;
+		}
+		else
+		{
+			// Fullwidth. Read 32 bytes.
+			fread(buf, 1, 32, f_osd);
+			if (chr_font_data[chr] != NULL)
+			{
+				// Character already exists. Skip it.
+				continue;
+			}
+			
+			// Make sure the character is byteswapped correctly.
+			uint16_t *tmp = (uint16_t*)malloc(16 * sizeof(tmp));
+			memcpy(tmp, buf, 32);
+			for (int i = 0; i < 16; i++)
+			{
+				tmp[i] = le16_to_cpu(tmp[i]);
+			}
+			
+			chr_font_data[chr] = tmp;
+			chr_font_flags[chr] = flags;
+		}
+		
+		num_chrs++;
+	}
+	
+	// Finished reading the file.
+	fclose(f_osd);
+	
+	LOG_MSG(gens, LOG_MSG_LEVEL_INFO,
+		"%d characters loaded from 'osd_font.bin'. (%d non-BMP characters skipped)",
+		num_chrs, num_chrs_nonbmp);
 }
 
 
@@ -69,7 +212,7 @@ void osd_init(void)
 void osd_end(void)
 {
 	// Free the OSD font data.
-	for (unsigned int chr = 0x80; chr < 65536; chr++)
+	for (unsigned int chr = 0; chr < 65536; chr++)
 		free(chr_font_data[chr]);
 	
 	memset(chr_font_data, 0x00, sizeof(chr_font_data));
