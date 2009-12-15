lutris/gens

commit ef6e1af2d64414d059c6494585d09e31708404c7

md_rar.cpp: Use szprintf() to create the command line instead of a stringstream.
Also, increased buffer sizes from 1024 bytes to 4096 bytes.

src/gens/util/file/decompressor/md_rar.cpp
@@ -58,6 +58,7 @@ using std::stringstream;
 // libgsft includes.
 #include "libgsft/gsft_strdup.h"
 #include "libgsft/gsft_unused.h"
+#include "libgsft/gsft_szprintf.h"
 
 
 /**
@@ -100,11 +101,12 @@ int decompressor_rar_get_file_info(FILE *zF, const char* filename, mdp_z_entry_t
 	}
 	
 	// Build the command line.
-	stringstream ssCmd;
-	ssCmd << "\"" << Misc_Filenames.RAR_Binary << "\" v \"" << filename << "\"";
+	char cmd_line[GENS_PATH_MAX*2 + 256];
+	szprintf(cmd_line, sizeof(cmd_line), "\"%s\" v \"%s\"",
+		 Misc_Filenames.RAR_Binary, filename);
 	
 	// Open the RAR file.
-	FILE *pRAR = gens_popen(ssCmd.str().c_str(), "r");
+	FILE *pRAR = gens_popen(cmd_line, "r");
 	if (!pRAR)
 	{
 		// Error opening `rar`.
@@ -112,12 +114,12 @@ int decompressor_rar_get_file_info(FILE *zF, const char* filename, mdp_z_entry_t
 	}
 	
 	// Read from the pipe.
-	char buf[1025];
+	char buf[4096+1];
 	size_t rv;
 	stringstream ss;
-	while ((rv = fread(buf, 1, 1024, pRAR)))
+	while ((rv = fread(buf, 1, sizeof(buf)-1, pRAR)))
 	{
-		buf[rv] = 0x00;
+		buf[sizeof(buf)-1] = 0x00;
 		ss << buf;
 	}
 	gens_pclose(pRAR);
@@ -261,16 +263,18 @@ size_t decompressor_rar_get_file(FILE *zF, const char *filename,
 		return 0;
 	}
 	
-	// Build the command line.
-	stringstream ssCmd;
-	ssCmd << "\"" << Misc_Filenames.RAR_Binary << "\" p -ierr \"" << filename
-			<< "\" \"" << z_entry->filename << "\"";
-#ifndef GENS_OS_WIN32
-	ssCmd << " 2>/dev/null";
+	char cmd_line[GENS_PATH_MAX*3 + 256];
+	szprintf(cmd_line, sizeof(cmd_line), "\"%s\" p -ierr  \"%s\" \"%s\"%s",
+		 Misc_Filenames.RAR_Binary, filename, z_entry->filename,
+#if !defined(GENS_OS_WIN32)
+		" 2>/dev/null"
+#else
+		""
 #endif
+		);
 	
 	// Open the RAR file.
-	FILE *pRAR = gens_popen(ssCmd.str().c_str(), "r");
+	FILE *pRAR = gens_popen(cmd_line, "r");
 	if (!pRAR)
 	{
 		// Error opening `rar`.
@@ -281,8 +285,8 @@ size_t decompressor_rar_get_file(FILE *zF, const char *filename,
 	// Read from the pipe.
 	size_t extracted_size = 0;
 	size_t rv;
-	unsigned char bufRAR[1024];
-	while ((rv = fread(bufRAR, 1, 1024, pRAR)))
+	unsigned char bufRAR[4096];
+	while ((rv = fread(bufRAR, 1, sizeof(bufRAR), pRAR)))
 	{
 		if (extracted_size + rv > size)
 			break;

commit 59bd2d46617077bb588ef2685542b01867ed93b5

md_rar.cpp: Fixed ROM file detection in RAR archives.
This is a regression from commit ef6e1af2d64414d059c6494585d09e31708404c7.

If the RAR decompressor read 4096 bytes but there was only e.g. 2048 bytes
available in the buffer, then no data would be copied, period. This fixes
it so at least the 2048 bytes are copied (and the returned filesize is
incremented by 2048).

This bug caused ROM loading from RAR archives to fail, since the ROM format
detection code would always get 0 bytes read instead of 2048.

src/gens/util/file/decompressor/md_rar.cpp
@@ -291,6 +291,7 @@ int decompressor_rar_get_file(FILE *zF, const char *filename,
 	if (!pRAR)
 	{
 		// Error opening `rar`.
+		printf("ERR\n");
 		return -MDP_ERR_Z_EXE_NOT_FOUND;
 	}
 	
@@ -298,11 +299,21 @@ int decompressor_rar_get_file(FILE *zF, const char *filename,
 	size_t extracted_size = 0;
 	size_t rv;
 	unsigned char bufRAR[4096];
+	unsigned char *buf8 = (unsigned char*)buf;
+	
 	while ((rv = fread(bufRAR, 1, sizeof(bufRAR), pRAR)))
 	{
 		if (extracted_size + rv > size)
+		{
+			// Do a partial copy.
+			rv = (rv - (size - extracted_size));
+			memcpy(&buf8[extracted_size], &bufRAR, rv);
+			extracted_size += rv;
 			break;
-		memcpy(&((unsigned char*)buf)[extracted_size], &bufRAR, rv);
+		}
+		
+		// Do a full copy.
+		memcpy(&buf8[extracted_size], &bufRAR, rv);
 		extracted_size += rv;
 	}
 	pclose(pRAR);