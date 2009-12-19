lutris/gens

commit 7ccd43c4fb3084644e83a5f8dd973e6dfba0f4a4

gens_core/sound/psg.c: Don't ignore DATA bytes for volume registers or the noise channel.
According to SMS Power's PSG documentation[1], these DATA bytes are actually
used for the current channel instead of ignored. Some games write DATA bytes
where DATA bytes shouldn't be used, so those games would experience sound
issues. (The PSG documentation lists a few SMS games that do this.)

I also tested this in Regen, and Regen applies the DATA bytes correctly
instead of ignoring them.

[1] http://www.smspower.org/dev/docs/wiki/Sound/PSG#writes

src/gens/gens_core/sound/psg.c
@@ -131,6 +131,7 @@ void PSG_Write(int data)
 	
 	if (data & 0x80)
 	{
+		// LATCH/DATA byte.
 		PSG.Current_Register = (data & 0x70) >> 4;
 		PSG.Current_Channel = PSG.Current_Register >> 1;
 		
@@ -138,78 +139,53 @@ void PSG_Write(int data)
 		
 		PSG.Register[PSG.Current_Register] =
 			(PSG.Register[PSG.Current_Register] & 0x3F0) | data;
+	}
+	
+	if (PSG.Current_Register & 1)
+	{
+		// Volume
+		PSG_Special_Update();
+		PSG.Volume[PSG.Current_Channel] = PSG_Volume_Table[data];
 		
-		if (PSG.Current_Register & 1)
-		{
-			// Volume
-			PSG_Special_Update();
-			PSG.Volume[PSG.Current_Channel] = PSG_Volume_Table[data];
-			
-			LOG_MSG(psg, LOG_MSG_LEVEL_DEBUG1,
-				"channel %d    volume = %.8X",
-				PSG.Current_Channel,
-				PSG.Volume[PSG.Current_Channel]);
-		}
-		else
-		{
-			// Frequency
-			PSG_Special_Update();
-			
-			if (PSG.Current_Channel != 3)
-			{
-				// Normal channel
-				PSG.CntStep[PSG.Current_Channel] =
-					PSG_Step_Table[PSG.Register[PSG.Current_Register]];
-				
-				if ((PSG.Current_Channel == 2) && ((PSG.Register[6] & 3) == 3))
-					PSG.CntStep[3] = PSG.CntStep[2] >> 1;
-				
-				LOG_MSG(psg, LOG_MSG_LEVEL_DEBUG1,
-					"channel %d    step = %.8X",
-					PSG.Current_Channel,
-					PSG.CntStep[PSG.Current_Channel]);
-			}
-			else
-			{
-				// Noise channel
-				PSG.LFSR = LFSR_INIT;
-				PSG_Noise_Step_Table[3] = PSG.CntStep[2] >> 1;
-				PSG.CntStep[3] = PSG_Noise_Step_Table[data & 3];
-				
-				// Check if we should use white noise or periodic noise.
-				if (data & 4)
-					PSG.LFSR_Mask = LFSR_MASK_WHITE;
-				else
-					PSG.LFSR_Mask = LFSR_MASK_PERIODIC;
-				
-				LOG_MSG(psg, LOG_MSG_LEVEL_DEBUG1,
-					"channel N    type = %.2X", data);
-			}
-		}
+		LOG_MSG(psg, LOG_MSG_LEVEL_DEBUG1,
+			"channel %d    volume = %.8X",
+			PSG.Current_Channel,
+			PSG.Volume[PSG.Current_Channel]);
 	}
 	else
 	{
-		if (!(PSG.Current_Register & 1))
+		// Frequency
+		PSG_Special_Update();
+		
+		if (PSG.Current_Channel != 3)
 		{
-			// Frequency
-			if (PSG.Current_Channel != 3)
-			{
-				PSG_Special_Update();
-				
-				PSG.Register[PSG.Current_Register] =
-					(PSG.Register[PSG.Current_Register] & 0x0F) | ((data & 0x3F) << 4);
-				
-				PSG.CntStep[PSG.Current_Channel] =
-					PSG_Step_Table[PSG.Register[PSG.Current_Register]];
-				
-				if ((PSG.Current_Channel == 2) && ((PSG.Register[6] & 3) == 3))
-					PSG.CntStep[3] = PSG.CntStep[2] >> 1;
-				
-				LOG_MSG(psg, LOG_MSG_LEVEL_DEBUG1,
-					"channel %d    step = %.8X",
-					PSG.Current_Channel,
-					PSG.CntStep[PSG.Current_Channel]);
-			}
+			// Normal channel
+			PSG.CntStep[PSG.Current_Channel] =
+				PSG_Step_Table[PSG.Register[PSG.Current_Register]];
+			
+			if ((PSG.Current_Channel == 2) && ((PSG.Register[6] & 3) == 3))
+				PSG.CntStep[3] = PSG.CntStep[2] >> 1;
+			
+			LOG_MSG(psg, LOG_MSG_LEVEL_DEBUG1,
+				"channel %d    step = %.8X",
+				PSG.Current_Channel,
+				PSG.CntStep[PSG.Current_Channel]);
+		}
+		else
+		{
+			// Noise channel
+			PSG.LFSR = LFSR_INIT;
+			PSG_Noise_Step_Table[3] = PSG.CntStep[2] >> 1;
+			PSG.CntStep[3] = PSG_Noise_Step_Table[data & 3];
+			
+			// Check if we should use white noise or periodic noise.
+			if (data & 4)
+				PSG.LFSR_Mask = LFSR_MASK_WHITE;
+			else
+				PSG.LFSR_Mask = LFSR_MASK_PERIODIC;
+			
+			LOG_MSG(psg, LOG_MSG_LEVEL_DEBUG1,
+				"channel N    type = %.2X", data);
 		}
 	}
 }

commit 2a32a8745a16cdc6d7451ba3f17de40a95444e7a

gens_core/sound/psg.c: Fixed loading the upper 6 bits of the Tone registers.
Also, mask off the data byte when updating the volume information.

These regressed in commit 7ccd43c4fb3084644e83a5f8dd973e6dfba0f4a4.

src/gens/gens_core/sound/psg.c
@@ -135,17 +135,22 @@ void PSG_Write(int data)
 		PSG.Current_Register = (data & 0x70) >> 4;
 		PSG.Current_Channel = PSG.Current_Register >> 1;
 		
-		data &= 0x0F;
-		
+		// Save the data value in the register.
 		PSG.Register[PSG.Current_Register] =
-			(PSG.Register[PSG.Current_Register] & 0x3F0) | data;
+			(PSG.Register[PSG.Current_Register] & 0x3F0) | (data & 0x0F);
+	}
+	else if (!(PSG.Current_Register & 1) && PSG.Current_Channel != 3)
+	{
+		// DATA byte: Tone. (Upper 6 bits.)
+		PSG.Register[PSG.Current_Register] =
+			(PSG.Register[PSG.Current_Register] & 0x0F) | ((data & 0x3F) << 4);
 	}
 	
 	if (PSG.Current_Register & 1)
 	{
-		// Volume
+		// Volume register.
 		PSG_Special_Update();
-		PSG.Volume[PSG.Current_Channel] = PSG_Volume_Table[data];
+		PSG.Volume[PSG.Current_Channel] = PSG_Volume_Table[data & 0x0F];
 		
 		LOG_MSG(psg, LOG_MSG_LEVEL_DEBUG1,
 			"channel %d    volume = %.8X",