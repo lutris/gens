lutris/gens

commit 39c907ae0f8c094ea2fde8a8a096fb82caf5d13f

pwm.c: New and improved PWM scaling algorithm.
This patch improves audio in both Doom 32X and Wolf32X, and probably
many other 32X games.

This patch was submitted by Chilly Willy on the Sonic Retro forums.

src/gens/gens_core/sound/pwm.c
@@ -72,6 +72,11 @@ unsigned int PWM_Int_Tmp;
 unsigned int PWM_FIFO_L_Tmp;
 unsigned int PWM_FIFO_R_Tmp;
 
+/* PWM scaling variables. */
+static int PWM_Offset;
+static int PWM_Scale;
+static int PWM_Loudness;
+
 
 /**
  * PWM_Init(): Initialize the PWM audio emulator.
@@ -94,8 +99,16 @@ void PWM_Init(void)
 	PWM_FIFO_L_Tmp = 0;
 	PWM_FIFO_R_Tmp = 0;
 	
+	PWM_Loudness = 0;
 	PWM_Set_Cycle(0);
 	PWM_Set_Int(0);
+}
+
+
+void PWM_Recalc_Scale(void)
+{
+	PWM_Offset = (PWM_Cycle / 2) + 1;
+	PWM_Scale = 0x7FFF00 / PWM_Offset;
 }
 
 
@@ -104,6 +117,8 @@ void PWM_Set_Cycle(unsigned int cycle)
 	cycle--;
 	PWM_Cycle = (cycle & 0xFFF);
 	PWM_Cycle_Cnt = PWM_Cycles;
+	
+	PWM_Recalc_Scale();
 }
 
 
@@ -198,6 +213,7 @@ static void PWM_Shift_Data(void)
 void PWM_Update_Timer(unsigned int cycle)
 {
 	// Don't do anything if PWM is disabled in the Sound menu.
+	// FIXME: This seems to cause some games to freeze.
 	if (!PWM_Enable)
 		return;
 	
@@ -230,11 +246,9 @@ void PWM_Update(int **buf, int length)
 	if (!PWM_Enable)
 		return;
 	
-	// TODO: Calculate PWM_adjust when PWM_Cycle is written.
-	// TODO: This supports a max cycle count of 1024. Figure out how to make it better.
-	const int PWM_adjust = (((PWM_Cycle >> 1) + 1) << 5);
-	int tmpOutL = ((PWM_Out_L & 0xFFFF) << 5) - PWM_adjust;
-	int tmpOutR = ((PWM_Out_R & 0xFFFF) << 5) - PWM_adjust;
+	// New PWM scaling algorithm provided by Chilly Willy on the Sonic Retro forums.
+	int tmpOutL = (((int)(PWM_Out_L & 0xFFF) - PWM_Offset) * PWM_Scale) >> (8 - PWM_Loudness);
+	int tmpOutR = (((int)(PWM_Out_R & 0xFFF) - PWM_Offset) * PWM_Scale) >> (8 - PWM_Loudness);
 	
 	// Multiply PWM by 4 so it's audible.
 	tmpOutL <<= 2;

src/gens/gens_core/sound/pwm.h
@@ -47,6 +47,7 @@ extern unsigned int PWM_Out_R;
 extern unsigned int PWM_Out_L;
 
 void PWM_Init(void);
+void PWM_Recalc_Scale(void);
 
 /* Functions called by x86 asm. */
 void PWM_Set_Cycle(unsigned int cycle);

src/gens/util/file/save.cpp
@@ -1881,7 +1881,8 @@ void Savestate::GsxImport32X(const unsigned char* data)
 	ImportDataAuto(&PWM_Mode, data, offset, sizeof(PWM_Mode));
 	ImportDataAuto(&PWM_Out_R, data, offset, sizeof(PWM_Out_R));
 	ImportDataAuto(&PWM_Out_L, data, offset, sizeof(PWM_Out_L));
-
+	PWM_Recalc_Scale();
+	
 	ImportDataAuto(_32X_Rom, data, offset, 1024); // just in case some of these bytes are not in fact read-only as was apparently the case with Sega CD games (1024 seems acceptably small)
 	ImportDataAuto(_32X_MSH2_Rom, data, offset, sizeof(_32X_MSH2_Rom));
 	ImportDataAuto(_32X_SSH2_Rom, data, offset, sizeof(_32X_SSH2_Rom));

commit a3fbeb8ae65982072d6251464307d72f243cdd84

pwm.c: Temporarily reverted Chilly Willy's PWM scaling algorithm, plus some other changes.
The changes from commit 39c907ae0f8c094ea2fde8a8a096fb82caf5d13f broke drx's
"Sonic 32X" hack. Specifically, the "SEGA" sound was completely messed up.
(NOTE: This hack requires the official 32X firmware in order to run; otherwise,
it will freeze on the "SEGA" screen.)

Other changes:

- Run PWM_Update_Timer() even if PWM sound output is disabled. Otherwise,
  some games will freeze.

- Split the PWM calculation into a static inline function.

- If both PWM_Out_L and PWM_Out_R are 0, return from PWM_Update() immediately.

- Don't check PWM_Out_L and PWM_Out_R in the while() loop in PWM_Update().
  This requires a check every iteration and is wasteful, since if these
  variables are 0, then the output will be zero. (That is, the output will
  be zero with the new PWM_Update_Scale() static inline function.)

src/gens/gens_core/sound/pwm.c
@@ -72,10 +72,13 @@ unsigned int PWM_Int_Tmp;
 unsigned int PWM_FIFO_L_Tmp;
 unsigned int PWM_FIFO_R_Tmp;
 
+#if 0
+// TODO: Fix Chilly Willy's new scaling algorithm.
 /* PWM scaling variables. */
 static int PWM_Offset;
 static int PWM_Scale;
 static int PWM_Loudness;
+#endif
 
 
 /**
@@ -99,17 +102,24 @@ void PWM_Init(void)
 	PWM_FIFO_L_Tmp = 0;
 	PWM_FIFO_R_Tmp = 0;
 	
+#if 0
+// TODO: Fix Chilly Willy's new scaling algorithm.
 	PWM_Loudness = 0;
+#endif
+	
 	PWM_Set_Cycle(0);
 	PWM_Set_Int(0);
 }
 
 
+#if 0
+// TODO: Fix Chilly Willy's new scaling algorithm.
 void PWM_Recalc_Scale(void)
 {
 	PWM_Offset = (PWM_Cycle / 2) + 1;
 	PWM_Scale = 0x7FFF00 / PWM_Offset;
 }
+#endif
 
 
 void PWM_Set_Cycle(unsigned int cycle)
@@ -118,7 +128,10 @@ void PWM_Set_Cycle(unsigned int cycle)
 	PWM_Cycle = (cycle & 0xFFF);
 	PWM_Cycle_Cnt = PWM_Cycles;
 	
+#if 0
+	// TODO: Fix Chilly Willy's new scaling algorithm.
 	PWM_Recalc_Scale();
+#endif
 }
 
 
@@ -213,9 +226,6 @@ static void PWM_Shift_Data(void)
 void PWM_Update_Timer(unsigned int cycle)
 {
 	// Don't do anything if PWM is disabled in the Sound menu.
-	// FIXME: This seems to cause some games to freeze.
-	if (!PWM_Enable)
-		return;
 	
 	// Don't do anything if PWM isn't active.
 	if ((PWM_Mode & 0x0F) == 0x00)
@@ -241,25 +251,45 @@ void PWM_Update_Timer(unsigned int cycle)
 }
 
 
+static inline int PWM_Update_Scale(int PWM_In)
+{
+	if (PWM_In == 0)
+		return 0;
+	
+	// TODO: Chilly Willy's new scaling algorithm breaks drx's Sonic 1 32X (with PWM drums).
+	//return (((PWM_In & 0xFFFF) - PWM_Offset) * PWM_Scale) >> (8 - PWM_Loudness);
+	const int PWM_adjust = (((PWM_Cycle >> 1) + 1) << 5);
+	int PWM_Ret = (((PWM_In & 0xFFFF) << 5) - PWM_adjust);
+	
+	// Multiply PWM by 4 so it's audible.
+	PWM_Ret <<= 2;
+	
+	// Make sure the PWM isn't oversaturated.
+	if (PWM_Ret > 32767)
+		PWM_Ret = 32767;
+	else if (PWM_Ret < -32768)
+		PWM_Ret = -32768;
+	
+	return PWM_Ret;
+}
+
+
 void PWM_Update(int **buf, int length)
 {
 	if (!PWM_Enable)
 		return;
 	
-	// New PWM scaling algorithm provided by Chilly Willy on the Sonic Retro forums.
-	int tmpOutL = (((int)(PWM_Out_L & 0xFFF) - PWM_Offset) * PWM_Scale) >> (8 - PWM_Loudness);
-	int tmpOutR = (((int)(PWM_Out_R & 0xFFF) - PWM_Offset) * PWM_Scale) >> (8 - PWM_Loudness);
+	if (PWM_Out_L == 0 && PWM_Out_R == 0)
+		return;
 	
-	// Multiply PWM by 4 so it's audible.
-	tmpOutL <<= 2;
-	tmpOutR <<= 2;
+	// New PWM scaling algorithm provided by Chilly Willy on the Sonic Retro forums.
+	int tmpOutL = PWM_Update_Scale((int)PWM_Out_L);
+	int tmpOutR = PWM_Update_Scale((int)PWM_Out_R);
 	
 	while (length > 0)
 	{
-		if (PWM_Out_L)
-			buf[0][length-1] += tmpOutL;
-		if (PWM_Out_R)
-			buf[1][length-1] += tmpOutR;
+		buf[0][length-1] += tmpOutL;
+		buf[1][length-1] += tmpOutR;
 		
 		length--;
 	}

src/gens/gens_core/sound/pwm.h
@@ -47,7 +47,11 @@ extern unsigned int PWM_Out_R;
 extern unsigned int PWM_Out_L;
 
 void PWM_Init(void);
+
+#if 0
+// TODO: Fix Chilly Willy's new scaling algorithm.
 void PWM_Recalc_Scale(void);
+#endif
 
 /* Functions called by x86 asm. */
 void PWM_Set_Cycle(unsigned int cycle);

src/gens/util/file/save.cpp
@@ -1910,7 +1910,10 @@ void Savestate::GsxImport32X(const unsigned char* data)
 	PWM_Out_R		= le32_to_cpu(sv.pwm_out_R);
 	PWM_Out_L		= le32_to_cpu(sv.pwm_out_L);
 	
+#if 0
+	// TODO: Fix Chilly Willy's new scaling algorithm.
 	PWM_Recalc_Scale();
+#endif
 	
 	// just in case some of these bytes are not in fact read-only
 	// as was apparently the case with Sega CD games (1024 seems acceptably small)