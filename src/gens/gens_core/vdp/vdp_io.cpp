lutris/gens

commit c24125a2b0a22adb1c352390c06df585575a97a4

vdp_io.cpp::VDP_Update_Mode(): Swapped the M2 and M3 bits.
The official TMS9918 and Sega documentation have these bits swapped
for some reason.

src/gens/gens_core/vdp/vdp_io.cpp
@@ -308,11 +308,12 @@ void VDP_Set_Visible_Lines(void)
  */
 static inline void VDP_Update_Mode(void)
 {
-	register uint8_t Set1 = VDP_Reg.m5.Set1;
-	register uint8_t Set2 = VDP_Reg.m5.Set2;
+	const register uint8_t Set1 = VDP_Reg.m5.Set1;
+	const register uint8_t Set2 = VDP_Reg.m5.Set2;
 	VDP_Mode = ((Set2 & 0x10) >> 4) |	// M1
-		   ((Set2 & 0x08) >> 2) |	// M2
-		   ((Set1 & 0x06) << 1) |	// M3, M4/PSEL
+		   ((Set1 & 0x02))      |	// M2
+		   ((Set2 & 0x04))	|	// M3
+		   ((Set1 & 0x04) << 1) |	// M4/PSEL
 		   ((Set2 & 0x04) << 2);	// M5
 	
 	// CRam needs to be updated.

commit 30fac06afc12957bbcbfd61718944121e5c8125f

vdp_io.cpp::VDP_Set_Visible_Lines(): Fixed 240-line mode.
This is a regression from commit c24125a2b0a22adb1c352390c06df585575a97a4,
which swapped the M2 and M3 bits to match various unofficial MD documentation
and Mednafen.

src/gens/gens_core/vdp/vdp_io.cpp
@@ -263,7 +263,7 @@ void VDP_Set_Visible_Lines(void)
 	else if (VDP_Mode & VDP_MODE_M5)
 	{
 		// Mode 5. Must be either 224 lines or 240 lines.
-		if (VDP_Mode & VDP_MODE_M2)
+		if (VDP_Mode & VDP_MODE_M3)
 			LineOffset = 2; // 240 lines.
 		else
 			LineOffset = 1; // 224 lines.