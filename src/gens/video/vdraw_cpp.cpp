lutris/gens

commit 0431566c5a868c9003784f5b15853c5c80449042

vdp_rend.cpp: Combined MD_Screen[] and MD_Screen32[] into a union.
This will save around 160 KB of memory.

Updated the rest of Gens/GS to work with the new unionized MD_Screen.

src/gens/video/vdraw_cpp.cpp
@@ -132,7 +132,7 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			{
 				// Plugin supports 555to555.
 				bppMD = 15;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen[8]);
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u16[8]);
 				vdraw_rInfo.vmodeFlags |=
 					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_555, MDP_RENDER_VMODE_RGB_555);
 			}
@@ -140,7 +140,7 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			{
 				// Plugin supports 565to555.
 				bppMD = 16;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen[8]);
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u16[8]);
 				vdraw_rInfo.vmodeFlags |=
 					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_565, MDP_RENDER_VMODE_RGB_555);
 			}
@@ -148,7 +148,7 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			{
 				// Plugin supports 888to555.
 				bppMD = 32;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen32[8]);
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u32[8]);
 				vdraw_rInfo.vmodeFlags |=
 					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_888, MDP_RENDER_VMODE_RGB_555);
 			}
@@ -158,7 +158,7 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 				// TODO: Add a variable to indicate automatic color depth conversion.
 				// For now, just outputs 555.
 				bppMD = 15;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen[8]);
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u16[8]);
 				vdraw_rInfo.vmodeFlags |=
 					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_555, MDP_RENDER_VMODE_RGB_555);
 				vdraw_needs_conversion = true;
@@ -172,7 +172,7 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			{
 				// Plugin supports 565to565.
 				bppMD = 16;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen[8]);
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u16[8]);
 				vdraw_rInfo.vmodeFlags |=
 					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_565, MDP_RENDER_VMODE_RGB_565);
 			}
@@ -180,7 +180,7 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			{
 				// Plugin supports 555to565.
 				bppMD = 15;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen[8]);
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u16[8]);
 				vdraw_rInfo.vmodeFlags |=
 					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_555, MDP_RENDER_VMODE_RGB_565);
 			}
@@ -188,7 +188,7 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			{
 				// Plugin supports 888to565.
 				bppMD = 32;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen32[8]);
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u32[8]);
 				vdraw_rInfo.vmodeFlags |=
 					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_888, MDP_RENDER_VMODE_RGB_565);
 			}
@@ -198,7 +198,7 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 				// TODO: Add a variable to indicate automatic color depth conversion.
 				// For now, just outputs 565.
 				bppMD = 16;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen[8]);
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u16[8]);
 				vdraw_rInfo.vmodeFlags |=
 					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_565, MDP_RENDER_VMODE_RGB_565);
 				vdraw_needs_conversion = true;
@@ -211,7 +211,7 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			{
 				// Plugin supports 888to888.
 				bppMD = 32;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen32[8]);
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u32[8]);
 				vdraw_rInfo.vmodeFlags |=
 					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_888, MDP_RENDER_VMODE_RGB_888);
 			}
@@ -219,7 +219,7 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			{
 				// Plugin supports 565to888.
 				bppMD = 16;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen[8]);
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u16[8]);
 				vdraw_rInfo.vmodeFlags |=
 					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_565, MDP_RENDER_VMODE_RGB_888);
 			}
@@ -227,7 +227,7 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			{
 				// Plugin supports 555to888.
 				bppMD = 15;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen[8]);
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u16[8]);
 				vdraw_rInfo.vmodeFlags |=
 					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_555, MDP_RENDER_VMODE_RGB_888);
 			}
@@ -235,11 +235,11 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			{
 				// Plugin doesn't support 888 output at all.
 				// TODO: Add a variable to indicate automatic color depth conversion.
-				// For now, just outputs 888.
-				bppMD = 16;
-				vdraw_rInfo.mdScreen = (void*)(&MD_Screen[8]);
+				// For now, just output 888.
+				bppMD = 32;
+				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u32[8]);
 				vdraw_rInfo.vmodeFlags |=
-					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_565, MDP_RENDER_VMODE_RGB_565);
+					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_888, MDP_RENDER_VMODE_RGB_888);
 				vdraw_needs_conversion = true;
 			}
 			break;

src/gens/debugger/debugger.cpp
@@ -948,17 +948,23 @@ static void Refresh_VDP_Palette(void)
 {
 	Print_Text("******** VDP PALETTE ********", 180, 0, TEXT_RED);
 	
-	if (bppMD == 32)
+	if (bppMD == 15)
+	{
+		// 15-bit color palette update.
+		T_Refresh_VDP_Palette_Colors<uint16_t>(MD_Screen.u16, MD_Palette, 4);
+		T_Refresh_VDP_Palette_Outline<uint16_t>(MD_Screen.u16, 0x03, 0x7FFF);
+	}
+	else if (bppMD == 16)
+	{
+		// 16-bit color palette update.
+		T_Refresh_VDP_Palette_Colors<uint16_t>(MD_Screen.u16, MD_Palette, 4);
+		T_Refresh_VDP_Palette_Outline<uint16_t>(MD_Screen.u16, 0x03, 0xFFFF);
+	}
+	else //if (bppMD == 32)
 	{
 		// 32-bit color palette update.
-		T_Refresh_VDP_Palette_Colors<uint32_t>(MD_Screen32, MD_Palette32, 4);
-		T_Refresh_VDP_Palette_Outline<uint32_t>(MD_Screen32, 0x03, 0xFFFFFF);
-	}
-	else
-	{
-		// 15/16-bit color palette update.
-		T_Refresh_VDP_Palette_Colors<uint16_t>(MD_Screen, MD_Palette, 4);
-		T_Refresh_VDP_Palette_Outline<uint16_t>(MD_Screen, 0x03, 0xFFFF);
+		T_Refresh_VDP_Palette_Colors<uint32_t>(MD_Screen.u32, MD_Palette32, 4);
+		T_Refresh_VDP_Palette_Outline<uint32_t>(MD_Screen.u32, 0x03, 0xFFFFFF);
 	}
 	
 	Print_Text("******** VDP CONTROL ********", 180, 60, TEXT_WHITE);
@@ -1134,17 +1140,23 @@ static void Refresh_Word_RAM_Pattern(void)
 	
 	Print_Text("******** VDP PALETTE ********", 180, 0, TEXT_RED);
 	
-	if (bppMD == 32)
+	if (bppMD == 15)
+	{
+		// 15-bit color palette update.
+		T_Refresh_VDP_Palette_Colors<uint16_t>(MD_Screen.u16, MD_Palette, 16);
+		T_Refresh_VDP_Palette_Outline<uint16_t>(MD_Screen.u16, 0x0F, 0x7FFF);
+	}
+	else if (bppMD == 16)
+	{
+		// 16-bit color palette update.
+		T_Refresh_VDP_Palette_Colors<uint16_t>(MD_Screen.u16, MD_Palette, 16);
+		T_Refresh_VDP_Palette_Outline<uint16_t>(MD_Screen.u16, 0x0F, 0xFFFF);
+	}
+	else //if (bppMD == 32)
 	{
 		// 32-bit color palette update.
-		T_Refresh_VDP_Palette_Colors<uint32_t>(MD_Screen32, MD_Palette32, 16);
-		T_Refresh_VDP_Palette_Outline<uint32_t>(MD_Screen32, 0x0F, 0xFFFFFF);
-	}
-	else
-	{
-		// 15/16-bit color palette update.
-		T_Refresh_VDP_Palette_Colors<uint16_t>(MD_Screen, MD_Palette, 16);
-		T_Refresh_VDP_Palette_Outline<uint16_t>(MD_Screen, 0x0F, 0xFFFF);
+		T_Refresh_VDP_Palette_Colors<uint32_t>(MD_Screen.u32, MD_Palette32, 16);
+		T_Refresh_VDP_Palette_Outline<uint32_t>(MD_Screen.u32, 0x0F, 0xFFFFFF);
 	}
 	
 	Print_Text("****** WORD RAM PATTERN ******", 28, 0, TEXT_GREEN);
@@ -1191,9 +1203,9 @@ void Update_Debug_Screen(void)
 {
 	// Clear the MD screen.
 	if (bppMD == 32)
-		memset(MD_Screen32, 0x00, sizeof(MD_Screen32));
+		memset(&MD_Screen.u32, 0x00, sizeof(MD_Screen.u32));
 	else
-		memset(MD_Screen, 0x00, sizeof(MD_Screen));
+		memset(&MD_Screen.u16, 0x00, sizeof(MD_Screen.u16));
 	
 	if (debug_show_vdp)
 	{

src/gens/debugger/mcd_cell_dump.asm
@@ -26,7 +26,6 @@ section .data align=64
 	
 	extern SYM(MD_Screen)
 	extern SYM(MD_Palette)
-	extern SYM(MD_Screen32)
 	extern SYM(MD_Palette32)
 	
 	; MD bpp
@@ -111,7 +110,7 @@ align 16
 
 ._32BIT:
 	shl	ebp, 6
-	lea	edi, [SYM(MD_Screen32) + (((10+8)*336)+30)*4]	; edi = MD_Screen + copy offset
+	lea	edi, [SYM(MD_Screen) + (((10+8)*336)+30)*4]	; edi = MD_Screen + copy offset
 
 .Loop_EDX32:
 	mov	ecx, 16					; ecx = Number of patterns per row
@@ -215,7 +214,7 @@ align 16
 
 ._32BIT:
 	shl	ebp, 6
-	lea	edi, [SYM(MD_Screen32) + (((10+8)*336)+30)*4]	; edi = MD_Screen + copy offset
+	lea	edi, [SYM(MD_Screen) + (((10+8)*336)+30)*4]	; edi = MD_Screen + copy offset
 
 .Loop_EDX32:
 	mov	ecx, 16					; ecx = Number of patterns per row
@@ -349,7 +348,7 @@ align 16
 
 ._32BIT:
 	shl	ebp, 1
-	lea	edi, [SYM(MD_Screen32) + (((10+8)*336)+30)*4]	; edi = MD_Screen + copy offset
+	lea	edi, [SYM(MD_Screen) + (((10+8)*336)+30)*4]	; edi = MD_Screen + copy offset
 
 .Loop_EDX32:
 	mov	ecx, 16					; ecx = Number of patterns per row
@@ -484,7 +483,7 @@ align 16
 
 ._32BIT:
 	shl	ebp, 1
-	lea	edi, [SYM(MD_Screen32) + (((10+8)*336)+30)*4]	; edi = MD_Screen + copy offset
+	lea	edi, [SYM(MD_Screen) + (((10+8)*336)+30)*4]	; edi = MD_Screen + copy offset
 
 .Loop_EDX32:
 	mov	ecx, 16					; ecx = Number of patterns per row

src/gens/emulator/g_32x.cpp
@@ -629,9 +629,9 @@ static inline int T_gens_do_32X_frame(void)
 		screen_offset += ((320 - post_frame.width) / 2);
 	
 	if (bppMD == 32)
-		post_frame.md_screen = &MD_Screen32[screen_offset];
+		post_frame.md_screen = &MD_Screen.u32[screen_offset];
 	else
-		post_frame.md_screen = &MD_Screen[screen_offset];
+		post_frame.md_screen = &MD_Screen.u16[screen_offset];
 	
 	EventMgr::RaiseEvent(MDP_EVENT_POST_FRAME, &post_frame);
 	

src/gens/emulator/g_main.cpp
@@ -351,8 +351,7 @@ int W_VSync;
 int ice = 0;
 void Clear_Screen_MD(void)
 {
-	memset(MD_Screen, 0x00, sizeof(MD_Screen));
-	memset(MD_Screen32, 0x00, sizeof(MD_Screen32));
+	memset(&MD_Screen, 0x00, sizeof(MD_Screen));
 }
 
 

src/gens/emulator/g_mcd.cpp
@@ -383,22 +383,22 @@ static void SegaCD_Display_LED(void)
 	if (LED_Status & 2)
 	{
 		if (bppMD == 15)
-			T_Draw_SegaCD_LED(MD_Screen, ledReady_15, 0);
+			T_Draw_SegaCD_LED(MD_Screen.u16, ledReady_15, 0);
 		else if (bppMD == 16)
-			T_Draw_SegaCD_LED(MD_Screen, ledReady_16, 0);
+			T_Draw_SegaCD_LED(MD_Screen.u16, ledReady_16, 0);
 		else //if (bppMD == 32)
-			T_Draw_SegaCD_LED(MD_Screen32, ledReady_32, 0);
+			T_Draw_SegaCD_LED(MD_Screen.u32, ledReady_32, 0);
 	}
 	
 	// Access LED
 	if (LED_Status & 1)
 	{
 		if (bppMD == 15)
-			T_Draw_SegaCD_LED(MD_Screen, ledAccess_15, 8);
+			T_Draw_SegaCD_LED(MD_Screen.u16, ledAccess_15, 8);
 		else if (bppMD == 16)
-			T_Draw_SegaCD_LED(MD_Screen, ledAccess_16, 8);
+			T_Draw_SegaCD_LED(MD_Screen.u16, ledAccess_16, 8);
 		else //if (bppMD == 32)
-			T_Draw_SegaCD_LED(MD_Screen32, ledAccess_32, 8);
+			T_Draw_SegaCD_LED(MD_Screen.u32, ledAccess_32, 8);
 	}
 }
 
@@ -681,9 +681,9 @@ static inline int T_gens_do_MCD_frame(void)
 		screen_offset += ((320 - post_frame.width) / 2);
 	
 	if (bppMD == 32)
-		post_frame.md_screen = &MD_Screen32[screen_offset];
+		post_frame.md_screen = &MD_Screen.u32[screen_offset];
 	else
-		post_frame.md_screen = &MD_Screen[screen_offset];
+		post_frame.md_screen = &MD_Screen.u16[screen_offset];
 	
 	EventMgr::RaiseEvent(MDP_EVENT_POST_FRAME, &post_frame);
 	

src/gens/emulator/g_md.cpp
@@ -602,9 +602,9 @@ static inline int T_gens_do_MD_frame(void)
 		screen_offset += ((320 - post_frame.width) / 2);
 	
 	if (bppMD == 32)
-		post_frame.md_screen = &MD_Screen32[screen_offset];
+		post_frame.md_screen = &MD_Screen.u32[screen_offset];
 	else
-		post_frame.md_screen = &MD_Screen[screen_offset];
+		post_frame.md_screen = &MD_Screen.u16[screen_offset];
 	
 	EventMgr::RaiseEvent(MDP_EVENT_POST_FRAME, &post_frame);
 	

src/gens/gens_core/gfx/fastblur_32_x86.S
@@ -28,7 +28,7 @@
 
 /* External symbols. */
 #ifndef __APPLE__
-.extern SYM(MD_Screen32)
+.extern SYM(MD_Screen)
 #endif
 
 /** .data section **/
@@ -57,7 +57,7 @@ SYM(Fast_Blur_32_x86):
 	pushl	%esi
 	
 	/* Start at the beginning of the actual display data. */
-	movl	$(SYM(MD_Screen32) + (8*4)), %esi
+	movl	$(SYM(MD_Screen) + (8*4)), %esi
 	movl	$(MD_SCREEN_SIZE - 8), %ecx
 	xorl	%ebx, %ebx
 	jmp	0f
@@ -101,7 +101,7 @@ SYM(Fast_Blur_32_x86_mmx):
 	pushl	%esi
 	
 	/* Start at the beginning of the actual display data. */
-	movl	$(SYM(MD_Screen32) + (8*4)), %esi
+	movl	$(SYM(MD_Screen) + (8*4)), %esi
 	movl	$((MD_SCREEN_SIZE - 8) / 2), %ecx
 	
 	/* Load the 32-bit color mask. */

commit 2bc972112e8e0e7a7d66e72d912fe138deafd5e9

vdraw_cpp.cpp::vdraw_set_renderer(): Fixed a regression that broke automatic color conversion.
This is a regression from commit 0431566c5a868c9003784f5b15853c5c80449042.

src/gens/video/vdraw_cpp.cpp
@@ -155,8 +155,8 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			else
 			{
 				// Plugin doesn't support 555 output at all.
-				// TODO: Add a variable to indicate automatic color depth conversion.
-				// For now, just outputs 555.
+				// Render as 555, and convert it afterwards.
+				// TODO: Check if the plugin supports 555 rendering.
 				bppMD = 15;
 				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u16[8]);
 				vdraw_rInfo.vmodeFlags |=
@@ -195,8 +195,8 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			else
 			{
 				// Plugin doesn't support 565 output at all.
-				// TODO: Add a variable to indicate automatic color depth conversion.
-				// For now, just outputs 565.
+				// Render as 565, and convert it afterwards.
+				// TODO: Check if the plugin supports 565 rendering.
 				bppMD = 16;
 				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u16[8]);
 				vdraw_rInfo.vmodeFlags |=
@@ -234,12 +234,12 @@ int vdraw_set_renderer(const list<mdp_render_t*>::iterator& newMode, const bool 
 			else
 			{
 				// Plugin doesn't support 888 output at all.
-				// TODO: Add a variable to indicate automatic color depth conversion.
-				// For now, just output 888.
-				bppMD = 32;
+				// Render as 565, and convert it afterwards.
+				// TODO: Check if the plugin supports 565 rendering.
+				bppMD = 16;
 				vdraw_rInfo.mdScreen = (void*)(&MD_Screen.u32[8]);
 				vdraw_rInfo.vmodeFlags |=
-					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_888, MDP_RENDER_VMODE_RGB_888);
+					MDP_RENDER_VMODE_CREATE(MDP_RENDER_VMODE_RGB_565, MDP_RENDER_VMODE_RGB_565);
 				vdraw_needs_conversion = true;
 			}
 			break;