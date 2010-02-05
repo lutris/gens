lutris/gens

commit 3c9f57577ff5f8d5a85e6931f2fd330db986f47f

mem_m68k.asm: Added full mirroring for the VDP region. ($C00000-$DFFFFF)
According to Charles MacDonald's vdppin.txt document, the VDP is active
if the following equation is true:

(address & 0xE700E0) == 0xC00000

Only the low five bits are used for internal addresses in the VDP, so
once the address is verified using the above check, the address is masked
with 0x1F.

TODO: Add word read support for the HV counter.

src/gens/gens_core/mem/mem_m68k.asm
@@ -83,9 +83,9 @@ section .rodata align=64
 		dd	M68K_Read_Byte_Bad,		; 0xB00000 - 0xB7FFFF
 		dd	M68K_Read_Byte_Bad,		; 0xB80000 - 0xBFFFFF
 		dd	M68K_Read_Byte_VDP,		; 0xC00000 - 0xC7FFFF
-		dd	M68K_Read_Byte_Bad,		; 0xC80000 - 0xCFFFFF
-		dd	M68K_Read_Byte_Bad,		; 0xD00000 - 0xD7FFFF
-		dd	M68K_Read_Byte_Bad,		; 0xD80000 - 0xDFFFFF
+		dd	M68K_Read_Byte_VDP,		; 0xC80000 - 0xCFFFFF
+		dd	M68K_Read_Byte_VDP,		; 0xD00000 - 0xD7FFFF
+		dd	M68K_Read_Byte_VDP,		; 0xD80000 - 0xDFFFFF
 		dd	M68K_Read_Byte_Ram,		; 0xE00000 - 0xE7FFFF
 		dd	M68K_Read_Byte_Ram,		; 0xE80000 - 0xEFFFFF
 		dd	M68K_Read_Byte_Ram,		; 0xF00000 - 0xF7FFFF
@@ -118,9 +118,9 @@ section .rodata align=64
 		dd	M68K_Read_Word_Bad,		; 0xB00000 - 0xB7FFFF
 		dd	M68K_Read_Word_Bad,		; 0xB80000 - 0xBFFFFF
 		dd	M68K_Read_Word_VDP,		; 0xC00000 - 0xC7FFFF
-		dd	M68K_Read_Word_Bad,		; 0xC80000 - 0xCFFFFF
-		dd	M68K_Read_Word_Bad,		; 0xD00000 - 0xD7FFFF
-		dd	M68K_Read_Word_Bad,		; 0xD80000 - 0xDFFFFF
+		dd	M68K_Read_Word_VDP,		; 0xC80000 - 0xCFFFFF
+		dd	M68K_Read_Word_VDP,		; 0xD00000 - 0xD7FFFF
+		dd	M68K_Read_Word_VDP,		; 0xD80000 - 0xDFFFFF
 		dd	M68K_Read_Word_Ram,		; 0xE00000 - 0xE7FFFF
 		dd	M68K_Read_Word_Ram,		; 0xE80000 - 0xEFFFFF
 		dd	M68K_Read_Word_Ram,		; 0xF00000 - 0xF7FFFF
@@ -141,7 +141,7 @@ section .rodata align=64
 		dd	M68K_Write_Byte_Misc,		; 0xA00000 - 0xAFFFFF
 		dd	M68K_Write_Bad,			; 0xB00000 - 0xBFFFFF
 		dd	M68K_Write_Byte_VDP,		; 0xC00000 - 0xCFFFFF
-		dd	M68K_Write_Bad,			; 0xD00000 - 0xDFFFFF
+		dd	M68K_Write_Byte_VDP,		; 0xD00000 - 0xDFFFFF
 		dd	M68K_Write_Byte_Ram,		; 0xE00000 - 0xEFFFFF
 		dd	M68K_Write_Byte_Ram,		; 0xF00000 - 0xFFFFFF
 	
@@ -160,7 +160,7 @@ section .rodata align=64
 		dd	M68K_Write_Word_Misc,		; 0xA00000 - 0xAFFFFF
 		dd	M68K_Write_Bad,			; 0xB00000 - 0xBFFFFF
 		dd	M68K_Write_Word_VDP,		; 0xC00000 - 0xCFFFFF
-		dd	M68K_Write_Bad,			; 0xD00000 - 0xDFFFFF
+		dd	M68K_Write_Word_VDP,		; 0xD00000 - 0xDFFFFF
 		dd	M68K_Write_Word_Ram,		; 0xE00000 - 0xEFFFFF
 		dd	M68K_Write_Word_Ram,		; 0xF00000 - 0xFFFFFF
 	
@@ -768,11 +768,17 @@ section .text align=64
 	
 	global M68K_Read_Byte_VDP
 	M68K_Read_Byte_VDP:
-		cmp	ebx, 0xC00004
+		; Valid address: ((address & 0xE700E0) == 0xC00000)
+		; Information from vdppin.txt, (c) 2008 Charles MacDonald.
+		test	ebx, 0x700E0
+		jnz	short .bad
+		and	ebx, byte 0x1F	; only check low 5 bits.
+		
+		cmp	ebx, byte 0x04
 		jb	short .bad
-		cmp	ebx, 0xC00008
+		cmp	ebx, byte 0x08
 		jb	short .vdp_status
-		cmp	ebx, 0xC00009
+		cmp	ebx, byte 0x09
 		ja	short .bad
 	
 	.vdp_counter:
@@ -1116,7 +1122,14 @@ section .text align=64
 	
 	global M68K_Read_Word_VDP
 	M68K_Read_Word_VDP:
-		cmp	ebx, 0xC00003
+		; Valid address: ((address & 0xE700E0) == 0xC00000)
+		; Information from vdppin.txt, (c) 2008 Charles MacDonald.
+		test	ebx, 0x700E0
+		jnz	short .bad
+		and	ebx, byte 0x1F	; only check low 5 bits.
+		
+		; TODO: $C00008 == HV counter.
+		cmp	ebx, byte 0x03
 		ja	short .no_vdp_data
 		
 		call	SYM(VDP_Read_Data)
@@ -1407,7 +1420,13 @@ section .text align=64
 	
 	global M68K_Write_Byte_VDP
 	M68K_Write_Byte_VDP:
-		cmp	ebx, 0xC00003
+		; Valid address: ((address & 0xE700E0) == 0xC00000)
+		; Information from vdppin.txt, (c) 2008 Charles MacDonald.
+		test	ebx, 0x700E0
+		jnz	short .bad
+		and	ebx, byte 0x1F	; only check low 5 bits.
+		
+		cmp	ebx, byte 0x03
 		ja	short .no_data_port
 		
 		push	eax
@@ -1420,8 +1439,8 @@ section .text align=64
 	align 16
 	
 	.no_data_port:
-		cmp	ebx, 0xC00011
-		jne	.bad
+		cmp	ebx, byte 0x11
+		jne	short .bad
 		
 		push	eax
 		call	SYM(PSG_Write)
@@ -1692,7 +1711,13 @@ section .text align=64
 	
 	global M68K_Write_Word_VDP
 	M68K_Write_Word_VDP:
-		cmp	ebx, 0xC00003
+		; Valid address: ((address & 0xE700E0) == 0xC00000)
+		; Information from vdppin.txt, (c) 2008 Charles MacDonald.
+		test	ebx, 0x700E0
+		jnz	short .bad
+		and	ebx, byte 0x1F	; only check low 5 bits.
+		
+		cmp	ebx, byte 0x03
 		ja	short .no_data_port
 		
 		push	eax

commit b8a9d1ba3a18737ddd75d27b61cd05296b903194

mem_m68k.asm: Fixed some regressions caused by the VDP mirroring changes.
This is a regression from commit 3c9f57577ff5f8d5a85e6931f2fd330db986f47f.

I updated the VDP memory functions to check for all VDP mirrors. If the
address matched a mirror, then the address was AND'd with 0x1F to get
the actual VDP address. However, I forgot to update some of the internal
VDP address checks to use the 5-bit addresses, so it ended up causing
random breakage.

This bug was reported by Jorge in #retro.

src/gens/gens_core/mem/mem_m68k.asm
@@ -1166,7 +1166,7 @@ section .text align=64
 	align 16
 	
 	.no_vdp_data:
-		cmp	ebx, 0xC00007
+		cmp	ebx, byte 0x07
 		ja	.no_vdp_status
 		
 		call	SYM(VDP_Read_Status)
@@ -1176,7 +1176,7 @@ section .text align=64
 	align 16
 	
 	.no_vdp_status:
-		cmp	ebx, 0xC00009
+		cmp	ebx, byte 0x09
 		ja	short .bad
 		call	SYM(VDP_Read_V_Counter)
 		mov	bl, al
@@ -1757,7 +1757,7 @@ section .text align=64
 	align 16
 	
 	.no_data_port:
-		cmp	ebx, 0xC00007
+		cmp	ebx, byte 0x07
 		ja	short .bad
 		
 		push	eax