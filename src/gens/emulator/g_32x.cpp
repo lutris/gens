lutris/gens

commit 807b911dd6eec0fdd01cd6091d1cec6af93de312

gens_core/vdp/vdp_io.c: New VDP_Lines_t struct for VDP line information.
This struct contains all the information about the entire display size
and the visible size. Among other things, this allows us to draw the top
border in the correct frame. (Previously, the top border was drawn from
the previous frame, which is wrong.)

Updated g_md.cpp::T_gens_do_MD_frame() to use a single loop for the entire
frame. g_mcd.cpp and g_32x.cpp are currently #if 0'd.

src/gens/emulator/g_32x.cpp
@@ -172,7 +172,9 @@ int Init_32X(ROM_t* MD_ROM)
 			break;
 	}
 	
-	VDP_Num_Vis_Lines = 224;
+	// Initialize VDP_Lines.Display.
+	VDP_Set_Visible_Lines();
+	
 	Gen_Version = 0x20 + 0x0;	// Version de la megadrive (0x0 - 0xF)
 	
 	// Two copies of the ROM are needed, one for the 68000 and one for the SH2s.
@@ -316,16 +318,17 @@ void Reset_32X(void)
 int Do_32X_VDP_Only(void)
 {
 	// Set the number of visible lines.
-	VDP_SET_VISIBLE_LINES();
+	// Initialize VDP_Lines.Display.
+	VDP_Set_Visible_Lines();
 	
-	for (VDP_Current_Line = 0;
-	     VDP_Current_Line < VDP_Num_Vis_Lines;
-	     VDP_Current_Line++)
+	for (VDP_Lines.Display.Current = 0;
+	     VDP_Lines.Display.Current < VDP_Lines.Display.Total;
+	     VDP_Lines.Display.Current++, VDP_Lines.Visible.Current++)
 	{
 		Render_Line_32X();
 		Post_Line_32X();
 	}
-
+	
 	return 0;
 }
 
@@ -337,6 +340,8 @@ int Do_32X_VDP_Only(void)
 template<bool VDP>
 static inline int T_gens_do_32X_frame(void)
 {
+	// TODO: Update for VDP_Lines.
+#if 0
 	int i, j, k, l, p_i, p_j, p_k, p_l, *buf[2];
 	int HInt_Counter, HInt_Counter_32X;
 	int CPL_PWM;
@@ -633,7 +638,8 @@ static inline int T_gens_do_32X_frame(void)
 	post_frame.bpp = bppMD;
 	
 	EventMgr::RaiseEvent(MDP_EVENT_POST_FRAME, &post_frame);
-	
+#endif
+		
 	return 1;
 }
 

src/gens/emulator/g_mcd.cpp
@@ -176,7 +176,9 @@ int Init_SegaCD(const char* iso_name)
 	// Set clock rates depending on the CPU mode (NTSC / PAL).
 	Set_Clock_Freq(1);
 	
-	VDP_Num_Vis_Lines = 224;
+	// Initialize VDP_Lines.Display.
+	VDP_Set_Visible_Lines();
+	
 	Gen_Version = 0x20 + 0x0;	// Version de la megadrive (0x0 - 0xF)
 	
 	// TODO: Why are these two bytes set to 0xFF?
@@ -401,6 +403,8 @@ static void SegaCD_Display_LED(void)
 template<bool VDP, bool perfect_sync>
 static inline int T_gens_do_MCD_frame(void)
 {
+	// TODO: Update for VDP_Lines.
+#if 0
 	int *buf[2], i, j;
 	int HInt_Counter;
 	
@@ -710,6 +714,7 @@ static inline int T_gens_do_MCD_frame(void)
 	post_frame.bpp = bppMD;
 	
 	EventMgr::RaiseEvent(MDP_EVENT_POST_FRAME, &post_frame);
+#endif
 	
 	return 1;
 }

src/gens/emulator/g_md.cpp
@@ -181,7 +181,9 @@ void Init_Genesis_Bios(void)
 	memcpy(Rom_Data.u8, Genesis_Rom, Rom_Size);
 	Game_Mode = 0;
 	CPU_Mode = 0;
-	VDP_Num_Vis_Lines = 224;
+	
+	// Initialize VDP_Lines.Display.
+	VDP_Set_Visible_Lines();
 	
 	// Set the clock frequencies.
 	Set_Clock_Freq(0);
@@ -304,7 +306,9 @@ int Init_Genesis(ROM_t* MD_ROM)
 			break;
 	}
 	
-	VDP_Num_Vis_Lines = 224;
+	// Initialize VDP_Lines.Display.
+	VDP_Set_Visible_Lines();
+	
 	Gen_Version = 0x20 + 0x0;	// Version de la megadrive (0x0 - 0xF)
 	
 	// Byteswap the ROM data.
@@ -400,12 +404,12 @@ void Reset_Genesis(void)
  */
 int Do_VDP_Only(void)
 {
-	// Set the number of visible lines.
-	VDP_SET_VISIBLE_LINES();
+	// Initialize VDP_Lines.Display.
+	VDP_Set_Visible_Lines();
 	
-	for (VDP_Current_Line = 0;
-	     VDP_Current_Line < VDP_Num_Lines;
-	     VDP_Current_Line++)
+	for (VDP_Lines.Display.Current = 0;
+	     VDP_Lines.Display.Current < VDP_Lines.Display.Total;
+	     VDP_Lines.Display.Current++, VDP_Lines.Visible.Current++)
 	{
 		VDP_Render_Line_m5();
 	}
@@ -445,8 +449,8 @@ static inline int T_gens_do_MD_frame(void)
 	int *buf[2];
 	int HInt_Counter;
 	
-	// Set the number of visible lines.
-	VDP_SET_VISIBLE_LINES();
+	// Initialize VDP_Lines.Display.
+	VDP_Set_Visible_Lines();
 	
 	YM_Buf[0] = PSG_Buf[0] = Seg_L;
 	YM_Buf[1] = PSG_Buf[1] = Seg_R;
@@ -469,20 +473,16 @@ static inline int T_gens_do_MD_frame(void)
 	
 	HInt_Counter = VDP_Reg.m5.H_Int;	// Hint_Counter = step H interrupt
 	
-	/**
-	 * Visible Area.
-	 * V28: Lines 0-223.
-	 * V30: Lines 0-239.
-	 */
-	for (VDP_Current_Line = 0;
-	     VDP_Current_Line < VDP_Num_Vis_Lines;
-	     VDP_Current_Line++)
+	/** Main execution loop. **/
+	for (VDP_Lines.Display.Current = 0;
+	     VDP_Lines.Display.Current < VDP_Lines.Display.Total;
+	     VDP_Lines.Display.Current++, VDP_Lines.Visible.Current++)
 	{
-		buf[0] = Seg_L + Sound_Extrapol[VDP_Current_Line][0];
-		buf[1] = Seg_R + Sound_Extrapol[VDP_Current_Line][0];
-		YM2612_DacAndTimers_Update(buf, Sound_Extrapol[VDP_Current_Line][1]);
-		YM_Len += Sound_Extrapol[VDP_Current_Line][1];
-		PSG_Len += Sound_Extrapol[VDP_Current_Line][1];
+		buf[0] = Seg_L + Sound_Extrapol[VDP_Lines.Display.Current][0];
+		buf[1] = Seg_R + Sound_Extrapol[VDP_Lines.Display.Current][0];
+		YM2612_DacAndTimers_Update(buf, Sound_Extrapol[VDP_Lines.Display.Current][1]);
+		YM_Len += Sound_Extrapol[VDP_Lines.Display.Current][1];
+		PSG_Len += Sound_Extrapol[VDP_Lines.Display.Current][1];
 		
 		Fix_Controllers();
 		Cycles_M68K += CPL_M68K;
@@ -490,97 +490,45 @@ static inline int T_gens_do_MD_frame(void)
 		if (VDP_Reg.DMAT_Length)
 			main68k_addCycles(VDP_Update_DMA());
 		
-		VDP_Status |=  0x0004;	// HBlank = 1
-		main68k_exec(Cycles_M68K - 404);
-		VDP_Status &= ~0x0004;	// HBlank = 0
+		const bool inVisibleArea = (VDP_Lines.Visible.Current >= 0 &&
+						VDP_Lines.Visible.Current < VDP_Lines.Visible.Total);
 		
-		if (--HInt_Counter < 0)
+		if (inVisibleArea)
 		{
-			HInt_Counter = VDP_Reg.m5.H_Int;
-			VDP_Int |= 0x4;
+			// In visible area.
+			VDP_Status |=  0x0004;	// HBlank = 1
+			main68k_exec(Cycles_M68K - 404);
+			VDP_Status &= ~0x0004;	// HBlank = 0
+			
+			if (--HInt_Counter < 0)
+			{
+				VDP_Int |= 0x4;
+				VDP_Update_IRQ_Line();
+				HInt_Counter = VDP_Reg.m5.H_Int;
+			}
+		}
+		else if (VDP_Lines.Visible.Current == VDP_Lines.Visible.Total)
+		{
+			// VBlank line!
+			if (--HInt_Counter < 0)
+			{
+				VDP_Int |= 0x4;
+				VDP_Update_IRQ_Line();
+			}
+	
+			CONGRATULATIONS_PRECHECK;
+			VDP_Status |= 0x000C;		// VBlank = 1 et HBlank = 1 (retour de balayage vertical en cours)
+			main68k_exec(Cycles_M68K - 360);
+			Z80_EXEC(168);
+			CONGRATULATIONS_POSTCHECK;
+	
+			VDP_Status &= ~0x0004;		// HBlank = 0
+			VDP_Status |=  0x0080;		// V Int happened
+	
+			VDP_Int |= 0x8;
 			VDP_Update_IRQ_Line();
+			mdZ80_interrupt(&M_Z80, 0xFF);
 		}
-		
-		if (VDP)
-		{
-			// VDP needs to be updated.
-			VDP_Render_Line_m5();
-		}
-		
-		main68k_exec(Cycles_M68K);
-		Z80_EXEC(0);
-	}
-	
-	/**
-	 * Vertical Blank start.
-	 * V28: Line 224.
-	 * V30: Line 240.
-	 */
-	
-	buf[0] = Seg_L + Sound_Extrapol[VDP_Current_Line][0];
-	buf[1] = Seg_R + Sound_Extrapol[VDP_Current_Line][0];
-	YM2612_DacAndTimers_Update (buf, Sound_Extrapol[VDP_Current_Line][1]);
-	YM_Len += Sound_Extrapol[VDP_Current_Line][1];
-	PSG_Len += Sound_Extrapol[VDP_Current_Line][1];
-	
-	Fix_Controllers();
-	Cycles_M68K += CPL_M68K;
-	Cycles_Z80 += CPL_Z80;
-	if (VDP_Reg.DMAT_Length)
-		main68k_addCycles(VDP_Update_DMA());
-	
-	if (--HInt_Counter < 0)
-	{
-		VDP_Int |= 0x4;
-		VDP_Update_IRQ_Line();
-	}
-	
-	CONGRATULATIONS_PRECHECK;
-	VDP_Status |= 0x000C;		// VBlank = 1 et HBlank = 1 (retour de balayage vertical en cours)
-	main68k_exec(Cycles_M68K - 360);
-	Z80_EXEC(168);
-	CONGRATULATIONS_POSTCHECK;
-	
-	VDP_Status &= ~0x0004;		// HBlank = 0
-	VDP_Status |=  0x0080;		// V Int happened
-	
-	VDP_Int |= 0x8;
-	VDP_Update_IRQ_Line();
-	mdZ80_interrupt(&M_Z80, 0xFF);
-	
-	main68k_exec(Cycles_M68K);
-	Z80_EXEC(0);
-	
-	if (VDP)
-	{
-		// VDP needs to be updated.
-		VDP_Render_Line_m5();
-	}
-	
-	/**
-	 * Remainder of Vertical Blank.
-	 * V28: Lines 225-max
-	 * V30: Lines 241-max
-	 */
-	for (VDP_Current_Line++;
-	     VDP_Current_Line < VDP_Num_Lines;
-	     VDP_Current_Line++)
-	{
-		buf[0] = Seg_L + Sound_Extrapol[VDP_Current_Line][0];
-		buf[1] = Seg_R + Sound_Extrapol[VDP_Current_Line][0];
-		YM2612_DacAndTimers_Update(buf, Sound_Extrapol[VDP_Current_Line][1]);
-		YM_Len += Sound_Extrapol[VDP_Current_Line][1];
-		PSG_Len += Sound_Extrapol[VDP_Current_Line][1];
-		
-		Fix_Controllers();
-		Cycles_M68K += CPL_M68K;
-		Cycles_Z80 += CPL_Z80;
-		if (VDP_Reg.DMAT_Length)
-			main68k_addCycles(VDP_Update_DMA());
-		
-		VDP_Status |=  0x0004;	// HBlank = 1
-		main68k_exec(Cycles_M68K - 404);
-		VDP_Status &= ~0x0004;	// HBlank = 0
 		
 		if (VDP)
 		{
@@ -603,13 +551,14 @@ static inline int T_gens_do_MD_frame(void)
 		gym_dump_update(0, 0, 0);
 	
 	// Raise the MDP_EVENT_POST_FRAME event.
+	// TODO: Adjust post_frame.md_screen offset for VDP border stuff.
 	mdp_event_post_frame_t post_frame;
 	if (bppMD == 32)
 		post_frame.md_screen = &MD_Screen32[8];
 	else
 		post_frame.md_screen = &MD_Screen[8];
 	post_frame.width = vdp_getHPix();
-	post_frame.height = VDP_Num_Vis_Lines;
+	post_frame.height = VDP_Lines.Visible.Total;
 	post_frame.pitch = 336;
 	post_frame.bpp = bppMD;
 	

src/gens/gens_core/vdp/vdp_io.h
@@ -210,11 +210,30 @@ extern union
 
 extern uint8_t  H_Counter_Table[512][2];
 
-extern int VDP_Current_Line;
-extern int VDP_Num_Lines;
-extern int VDP_Num_Vis_Lines;
 extern int VDP_Int;
 extern int VDP_Status;
+
+// VDP line counters.
+// NOTE: Gens/GS currently uses 312 lines for PAL. It should use 313!
+typedef struct _VDP_Lines_t
+{
+	/** Total lines using NTSC/PAL line numbering. **/
+	struct
+	{
+		unsigned int Total;	// Total number of lines on the display. (262, 313)
+		unsigned int Current;	// Current display line.
+	} Display;
+	
+	/** Visible lines using VDP line numbering. **/
+	struct
+	{
+		int Total;	// Total number of visible lines. (192, 224, 240)
+		int Current;	// Current visible line. (May be negative for top border.)
+		int Border_Size;	// Size of the border. (192 lines == 24; 224 lines == 8)
+		int NTSC_V30_Roll;	// Current NTSC V30 roll offset.
+	} Visible;
+} VDP_Lines_t;
+extern VDP_Lines_t VDP_Lines;
 
 // Flags.
 typedef union
@@ -303,7 +322,7 @@ static inline int vdp_getVPix(void)
 	rval = 1;
 #endif
 	
-	return VDP_Num_Vis_Lines;
+	return VDP_Lines.Visible.Total;
 }
 
 #ifdef __cplusplus
@@ -315,15 +334,37 @@ static inline int vdp_getVPix(void)
 /**
  * VDP_SET_VISIBLE_LINES(): Sets the number of visible lines, depending on CPU mode and VDP setting.
  * Possible options:
- * - Normal: 224 lines. (V28)
- * - If PAL and Set2.M2 (bit 3) is set, 240 lines. (V30)
+ * - Mode 4 (SMS): 192 lines. (TODO)
+ * - Mode 5, V28: 224 lines.
+ * - Mode 5, V30: 240 lines. (NTSC V30 results in a rolling picture.)
  */
-#define VDP_SET_VISIBLE_LINES()				\
-do {							\
-	if ((CPU_Mode == 1) && (VDP_Reg.m5.Set2 & 0x8))	\
-		VDP_Num_Vis_Lines = 240;		\
-	else						\
-		VDP_Num_Vis_Lines = 224;		\
-} while (0)
+static inline void VDP_Set_Visible_Lines(void)
+{
+	// CPU_Mode: 0 == NTSC; 1 == PAL
+	// TODO: Remove the "+1" for PAL once Gens/GS uses 313 lines instead of 312.
+	if (VDP_Reg.m5.Set2 & 0x08)
+	{
+		// V30.
+		VDP_Lines.Visible.Total = 240;
+		VDP_Lines.Visible.Current = ((CPU_Mode == 1) ? -43+1 : 0);
+		VDP_Lines.Visible.Border_Size = 0;
+	}
+	else
+	{
+		// V28.
+		VDP_Lines.Visible.Total = 224;
+		VDP_Lines.Visible.Current = ((CPU_Mode == 1) ? -51+1 : -24);
+		VDP_Lines.Visible.Border_Size = 8;
+	}
+	
+	// TODO: V24 (192 lines)
+#if 0
+	{
+		VDP_Lines.Visible.Total = 192;
+		VDP_Lines.Visible.Current = ((CPU_Mode == 1) ? -67+1 : -40);
+		VDP_Lines.Visible.Border_Size = 24;
+	}
+#endif
+}
 
 #endif /* GENS_VDP_IO_H */

src/gens/debugger/debugger.cpp
@@ -231,14 +231,14 @@ void Debug_Event(int key, int mod)
 		
 		case GENS_KEY_l:
 			// Go up one VDP line.
-			if (VDP_Current_Line > 0)
-				VDP_Current_Line--;
+			if (VDP_Lines.Display.Current > 0)
+				VDP_Lines.Display.Current--;
 			break;
 		
 		case GENS_KEY_m:
 			// Go down one VDP line.
-			if (VDP_Current_Line < 319)
-				VDP_Current_Line++;
+			if (VDP_Lines.Display.Current < (VDP_Lines.Display.Total - 1))
+				VDP_Lines.Display.Current++;
 			break;
 		
 		case GENS_KEY_x:
@@ -834,7 +834,7 @@ static void Refresh_VDP_State(void)
 			(tmp >> 3) & 1, (tmp >> 2) & 1);
 	PrintF_Text(162, 110, TEXT_WHITE,
 			"DMA Busy %d  PAL Mode %d Line Num %d",
-			(tmp >> 1) & 1, tmp & 1, VDP_Current_Line);
+			(tmp >> 1) & 1, tmp & 1, VDP_Lines.Display.Current);
 	PrintF_Text(162, 118, TEXT_WHITE,
 			"VDP Int =%02X DMA_Length=%04X",
 			VDP_Int, VDP_Reg.DMAT_Length);
@@ -899,8 +899,8 @@ static inline void T_Refresh_VDP_Palette_Colors(pixel *screen, pixel *palette, u
 			VDP_Update_Palette();
 	}
 	
-	unsigned int VBorder = (240 - VDP_Num_Vis_Lines) / 2;
-	pixel *pLine = &screen[(336 * (VBorder + 10)) + 180];
+	const unsigned int line_number = (VDP_Lines.Visible.Border_Size + 10);
+	pixel *pLine = &screen[(336 * line_number) + 180];
 	
 	// Palette number.
 	int pal_num = -1;
@@ -928,8 +928,8 @@ template<typename pixel>
 static inline void T_Refresh_VDP_Palette_Outline(pixel *screen, unsigned int paletteMask, pixel outlineColor)
 {
 	// Outline the selected palette. Ported from Gens Rerecording.
-	unsigned int VBorder = (240 - VDP_Num_Vis_Lines) / 2;
-	pixel *line1 = &screen[(336 * (VBorder + 9 + ((pattern_pal & paletteMask) * 8))) + 180];
+	const unsigned int line_number = (VDP_Lines.Visible.Border_Size + 9);
+	pixel *line1 = &screen[(336 * (line_number + ((pattern_pal & paletteMask) * 8))) + 180];
 	pixel *line2 = line1 + (336 * 9);
 	
 	for (unsigned int i = 16 * 8; i != 0; i -= 2, line1 += 2, line2 += 2)

src/gens/emulator/g_main.cpp
@@ -175,12 +175,15 @@ int Init_Settings(void)
 	bppOut = 32;
 	
 	// Old code from InitParameters().
-	VDP_Num_Vis_Lines = 224;
+	
+	// Initialize VDP_Lines.
+	VDP_Set_Visible_Lines();
+	
 #if 0	// TODO: Replace with MDP "exclusive mode" later.
 	Net_Play = 0;
 #endif
-	Sprite_Over = 1;
 	
+	Sprite_Over = 1;
 	GYM_Dumping = 0;
 	
 	Game = NULL;

src/gens/emulator/gens.cpp
@@ -142,7 +142,7 @@ void Set_Clock_Freq(const int system)
 		CPL_SSH2 = (int)rint(((((((double)CLOCK_PAL / 7.0) * 3.0) / 50.0) / 312.0) *
 					(double)SSH2_Speed) / 100.0);
 		
-		VDP_Num_Lines = 312;
+		VDP_Lines.Display.Total = 312;	// TODO: Change to 313!
 		VDP_Status |= 0x0001;
 		
 		if (system == 1) // SegaCD
@@ -167,8 +167,8 @@ void Set_Clock_Freq(const int system)
 		CPL_SSH2 = (int)rint(((((((double)CLOCK_NTSC / 7.0) * 3.0) / 60.0) / 262.0) *
 					(double)SSH2_Speed) / 100.0);
 		
-		VDP_Num_Lines = 262;
-		VDP_Status &= 0xFFFE;
+		VDP_Lines.Display.Total = 262;
+		VDP_Status &= ~0x0001;
 		
 		if (system == 1) // SegaCD
 		{

src/gens/emulator/options.cpp
@@ -274,7 +274,7 @@ void Options::setCountry(const int newCountry)
 		CPL_SSH2 = (int)rint(((((((double)CLOCK_PAL / 7.0) * 3.0) / 50.0) / 312.0) *
 					(double)SSH2_Speed) / 100.0);
 		
-		VDP_Num_Lines = 312;
+		VDP_Lines.Display.Total = 312;	// TODO: Change to 313!
 		VDP_Status |= 0x0001;
 		_32X_VDP.Mode &= ~0x8000;
 		
@@ -290,8 +290,8 @@ void Options::setCountry(const int newCountry)
 		CPL_SSH2 = (int)rint(((((((double)CLOCK_NTSC / 7.0) * 3.0) / 60.0) / 262.0) *
 					(double)SSH2_Speed) / 100.0);
 		
-		VDP_Num_Lines = 262;
-		VDP_Status &= 0xFFFE;
+		VDP_Lines.Display.Total = 262;
+		VDP_Status &= ~0x0001;
 		_32X_VDP.Mode |= 0x8000;
 		
 		CD_Access_Timer = 2096;

commit 1da80f5e6f27951a02ce2400bb84fa0366231e24

Fixed a horizontal interrupt counter regression that broke raster effects in PAL.
This is a regression from commit 807b911dd6eec0fdd01cd6091d1cec6af93de312 (VDP_Lines_t).

The horizontal interrupt counters have been moved out of the frame rendering
loop and into VDP_Reg (MD) and _32X_VDP (32X).

Games tested include Sonic 1 (rev00 and rev01), Sonic 2 (rev01), Sonic 3,
and Sonic 3 & Knuckles. All of them had broken raster effects in PAL before
this change. This change fixes raster effects in PAL for all of them. :)

This bug was reported by djohe in #retro.

src/gens/emulator/g_32x.cpp
@@ -340,7 +340,6 @@ template<bool VDP>
 static FORCE_INLINE int T_gens_do_32X_frame(void)
 {
 	int i, j, k, l, p_i, p_j, p_k, p_l, *buf[2];
-	int HInt_Counter, HInt_Counter_32X;
 	int CPL_PWM;
 	
 	// Initialize VDP_Lines.Display.
@@ -377,9 +376,6 @@ static FORCE_INLINE int T_gens_do_32X_frame(void)
 	
 	_32X_VDP.State &= ~0x8000;
 	
-	HInt_Counter = VDP_Reg.m5.H_Int;	// Hint_Counter = step d'interruption H
-	HInt_Counter_32X = _32X_HIC;
-	
 	p_i = 84;
 	p_j = (p_i * CPL_MSH2) / CPL_M68K;
 	p_k = (p_i * CPL_SSH2) / CPL_M68K;
@@ -411,6 +407,13 @@ static FORCE_INLINE int T_gens_do_32X_frame(void)
 		if (VDP_Reg.DMAT_Length)
 			main68k_addCycles(VDP_Update_DMA());
 		
+		// Initialize HInt_Counter on visible line 0.
+		if (VDP_Lines.Visible.Current == 0)
+		{
+			VDP_Reg.HInt_Counter = VDP_Reg.m5.H_Int;
+			_32X_VDP.HInt_Counter = _32X_HIC;
+		}
+		
 		const bool inVisibleArea = (VDP_Lines.Visible.Current >= 0 &&
 				VDP_Lines.Visible.Current < VDP_Lines.Visible.Total);
 		
@@ -433,16 +436,16 @@ static FORCE_INLINE int T_gens_do_32X_frame(void)
 			VDP_Status &= ~0x0004;	// HBlank = 0
 			_32X_VDP.State &= ~0x6000;
 			
-			if (--HInt_Counter < 0)
+			if (--VDP_Reg.HInt_Counter < 0)
 			{
-				HInt_Counter = VDP_Reg.m5.H_Int;
+				VDP_Reg.HInt_Counter = VDP_Reg.m5.H_Int;
 				VDP_Int |= 0x4;
 				VDP_Update_IRQ_Line();
 			}
 			
-			if (--HInt_Counter_32X < 0)
+			if (--_32X_VDP.HInt_Counter < 0)
 			{
-				HInt_Counter_32X = _32X_HIC;
+				_32X_VDP.HInt_Counter = _32X_HIC;
 				if (_32X_MINT & 0x04)
 					SH2_Interrupt(&M_SH2, 10);
 				if (_32X_SINT & 0x04)
@@ -478,15 +481,15 @@ static FORCE_INLINE int T_gens_do_32X_frame(void)
 		else if (VDP_Lines.Visible.Current == VDP_Lines.Visible.Total)
 		{
 			// VBlank line!
-			if (--HInt_Counter < 0)
+			if (--VDP_Reg.HInt_Counter < 0)
 			{
 				VDP_Int |= 0x4;
 				VDP_Update_IRQ_Line();
 			}
 			
-			if (--HInt_Counter_32X < 0)
+			if (--_32X_VDP.HInt_Counter < 0)
 			{
-				HInt_Counter_32X = _32X_HIC;
+				_32X_VDP.HInt_Counter = _32X_HIC;
 				if (_32X_MINT & 0x04)
 					SH2_Interrupt(&M_SH2, 10);
 				if (_32X_SINT & 0x04)
@@ -569,9 +572,10 @@ static FORCE_INLINE int T_gens_do_32X_frame(void)
 			VDP_Status &= ~0x0004;	// HBlank = 0
 			_32X_VDP.State &= ~0x6000;
 			
-			if (--HInt_Counter_32X < 0)
+			// TODO: WHy 32X HInt_Counter and not regular HInt_Counter?
+			if (--_32X_VDP.HInt_Counter < 0)
 			{
-				HInt_Counter_32X = _32X_HIC;
+				_32X_VDP.HInt_Counter = _32X_HIC;
 				if ((_32X_MINT & 0x04) && (_32X_MINT & 0x80))
 					SH2_Interrupt(&M_SH2, 10);
 				if ((_32X_SINT & 0x04) && (_32X_SINT & 0x80))

src/gens/emulator/g_mcd.cpp
@@ -415,7 +415,6 @@ static FORCE_INLINE int T_gens_do_MCD_frame(void)
 {
 	// TODO: Update for VDP border changes.
 	int *buf[2], i, j;
-	int HInt_Counter;
 	
 	// Initialize VDP_Lines.Display.
 	VDP_Set_Visible_Lines();
@@ -446,9 +445,13 @@ static FORCE_INLINE int T_gens_do_MCD_frame(void)
 	else
 		VDP_Status &= ~0x0010;
 	
-	HInt_Counter = VDP_Reg.m5.H_Int;	// Hint_Counter = step H interrupt
-	
 	/** Main execution loop. **/
+	/** TODO: Unroll the loop into four loops using a templated function:
+	 * - Loop 0: Top border.
+	 * - Loop 1: Active display.
+	 * - Loop 2: VBlank line.
+	 * - Loop 3: Bottom border.
+	 */
 	for (VDP_Lines.Display.Current = 0;
 	     VDP_Lines.Display.Current < VDP_Lines.Display.Total;
 	     VDP_Lines.Display.Current++, VDP_Lines.Visible.Current++)
@@ -475,6 +478,10 @@ static FORCE_INLINE int T_gens_do_MCD_frame(void)
 			Cycles_S68K += CPL_S68K;
 		if (VDP_Reg.DMAT_Length)
 			main68k_addCycles(VDP_Update_DMA());
+		
+		// Initialize HInt_Counter on visible line 0.
+		if (VDP_Lines.Visible.Current == 0)
+			VDP_Reg.HInt_Counter = VDP_Reg.m5.H_Int;
 		
 		const bool inVisibleArea = (VDP_Lines.Visible.Current >= 0 &&
 				VDP_Lines.Visible.Current < VDP_Lines.Visible.Total);
@@ -515,9 +522,9 @@ static FORCE_INLINE int T_gens_do_MCD_frame(void)
 			
 			VDP_Status &= ~0x0004;	// HBlank = 0
 			
-			if (--HInt_Counter < 0)
+			if (--VDP_Reg.HInt_Counter < 0)
 			{
-				HInt_Counter = VDP_Reg.m5.H_Int;
+				VDP_Reg.HInt_Counter = VDP_Reg.m5.H_Int;
 				VDP_Int |= 0x4;
 				VDP_Update_IRQ_Line();
 			}
@@ -548,7 +555,7 @@ static FORCE_INLINE int T_gens_do_MCD_frame(void)
 		else if (VDP_Lines.Visible.Current == VDP_Lines.Visible.Total)
 		{
 			// VBlank line!
-			if (--HInt_Counter < 0)
+			if (--VDP_Reg.HInt_Counter < 0)
 			{
 				VDP_Int |= 0x4;
 				VDP_Update_IRQ_Line();

src/gens/emulator/g_md.cpp
@@ -452,7 +452,6 @@ template<bool VDP>
 static FORCE_INLINE int T_gens_do_MD_frame(void)
 {
 	int *buf[2];
-	int HInt_Counter;
 	
 	// Initialize VDP_Lines.Display.
 	VDP_Set_Visible_Lines();
@@ -502,9 +501,14 @@ static FORCE_INLINE int T_gens_do_MD_frame(void)
 	else
 		VDP_Status &= ~0x0010;
 	
-	HInt_Counter = VDP_Reg.m5.H_Int;	// Hint_Counter = step H interrupt
-	
 	/** Main execution loop. **/
+	/** TODO: Unroll the loop into four loops using a templated function:
+	 * - Loop 0: Top border.
+	 * - Loop 1: Active display.
+	 * - Loop 2: VBlank line.
+	 * - Loop 3: Bottom border.
+	 */
+	
 	for (VDP_Lines.Display.Current = 0;
 	     VDP_Lines.Display.Current < VDP_Lines.Display.Total;
 	     VDP_Lines.Display.Current++, VDP_Lines.Visible.Current++)
@@ -521,6 +525,10 @@ static FORCE_INLINE int T_gens_do_MD_frame(void)
 		if (VDP_Reg.DMAT_Length)
 			main68k_addCycles(VDP_Update_DMA());
 		
+		// Initialize HInt_Counter on visible line 0.
+		if (VDP_Lines.Visible.Current == 0)
+			VDP_Reg.HInt_Counter = VDP_Reg.m5.H_Int;
+		
 		const bool inVisibleArea = (VDP_Lines.Visible.Current >= 0 &&
 						VDP_Lines.Visible.Current < VDP_Lines.Visible.Total);
 		
@@ -536,17 +544,17 @@ static FORCE_INLINE int T_gens_do_MD_frame(void)
 			main68k_exec(Cycles_M68K - 404);
 			VDP_Status &= ~0x0004;	// HBlank = 0
 			
-			if (--HInt_Counter < 0)
+			if (--VDP_Reg.HInt_Counter < 0)
 			{
 				VDP_Int |= 0x4;
 				VDP_Update_IRQ_Line();
-				HInt_Counter = VDP_Reg.m5.H_Int;
+				VDP_Reg.HInt_Counter = VDP_Reg.m5.H_Int;
 			}
 		}
 		else if (VDP_Lines.Visible.Current == VDP_Lines.Visible.Total)
 		{
 			// VBlank line!
-			if (--HInt_Counter < 0)
+			if (--VDP_Reg.HInt_Counter < 0)
 			{
 				VDP_Int |= 0x4;
 				VDP_Update_IRQ_Line();

src/gens/gens_core/vdp/vdp_32x.h
@@ -37,6 +37,7 @@ typedef struct _VDP_32X_t
 	unsigned int AF_St;
 	unsigned int AF_Len;
 	unsigned int AF_Line;
+	int HInt_Counter;
 } VDP_32X_t;
 extern VDP_32X_t _32X_VDP;
 

src/gens/gens_core/vdp/vdp_io.cpp
@@ -180,6 +180,9 @@ void VDP_Reset(void)
 	VDP_Flags.CRam = 1;
 	VDP_Flags.VRam = 1;
 	
+	// Initialize the Horizontal Interrupt counter.
+	VDP_Reg.HInt_Counter = VDP_Reg.m5.H_Int;
+	
 	// Initialize the Horizontal Counter table.
 	unsigned int hc;
 	unsigned int hc_val;

src/gens/gens_core/vdp/vdp_io.h
@@ -182,6 +182,9 @@ typedef struct
 	// which causes the entire screen to disappear if paused.
 	// TODO: Don't rerun the VDP drawing functions when paused!
 	int HasVisibleLines;	// 0 if VDP was off for the whole frame.
+	
+	// Horizontal Interrupt Counter.
+	int HInt_Counter;
 } VDP_Reg_t;
 extern VDP_Reg_t VDP_Reg;
 