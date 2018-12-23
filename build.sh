a3a8788d6cf897f7784b3b5d94ce85c50313eae3
Matt Kubilus <mattkubilus@gmail.com>
2018-12-23 16:59:40 -0500

    CI pkg juggling

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ apt-get install -y nasm gcc-multilib g++-multilib git
 apt-get install -y libgtk2.0-dev:i386 libsdl1.2-dev:i386
 autoreconf -fi
 export PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig/
-dpkg-buildpackage
+if [ -z "$TRAVIS_TAG" ]; then
+    dpkg-buildpackage
+else
+    dpkg-buildpackage -v $TRAVIS_TAG
+fi
 #./configure --host=i686-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
 #make
